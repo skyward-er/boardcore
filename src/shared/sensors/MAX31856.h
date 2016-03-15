7d0e65c142c4037ca068e873957a6df8b32ef9b6
Alain Carlucci <alain.carlucci@skywarder.eu>
2016-03-15 16:21:24 +0100

    [MAX31856] code fix

--- src/shared/sensors/MAX31856.h
+++ src/shared/sensors/MAX31856.h
@@ -24,82 +24,68 @@
 #ifndef MAX31856_H
 #define MAX31856_H
 
-#include "BusTemplate.h"
+#include "Sensor.h"
+#include <BusTemplate.h>
 
-class MAX31856_Regmap{
-    
-public:
-    
-    static constexpr uint8_t CR0 = 0x00;
-    static constexpr uint8_t CR1 = 0x01;
-    static constexpr uint8_t MASK = 0x02;
-    static constexpr uint8_t CJHF = 0x03;
-    static constexpr uint8_t CJLF = 0x04;
-    static constexpr uint8_t LTHFTH = 0x05;
-    static constexpr uint8_t LTHFTL = 0x06;
-    static constexpr uint8_t LTLFTH = 0x07;
-    static constexpr uint8_t LTLFTL = 0x08;
-    static constexpr uint8_t CJTO = 0x09;
-    static constexpr uint8_t CJTH = 0x0A;
-    static constexpr uint8_t CJTL = 0x0B;
-    static constexpr uint8_t LTCBH = 0x0C;
-    static constexpr uint8_t LTCBM = 0x0D;
-    static constexpr uint8_t LTCBL = 0x0E;
-    static constexpr uint8_t SR = 0x0F;
-    
-    static constexpr enum thTypes{
-        TH_B = 0x00,
-        TH_E = 0x01,
-        TH_J = 0x02,
-        TH_K = 0x03,
-        TH_N = 0x04,
-        TH_R = 0x05,
-        TH_S = 0x06,
-        TH_T = 0x07       
-    };
-    
-private:
-    //since this is only a wrapper class disallow creating instances
-    MAX31856_Regmap();
-};
+/** This is a thermocouple */
 
-template <class Regmap>
-class Thermocouple : public Sensor<>
-{
+template <class BusType>
+class MAX31856 : public TemperatureSensor {
 public:
-    
-    Thermocouple()
-    {
-        uint8_t cr0val = Sensor::ReadReg(Regmap::CR0); //get CR0 register actual value
+   
+    MAX31856() : lastTemperature(0.0f), cjEnabled(true) { }
+
+    bool init() {
+        // TODO: check WHO_AM_I
+        uint8_t cr0val = bus.read(REG_CR0); //get CR0 register actual value
         
         cr0val |= 0x01; //select 50Hz noise rejection
         
-        Sensor::WriteReg(Regmap::CR0, cr0val);
+        bus.write(REG_CR0, cr0val);
+
+        return true;
     }
+    
+    bool selfTest() {
+        return false;
+    }
+
+    void updateParams() {
+        uint32_t temp = getThermocoupleTemp();
+
+        // TODO convert uint32 temp to a float value
+        
+        lastTemperature = temp;
+    }
+
+    float getTemperature() {
+        return lastTemperature; 
+    }
+
+
+    // ----------- BELOW THERMOCOUPLE TUNING FUNCTIONS. ------------
     
     /**
      * @param mode true to set auto conversion mode, that stands
      * for continuous conversion, one every 100ms
      */
-    void setConversionMode(bool mode)
-    {
-        uint8_t regVal = Sensor::ReadReg(Regmap::CR0);
+    void setConversionMode(bool mode) {
+        uint8_t regVal = bus.read(REG_CR0);
         
         if(mode)
             regVal |= 0x80; //conversion mode is the 8-th bit of CR0 register
         else
             regVal &= ~0x80;
         
-        Sensor::WriteReg(Regmap::CR0,regVal);        
+        bus.write(REG_CR0,regVal);        
     }
     
     /**
      * @param mode true to enable internal cold junction temperature
      * sensor, false to disable it
      */
-    void enableCjSensor(bool mode)
-    {
-        uint8_t regVal = Sensor::ReadReg(Regmap::CR0);
+    void enableCjSensor(bool mode) {
+        uint8_t regVal = bus.read(REG_CR0);
         
         if(mode){
             regVal |= 0x08; //CJ temp. sensor is the 4-th bit of CR0 register
@@ -110,16 +96,15 @@ public:
             cjEnabled = false;
         }
         
-        Sensor::WriteReg(Regmap::CR0,regVal);        
+        bus.write(REG_CR0,regVal);        
     }
     
     /**
      * Trigger a one-shot conversion
      */
-    void requestConversion()
-    {
-        uint8_t regVal = Sensor::ReadReg(Regmap::CR0);
-        Sensor::WriteReg(Regmap::CR0, regVal | 0x40);   //conversion trigger is the 7-th bit of CR0 register
+    void requestConversion() {
+        uint8_t regVal = bus.read(REG_CR0);
+        bus.write(REG_CR0, regVal | 0x40);   //conversion trigger is the 7-th bit of CR0 register
     }
     
     /**
@@ -127,9 +112,8 @@ public:
      * achieve one masure
      * @param samplesNum number of samples, that are: 1,2,4,8,16
      */
-    void setSamples(uint8_t samplesNum)
-    {
-        uint8_t regVal = Sensor::ReadReg(Regmap::CR1);
+    void setSamples(uint8_t samplesNum) {
+        uint8_t regVal = bus.read(REG_CR1);
         regVal &= 0x8F; //clear bits from 7-th to 5-th
         
         switch(samplesNum){
@@ -153,53 +137,81 @@ public:
                 regVal |= 00000100 << 4;
                 break;                  
         }
-        Sensor::WriteReg(Regmap::CR1, regVal);
+        bus.write(REG_CR1, regVal);
     }
     
     /**
      * Set Thermocouple type
      */
-    void setThermoType(uint8_t type)
-    {
-        uint8_t regVal = Sensor::ReadReg(Regmap::CR1);
-        Sensor::WriteReg(Regmap::CR1, regVal | type);
+    void setThermoType(uint8_t type) {
+        uint8_t regVal = bus.read(REG_CR1);
+        bus.write(REG_CR1, regVal | type);
     }
     
     /**
      * @return cold junction temperature
      */
-    uint16_t getColdJunctionTemp()
-    {
-        return Sensor::ReadReg(Regmap::CJTH) << 8 | Sensor::ReadReg(Regmap::CJTL);
+    uint16_t getColdJunctionTemp() {
+        return bus.read(REG_CJTH) << 8 | bus.read(REG_CJTL);
     }
     
     /**
      * Set cold junction temperature, this is possible
      * only if internal cold junction temperature sensor is disabled
      */
-    void setColdJunctionTemp(uint16_t temp)
-    {
+    void setColdJunctionTemp(uint16_t temp) {
         if(!cjEnabled){
-            Sensor::WriteReg(Regmap::CJTH, (temp & 0xFF00) >> 16);
-            Sensor::WriteReg(Regmap::CJTL, temp & 0x00FF);
+            bus.write(REG_CJTH, (temp & 0xFF00) >> 16);
+            bus.write(REG_CJTL, temp & 0x00FF);
         }
     }
     
     /**
      * @return thermocouple temperature value, 19 bit right-aligned
      */
-    uint32_t getThermocoupleTemp()
-    {
-        return Sensor::ReadReg(Regmap::LTCBH) << 16 |
-               Sensor::ReadReg(Regmap::LTCBM) << 8 |
-               Sensor::ReadReg(Regmap::LTCBL);
+    uint32_t getThermocoupleTemp() {
+        return bus.read(REG_LTCBH) << 16 |
+               bus.read(REG_LTCBM) << 8 |
+               bus.read(REG_LTCBL);
     }
     
 private:
+    float lastTemperature;
+    BusType bus;
+
+    enum eRegMap{
+        REG_CR0     = 0x00,
+        REG_CR1     = 0x01,
+        REG_MASK    = 0x02,
+        REG_CJHF    = 0x03,
+        REG_CJLF    = 0x04,
+        REG_LTHFTH  = 0x05,
+        REG_LTHFTL  = 0x06,
+        REG_LTLFTH  = 0x07,
+        REG_LTLFTL  = 0x08,
+        REG_CJTO    = 0x09,
+        REG_CJTH    = 0x0A,
+        REG_CJTL    = 0x0B,
+        REG_LTCBH   = 0x0C,
+        REG_LTCBM   = 0x0D,
+        REG_LTCBL   = 0x0E,
+        REG_SR      = 0x0F,
+    };
+
+    enum eTypes {
+        TH_B = 0x00,
+        TH_E = 0x01,
+        TH_J = 0x02,
+        TH_K = 0x03,
+        TH_N = 0x04,
+        TH_R = 0x05,
+        TH_S = 0x06,
+        TH_T = 0x07,
+    };
     
     //flag to determine if internal cold junction temp sensor is enabled,
     //by default is enabled
-    bool cjEnabled = true;
+    bool cjEnabled;
 };
 
 #endif
