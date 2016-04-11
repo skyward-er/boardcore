3cd9cbef06145dbe3e1067c9b9bde88a1c03f5f1
Matteo Piazzolla <matteo.piazzolla@skywarder.eu>
2016-04-11 14:53:38 +0200

    ADC class

--- src/shared/sensors/ADC.h
+++ src/shared/sensors/ADC.h
@@ -1,28 +1,39 @@
+#include "Sensor.h"
+#include "Common.h"
+
 using namespace miosix;
 
 template<unsigned N,unsigned CHANNEL, class GpioADC>
-class SensorADC : public Sensor {
+class SensorADC  {
 public:
-    SensorADC(){
-        ADCx = getADC(N); 
+    SensorADC(uint8_t stime){
+        ADCx = getADC(N);
+        samplingTime = stime;
     }
 
     bool init() {
-		
+
 		GpioADC::mode(Mode::INPUT_ANALOG);
 		enableADC(ADCx);
 
         ADCx->SQR1 = 0x0; //One conversion
-        ADCx->SQR3 = CHANNEL; // Channel 9
-        
+        ADCx->SQR3 = CHANNEL;
+
         ADCx->CR2 = ADC_CR2_SWSTART
                   | ADC_CR2_CONT
                   | ADC_CR2_ADON;
-        //CCR pre scaler 
+        //CCR pre scaler
         ADC->CCR = 0x3<<16;
-        
-        ADCx->SMPR1= (0x7<<6)<<3;
-		printf("DIOCANE\n");
+
+    //    ADCx->SMPR1= (0x7<<6)<<3;
+
+        //set channel sampling time
+        if(CHANNEL<10){
+          ADCx->SMPR2 = (samplingTime << (3 * (CHANNEL))  );
+        }
+        if(CHANNEL>=10){
+          ADCx->SMPR1 = (samplingTime << (3 * (CHANNEL-10) ));
+        }
 
         return true;
     }
@@ -37,20 +48,19 @@ public:
 			else if(adc == ADC2){
 				RCC->APB2ENR |= RCC_APB2ENR_ADC2EN;
 				printf("ADC2\n");
-
 			}
 			else if(adc == ADC3){
 				RCC->APB2ENR |= RCC_APB2ENR_ADC3EN;
 				printf("ADC3\n");
 			}
-		}	
+		}
 
     }
-    
+
 
     bool selfTest() {
     return false;
-       
+
     }
 
     bool updateParams() {
@@ -59,8 +69,8 @@ public:
     ADCx->CR2 = ADC_CR2_SWSTART
                   | ADC_CR2_CONT
                   | ADC_CR2_ADON;
-                  
-    Thread::sleep(10); //Wait for tempsensor to stabilize
+
+    Thread::sleep(10);
     ADCx->CR2|=ADC_CR2_ADON; //Setting ADC ON twice starts a conversion
     while((ADCx->SR & ADC_SR_EOC)==0);
         last_value = ADCx->DR;
@@ -68,23 +78,36 @@ public:
     }
 
 
-    
-    
+
+
     constexpr ADC_TypeDef* getADC(unsigned n) {
         return  n==1 ? ADC1 :
                 n==2 ? ADC2 : ADC3;
     }
 
-    
+
     uint16_t getValue(){
         return last_value;
     }
+    enum channelSampling {
+        CYCLES_3     = 0x0,
+        CYCLES_15    = 0x1,
+        CYCLES_28    = 0x2,
+        CYCLES_56    = 0x3,
+        CYCLES_84    = 0x4,
+        CYCLES_112   = 0x5,
+        CYCLES_144   = 0x6,
+        CYCLES_480   = 0x7
+    };
 
 
 private:
 
     uint16_t last_value;
+    uint8_t samplingTime;
     ADC_TypeDef* ADCx;
+
+
 
 
 };
