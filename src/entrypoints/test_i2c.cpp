455bf3b0597495cd2eb1da18635c7655f394a271
silseva <silseva@fastwebnet.it>
2016-04-13 16:17:55 +0200

    Modified file, testing MPL3115

--- src/entrypoints/test_i2c.cpp
+++ src/entrypoints/test_i2c.cpp
@@ -3,18 +3,19 @@
 #include <BusTemplate.h>
 #include <sensors/MPL3115.h>
 #include <Leds.h>
+#include <drivers/stm32f2_f4_i2c.h>
 // #include <util/software_i2c.h>
 
 using namespace miosix;
 
-typedef Gpio<GPIOB_BASE,7> sda;
-typedef Gpio<GPIOB_BASE,8> scl;
-
-// typedef SoftwareI2C<sda,scl> i2c;
-
 int main()
 {
-    MPL3115<ProtocolI2C> baro;
+    sensors::sda::mode(Mode::ALTERNATE_OD);
+    sensors::sda::alternateFunction(4);
+    sensors::scl::mode(Mode::ALTERNATE_OD);
+    sensors::scl::alternateFunction(4);
+    
+    MPL3115<ProtocolI2C<I2C1Driver>> baro;
     
     printf("%d\n", baro.selfTest() ? 1 : 0);
     baro.setMode(baro.MODE_ALTIMETER);
