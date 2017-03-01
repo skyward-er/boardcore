6f64136bc8e6a29290b2df6db5b02836b6666b58
silseva <silseva@fastwebnet.it>
2017-03-01 09:30:33 +0100

    Restored work in progress code previously set aside

--- src/entrypoints/test_ethernet.cpp
+++ src/entrypoints/test_ethernet.cpp
@@ -6,17 +6,26 @@ using namespace miosix;
 using namespace std;
 
 int main()
-{    
-    udpComm->start();
+{
+     uint8_t ipAddr[] = {192,168,1,30};                        //Device's IP address
+     uint8_t mask[] = {255,255,255,0};                         //Subnet mask
+     uint8_t destIp[] = {192,168,1,4};                        //Destination IP address
+     
+    unsigned char message[] = "message from the hell...\n";
+     
+    W5200& eth = W5200::instance();
+    eth.setIpAddress(ipAddr);
+    eth.setSubnetMask(mask);
+     
+    auto udp = Singleton<UdpManager>::getInstance();    
+    
+    udp->setPort(1234);
 
-    while(1)
+    while(1)        
     {
-        ledOn();
-        Thread::sleep(1000);
-        ledOff();
+        udp->sendPacketTo(destIp,2020,(void *)message,sizeof(message)/sizeof(unsigned char));
         Thread::sleep(1000);
     }
-    
-    
+
     return 0;
 }
