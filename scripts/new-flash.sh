f923b3834e3f6a0cc54e1506a2edd5879bf80ea6
Alain <carpikemail@gmail.com>
2017-04-26 17:38:37 +0200

    [Scripts] Added openocd-flash

---
+++ scripts/new-flash.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <filename.bin>"
+    exit -1
+fi;
+
+gdb -ex 'target remote :3333' -ex 'monitor reset halt'  -ex 'monitor reset halt'  -ex 'monitor reset halt' -ex "monitor flash write_image erase $1 0x08000000" -ex 'continue&' -ex 'set confirm off' -ex 'quit'
