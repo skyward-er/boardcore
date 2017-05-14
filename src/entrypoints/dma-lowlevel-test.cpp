a906ca0fde125e15a98a912dd41d640621c13dfe
Alain Carlucci <alain.carlucci@skywarder.eu>
2017-05-14 11:44:58 +0200

    [Linter] Fixed stuff and added check on 'using namespace' in Header files

--- src/entrypoints/dma-lowlevel-test.cpp
+++ src/entrypoints/dma-lowlevel-test.cpp
@@ -1,3 +1,24 @@
+/* Copyright (c) 2015-2017 Skyward Experimental Rocketry
+ * Authors: Federico Terraneo
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 #include <sensors/iNemo.h>
 #include <sensors/FXAS21002.h>
@@ -29,10 +50,16 @@ int main()
     spiINEMOG::init();
     auto& spi=SPIDriver::instance();
     
-    inemo_t inemo(inemo_t::ACC_FS_16G,inemo_t::GYRO_FS_245,inemo_t::COMPASS_FS_2);
-    if(inemo.init()==false) puts("init failed");
+    inemo_t inemo (inemo_t::ACC_FS_16G,
+                   inemo_t::GYRO_FS_245,
+                   inemo_t::COMPASS_FS_2);
+
+    if(inemo.init()==false) 
+    {
+        puts("init failed");
+    }
+
     auto req=inemo.buildDMARequest();
-    
     auto sample=[&]()
     {
         if(spi.transaction(req)==false) puts("DMA error");
