75e2bc5f76a4736f54f3e87340f97b4c8e227753
Alain Carlucci <carpikemail@gmail.com>
2017-11-27 10:50:20 +0100

    [*] Added .clang-format file ad applied on entrypoints/

--- src/entrypoints/anakin-test-suite.cpp
+++ src/entrypoints/anakin-test-suite.cpp
@@ -1,16 +1,16 @@
 /* Copyright (c) 2015-2016 Skyward Experimental Rocketry
  * Authors: Alain Carlucci, Matteo Michele Piazzolla
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
@@ -30,21 +30,22 @@
 
 using namespace miosix;
 
-#define SENSOR(NAME,CSPORT,CSPIN) \
-    typedef Gpio<GPIO ## CSPORT ## _BASE, CSPIN> CS_ ## NAME; \
-    typedef ProtocolSPI < busSPI1, CS_ ## NAME > spi ## NAME
+#define SENSOR(NAME, CSPORT, CSPIN)                     \
+    typedef Gpio<GPIO##CSPORT##_BASE, CSPIN> CS_##NAME; \
+    typedef ProtocolSPI<busSPI1, CS_##NAME> spi##NAME
 
-#define READ(NAME, ID) spi ## NAME::read(ID)
+#define READ(NAME, ID) spi##NAME::read(ID)
 #define TEST(NAME, REG, REF) test_and_print(#NAME, REG, READ(NAME, REG), REF)
 
 #define ACCEL_ENDLESS_TEST
 
-// SPI1 
+// SPI1
 typedef Gpio<GPIOA_BASE, 5> GpioSck;
 typedef Gpio<GPIOA_BASE, 6> GpioMiso;
 typedef Gpio<GPIOA_BASE, 7> GpioMosi;
-typedef BusSPI < 1, GpioMosi, GpioMiso, GpioSck> busSPI1;
+typedef BusSPI<1, GpioMosi, GpioMiso, GpioSck> busSPI1;
 
+// clang-format off
 // Here's the list of SPI sensors that should be tested
 //    | NAME      | CSPORT | CSPIN |
 SENSOR(MPU9250,      D,       13);
@@ -55,56 +56,70 @@ SENSOR(LSM9DS0_G,    G,        9);
 SENSOR(LSM9DS0_A,    G,       11);
 SENSOR(LPS331AP,     E,        4);
 SENSOR(MS580301BA07, B,       10);
+// clang-format on
 
 static const char mOK[] = "OK";
 static const char mKO[] = "FAIL";
 
 int num_ok = 0, num_fail = 0;
 
-void banner() {
+void banner()
+{
     printf(
-"----------------------------------------------------------------------------\n"
-"d888888b d88888b .d8888. d888888b .d8888. db    db d888888b d888888b d88888b\n"
-"`~~88~~' 88'     88'  YP `~~88~~' 88'  YP 88    88   `88'   `~~88~~' 88'    \n"
-"   88    88ooooo `8bo.      88    `8bo.   88    88    88       88    88ooooo\n"
-"   88    88~~~~~   `Y8b.    88      `Y8b. 88    88    88       88    88~~~~~\n"
-"   88    88.     db   8D    88    db   8D 88b  d88   .88.      88    88.    \n"
-"   YP    Y88888P `8888Y'    YP    `8888Y' ~Y8888P' Y888888P    YP    Y88888P\n"
-"----------------------------------------------------------------------------\n"
-    "\n");
+        "----------------------------------------------------------------------"
+        "------\n"
+        "d888888b d88888b .d8888. d888888b .d8888. db    db d888888b d888888b "
+        "d88888b\n"
+        "`~~88~~' 88'     88'  YP `~~88~~' 88'  YP 88    88   `88'   `~~88~~' "
+        "88'    \n"
+        "   88    88ooooo `8bo.      88    `8bo.   88    88    88       88    "
+        "88ooooo\n"
+        "   88    88~~~~~   `Y8b.    88      `Y8b. 88    88    88       88    "
+        "88~~~~~\n"
+        "   88    88.     db   8D    88    db   8D 88b  d88   .88.      88    "
+        "88.    \n"
+        "   YP    Y88888P `8888Y'    YP    `8888Y' ~Y8888P' Y888888P    YP    "
+        "Y88888P\n"
+        "----------------------------------------------------------------------"
+        "------\n"
+        "\n");
 }
 
-void test_and_print(const char *name, uint8_t reg, 
-        uint8_t test, uint8_t reference) {
-    if (test == reference) 
+void test_and_print(const char* name, uint8_t reg, uint8_t test,
+                    uint8_t reference)
+{
+    if (test == reference)
         ++num_ok;
     else
         ++num_fail;
-    printf("[%13s] [REG 0x%02x]: 0x%02x -> [%s]\n", name, reg, 
-            test, (test == reference)?mOK:mKO);
+    printf("[%13s] [REG 0x%02x]: 0x%02x -> [%s]\n", name, reg, test,
+           (test == reference) ? mOK : mKO);
 }
 
-void assert_true(const char *name, const char *func, bool value) {
-    if(value)
+void assert_true(const char* name, const char* func, bool value)
+{
+    if (value)
         ++num_ok;
     else
         ++num_fail;
 
-    printf("[%13s] [FUNC %9s] -> [%s]\n", name, func, (value)?mOK:mKO);
+    printf("[%13s] [FUNC %9s] -> [%s]\n", name, func, (value) ? mOK : mKO);
 }
 
-void test_max31856() {
+void test_max31856()
+{
     // Check if a bunch of register have the reference value
-    TEST(MAX31856,  0x00, 0x00); // config 0
-    TEST(MAX31856,  0x01, 0x03); // config 1
-    TEST(MAX31856,  0x02, 0xFF); // mask 
-    TEST(MAX31856,  0x03, 0x7F); // hi fault
-    TEST(MAX31856,  0x04, 0xc0); // lo fault
-    TEST(MAX31856,  0x05, 0x7f); // lol
-    TEST(MAX31856,  0x0F, 0x00); // fault status reg
+    TEST(MAX31856, 0x00, 0x00);  // config 0
+    TEST(MAX31856, 0x01, 0x03);  // config 1
+    TEST(MAX31856, 0x02, 0xFF);  // mask
+    TEST(MAX31856, 0x03, 0x7F);  // hi fault
+    TEST(MAX31856, 0x04, 0xc0);  // lo fault
+    TEST(MAX31856, 0x05, 0x7f);  // lol
+    TEST(MAX31856, 0x0F, 0x00);  // fault status reg
 }
 
-void test_altimeter() {
+void test_altimeter()
+{
     MS580301BA07<spiMS580301BA07> altiMS580;
 
     // Read and check different const registers
@@ -114,25 +129,26 @@ void test_altimeter() {
     assert_true("MS580301BA07", "init()", altiMS580.init());
 }
 
-int main() {
-    MAX21105<spiMAX21105> max21(
-        MAX21105<spiMAX21105>::ACC_FS_2G,
-        MAX21105<spiMAX21105>::GYRO_FS_500
-    );
+int main()
+{
+    MAX21105<spiMAX21105> max21(MAX21105<spiMAX21105>::ACC_FS_2G,
+                                MAX21105<spiMAX21105>::GYRO_FS_500);
 
     banner();
-    // Pausa per lasciare il tempo ai sensori di accendersi 
+    // Pausa per lasciare il tempo ai sensori di accendersi
     Thread::sleep(100);
 
     printf("Polling sensors...\n");
 
+    // clang-format off
     // First check: compare sensors WHO_AM_I against the reference value
     TEST(MPU9250,   0x75, 0x71);
     TEST(FXAS21002, 0x0c, 0xd7);
     TEST(MAX21105,  0x20, 0xb4);
     TEST(LSM9DS0_G, 0x0f, 0xd4);
     TEST(LSM9DS0_A, 0x0f, 0x49);
-    TEST(LPS331AP, 0x0f, 0xBB);
+    TEST(LPS331AP,  0x0f, 0xBB);
+    // clang-format on
 
     test_max31856();
     test_altimeter();
@@ -140,26 +156,33 @@ int main() {
     assert_true("MAX21105", "init()", max21.init());
 
     printf("---------------------\n");
-    printf("NUM TEST OK:  %5d\n"
-           "NUM TEST FAIL:%5d\n\n", num_ok, num_fail);
+    printf(
+        "NUM TEST OK:  %5d\n"
+        "NUM TEST FAIL:%5d\n\n",
+        num_ok, num_fail);
 
 #ifdef ACCEL_ENDLESS_TEST
-    uint16_t cnt=0;
-    const Vec3* a = max21.accelDataPtr();
-    const Vec3* g = max21.gyroDataPtr();
+    uint16_t cnt      = 0;
+    const Vec3* a     = max21.accelDataPtr();
+    const Vec3* g     = max21.gyroDataPtr();
     const float* temp = max21.tempDataPtr();
-    while(true) {
-        max21.onSimpleUpdate(); 
+    while (true)
+    {
+        max21.onSimpleUpdate();
 
-        printf("%05u[ACC %+5.2f,%+5.2f,%+5.2f] "
-               "[GYR %+5.2f,%+5.2f,%+5.2f] "
-               "[TMP %+5.2fC] \r",
-                (uint16_t)(++cnt), a->getX(), a->getY(), a->getZ(),
-                g->getX(),g->getY(), g->getZ(), *temp);
+        printf(
+            "%05u[ACC %+5.2f,%+5.2f,%+5.2f] "
+            "[GYR %+5.2f,%+5.2f,%+5.2f] "
+            "[TMP %+5.2fC] \r",
+            (uint16_t)(++cnt), a->getX(), a->getY(), a->getZ(), g->getX(),
+            g->getY(), g->getZ(), *temp);
         Thread::sleep(10);
     }
 #endif
 
     // Bye.
-    while(1) { Thread::sleep(1000); }
+    while (1)
+    {
+        Thread::sleep(1000);
+    }
 }
--- src/entrypoints/dsgamma-config.cpp
+++ src/entrypoints/dsgamma-config.cpp
@@ -1,16 +1,16 @@
 /* Copyright (c) 2017 Skyward Experimental Rocketry
  * Authors: Alvise de'Faveri Tron, Nuno Barcellos
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
@@ -20,42 +20,43 @@
  * THE SOFTWARE.
  */
 
-#include <cstdio>
-#include "miosix.h"
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <cstdio>
+#include "miosix.h"
 
 using namespace std;
 using namespace miosix;
 
-//GAMMA868 configuration status
-struct config{
+// GAMMA868 configuration status
+struct config
+{
     int local_addr[3] = {127, 127, 127};
-    int dest_addr[3] = {127, 127, 127};
-    int lora_mode = 6;
-    int lora_pow = 15;
-    int handshake = 0;
-    int baudrate = 4;
+    int dest_addr[3]  = {127, 127, 127};
+    int lora_mode     = 6;
+    int lora_pow      = 15;
+    int handshake     = 0;
+    int baudrate      = 4;
 };
 
-//PIN CONFIG
-typedef Gpio<GPIOG_BASE,13> greenLed;
-typedef Gpio<GPIOG_BASE,14> redLed;
-typedef Gpio<GPIOA_BASE,0> button;
-typedef Gpio<GPIOB_BASE,2> learnSwitch;
-typedef Gpio<GPIOB_BASE,0> learnAck;
+// PIN CONFIG
+typedef Gpio<GPIOG_BASE, 13> greenLed;
+typedef Gpio<GPIOG_BASE, 14> redLed;
+typedef Gpio<GPIOA_BASE, 0> button;
+typedef Gpio<GPIOB_BASE, 2> learnSwitch;
+typedef Gpio<GPIOB_BASE, 0> learnAck;
 
-//Needed to check if learn mode is really active.
-FastMutex learnMutex;          
-ConditionVariable learnCond;    
-int learnMode=0;   
+// Needed to check if learn mode is really active.
+FastMutex learnMutex;
+ConditionVariable learnCond;
+int learnMode = 0;
 
-//Serial port
+// Serial port
 int fd = -1;
 
-//Functions
+// Functions
 int initialize();
 int enterLearnMode();
 void confirmLearnMode(void *arg);
@@ -68,45 +69,53 @@ void waitForOk();
 
 /*
  * GAMMA868 Configuration Software:
- * This software is intended for stm32f429i discovery boards. 
- * 
+ * This software is intended for stm32f429i discovery boards.
+ *
  * Discovery board should be connected as follows
  * - Default USART(default @19200 baud)     Anything with a keyboard.
  * - Auxtty USART(must be @9600 baud)       Gamma868 SERIAL(pin 18: rx, 17:tx)
  * - learnSwitch (see pin config)           Gamma868 LRN SW(pin 5)
  * - learnAck (see pin config)              Gamma868 LRN LED(pin 6)
- * 
+ *
  * Gamma module must be in SERIAL MODEM mode (see documentation).
  */
-int main(){
-    
+int main()
+{
+
     printf("\n----- GAMMA868 CONFIGURATOR -----\n");
-    int init = initialize();                          //Initialize
-    if(init < 0){
+    int init = initialize();  // Initialize
+    if (init < 0)
+    {
         printf("Exiting program.");
         return -1;
     }
-    
-    while(1){
+
+    while (1)
+    {
         printf("Press button to start dsgamma configuration.\n");
         waitForButton();
-        int lrn = enterLearnMode();             //Learn Mode
-        
-        if(lrn < 0){
+        int lrn = enterLearnMode();  // Learn Mode
+
+        if (lrn < 0)
+        {
             printf("Check that the device is correctly connected and retry.\n");
-        }else{
-            //Get current configuration
+        }
+        else
+        {
+            // Get current configuration
             Thread::sleep(100);
             printConfig();
-            
-            //Write new configuration
-            //TODO check values before saving them
+
+            // Write new configuration
+            // TODO check values before saving them
             struct config newConf;
-            
+
             printf("LOCAL ADDRESS (3 bytes, 0-127 each):\n");
-            scanf("%d %d %d", &newConf.local_addr[0], &newConf.local_addr[1], &newConf.local_addr[2]);
+            scanf("%d %d %d", &newConf.local_addr[0], &newConf.local_addr[1],
+                  &newConf.local_addr[2]);
             printf("DESTINATION ADDRESS (3 bytes, 0-127 each):\n");
-            scanf("%d %d %d", &newConf.dest_addr[0], &newConf.dest_addr[1], &newConf.dest_addr[2]); 
+            scanf("%d %d %d", &newConf.dest_addr[0], &newConf.dest_addr[1],
+                  &newConf.dest_addr[2]);
             printf("LORA MODE (1-6):\n");
             scanf("%d", &newConf.lora_mode);
             printf("LORA POWER (0-15):\n");
@@ -116,32 +125,33 @@ int main(){
             printf("BAUDRATE (0-4):\n");
             scanf("%d", &newConf.baudrate);
 
-            writeConfig(newConf);       //TODO catch error
+            writeConfig(newConf);  // TODO catch error
 
-            //Get current configuration
+            // Get current configuration
             Thread::sleep(100);
             printConfig();
 
-            //Wait for button to close learn mode
+            // Wait for button to close learn mode
             printf("Press button to end configuration.\n");
             waitForButton();
-            write(fd,"#Q", 2);
+            write(fd, "#Q", 2);
             printf("Configuration ended.\n\n");
         }
-        
-        //Clean up
-        learnMode=0;
+
+        // Clean up
+        learnMode = 0;
         greenLed::low();
         Thread::sleep(500);
     }
-
 }
 
 /*
- * Configures discovery gpio and serial port to communicate with the gamma868 module.
+ * Configures discovery gpio and serial port to communicate with the gamma868
+ * module.
  */
-int initialize(){
-    //Discovery gpio setup
+int initialize()
+{
+    // Discovery gpio setup
     {
         FastInterruptDisableLock dLock;
         greenLed::mode(Mode::OUTPUT);
@@ -150,16 +160,18 @@ int initialize(){
         learnSwitch::mode(Mode::OUTPUT);
         learnAck::mode(Mode::INPUT);
     }
-    learnSwitch::high();                            //Learn switch is active low
-     
-    //Serial port setup
+    learnSwitch::high();  // Learn switch is active low
+
+    // Serial port setup
     printf("Opening serial port /dev/auxtty on discovery board ... ");
-    fd = open("/dev/auxtty",O_RDWR);
-    if(fd<0){
+    fd = open("/dev/auxtty", O_RDWR);
+    if (fd < 0)
+    {
         printf("Failed!\n");
         return -1;
     }
-    else{
+    else
+    {
         printf("Ok\n");
         return 0;
     }
@@ -168,35 +180,40 @@ int initialize(){
 /*
  * Puts the gamma868 in "learn mode" (configuration mode).
  */
-int enterLearnMode(){
-    //Enter learn mode
+int enterLearnMode()
+{
+    // Enter learn mode
     printf("Entering learn mode ... ");
     fflush(stdout);
     learnSwitch::low();
 
-    //Create learn mode confirmation and timeout thread
+    // Create learn mode confirmation and timeout thread
     Thread *checkLearnModeThread, *timeoutThread;
-    checkLearnModeThread = Thread::create(confirmLearnMode,STACK_MIN);
-    timeoutThread = Thread::create(timer,STACK_MIN);
-    
-    if(checkLearnModeThread == NULL || timeoutThread == NULL){
+    checkLearnModeThread = Thread::create(confirmLearnMode, STACK_MIN);
+    timeoutThread        = Thread::create(timer, STACK_MIN);
+
+    if (checkLearnModeThread == NULL || timeoutThread == NULL)
+    {
         printf("Failed: learn mode control threads not created.\n");
         return -1;
     }
 
-    //Wait for confirm (or timeout)
+    // Wait for confirm (or timeout)
     {
         Lock<FastMutex> l(learnMutex);
-        while(learnMode==0) learnCond.wait(l);
+        while (learnMode == 0)
+            learnCond.wait(l);
     }
-    
-    learnSwitch::high();            //Stop "pushing" the button
-    
-    if(learnMode==-1){
+
+    learnSwitch::high();  // Stop "pushing" the button
+
+    if (learnMode == -1)
+    {
         printf("Failed!\n");
         return -2;
     }
-    else{
+    else
+    {
         printf("Ok\n");
         greenLed::high();
         return 0;
@@ -206,81 +223,88 @@ int enterLearnMode(){
 /*
  * Prints gamma868 configuration.
  */
-void printConfig(){
-    //TODO timeout
+void printConfig()
+{
+    // TODO timeout
     char config[15];
-    write(fd,"#?", 2);
+    write(fd, "#?", 2);
     read(fd, config, 15);
     printf("Current configuration: \n");
-    for (int i = 2; i < 13; i++){
-        printf("%02X ", config[i]);             //Prints hex values
+    for (int i = 2; i < 13; i++)
+    {
+        printf("%02X ", config[i]);  // Prints hex values
     }
     printf("\n");
-    
 }
 
 /*
  * Sends configuration to the gamma868 module.
  */
-int writeConfig(struct config conf){
-    //TODO check values before writing 
-    
+int writeConfig(struct config conf)
+{
+    // TODO check values before writing
+
     char conf_addr[8] = "#A";
-    for(int i = 0; i < 3; i++){
-        conf_addr[2+i] = (char)conf.local_addr[i];
-        conf_addr[5+i] = (char)conf.dest_addr[i];
+    for (int i = 0; i < 3; i++)
+    {
+        conf_addr[2 + i] = (char)conf.local_addr[i];
+        conf_addr[5 + i] = (char)conf.dest_addr[i];
     }
     write(fd, conf_addr, 8);
-    waitForOk(); 
-    
+    waitForOk();
+
     char conf_baud[3] = "#B";
-    conf_baud[2] = (char)conf.baudrate;
+    conf_baud[2]      = (char)conf.baudrate;
     write(fd, conf_baud, 3);
-    waitForOk(); 
-    
+    waitForOk();
+
     char conf_handshake[3] = "#H";
-    conf_handshake[2] = (char)conf.handshake;
+    conf_handshake[2]      = (char)conf.handshake;
     write(fd, conf_handshake, 3);
-    waitForOk(); 
-    
+    waitForOk();
+
     char conf_lora[4] = "#C";
-    conf_lora[2] = (char)conf.lora_mode;
-    conf_lora[3] = (char)conf.lora_pow;
+    conf_lora[2]      = (char)conf.lora_mode;
+    conf_lora[3]      = (char)conf.lora_pow;
     write(fd, conf_lora, 4);
-    waitForOk(); 
-    
+    waitForOk();
+
     return 0;
 }
 
 /*
- * Checks how many times the gamma868 LRN LED flashes: 2 flashes confirm 
+ * Checks how many times the gamma868 LRN LED flashes: 2 flashes confirm
  * that the device has entered learn mode.
  * Runs in a separate Thread.
  */
-void confirmLearnMode(void *arg){
+void confirmLearnMode(void *arg)
+{
     int times = 0;
     int learn = learnAck::value();
-    
-    while(1){
+
+    while (1)
+    {
         Thread::sleep(100);
-        if (learnMode!=0 ) break;           //Breaks if timer has set learnMode to -1
-        
+        if (learnMode != 0)
+            break;  // Breaks if timer has set learnMode to -1
+
         int curState = learnAck::value();
-        if(curState != learn){              //Count how many times the led changes state
+        if (curState != learn)
+        {  // Count how many times the led changes state
             learn = curState;
             times++;
         }
-        
-        if (times == 5){                    //Set learnMode flag to 1 (with mutex).
+
+        if (times == 5)
+        {  // Set learnMode flag to 1 (with mutex).
             {
                 Lock<FastMutex> l(learnMutex);
-                learnMode=1;
+                learnMode = 1;
                 learnCond.signal();
             }
             break;
         }
     }
-    
 }
 
 /*
@@ -288,34 +312,42 @@ void confirmLearnMode(void *arg){
  * signal an error.
  * Runs in a separate Thread.
  */
-void timer(void *arg){
-    for(int i = 0; i < 50; i++){
-        if (learnMode > 0) break;                //If the learnMode confirm arrives, stop the timer.
-        
-        if(i == 49){
+void timer(void *arg)
+{
+    for (int i = 0; i < 50; i++)
+    {
+        if (learnMode > 0)
+            break;  // If the learnMode confirm arrives, stop the timer.
+
+        if (i == 49)
+        {
             {
                 Lock<FastMutex> l(learnMutex);
-                learnMode=-1;
+                learnMode = -1;
                 learnCond.signal();
             }
         }
-        Thread::sleep(100);                     //100ms x 50 cycles = 5 sec
+        Thread::sleep(100);  // 100ms x 50 cycles = 5 sec
     }
 }
 
 /*
  * Waits for the discovery user button to be pressed (blocking).
  */
-void waitForButton(){
-    while(1){
-        if(button::value()==1) break;       //Wait for button
+void waitForButton()
+{
+    while (1)
+    {
+        if (button::value() == 1)
+            break;  // Wait for button
     }
 }
 
 /*
  * Waits until an "OK" is received on the serial port (blocking).
  */
-void waitForOk(){
+void waitForOk()
+{
     char reply[3];
     read(fd, reply, 3);
     printf("%s\n", reply);
