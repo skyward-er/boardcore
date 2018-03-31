f717e8dc47a6bfe4247e755b5ea1926acb6adb94
Luca Erbetta <luca.erbetta105@gmail.com>
2018-03-31 17:46:14 +0200

    Add comment to warn about a possible deadlock

--- src/shared/events/EventBroker.h
+++ src/shared/events/EventBroker.h
@@ -82,6 +82,8 @@ public:
      * Subscribe to a specific topic.
      * Since there is no way to unsubscribe for now, the subscribed FSM
      * MUST NOT be destroyed before the termination of the program.
+     * This function is meant to be called at initialization. DO NOT call it in
+     * response to an event, or it will cause a deadlock.
      * @param subscriber
      * @param topic
      */
