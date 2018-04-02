ae5c561ccb3b5c73a6279a45926e609083db8abd
Alvise De Faveri <alvise.defaveri@skywarder.eu>
2018-04-03 01:42:04 +0200

    [TMTC] Add external config and work on send() and AOs.
    Add external TMTC_config.h to keep all the defines.
    
    Move TMTCManager::send() function to the cpp and make it
    check if there's enough space before adding the message in the buffer.
    
    Work on Sender::run(), add a number of attempts that can be repeated
    when sending each packet and a timeout after every packet.
    
    Add mavlink message type switch-case inside Receiver::run().

--- src/shared/boards/homeone/TMTC/TMTCManager.cpp
+++ src/shared/boards/homeone/TMTC/TMTCManager.cpp
@@ -23,43 +23,89 @@
 
 #include "TMTCManager.h"
 
-/* Run() function of the Receiver: handle incoming commands */
-void Receiver::run(){
+/* 
+ * Run() function of the Receiver:
+ * parse the received packet one byte at a time until you find a complete mavlink
+ * message, then forward the corresponding event.
+ */
+void Receiver::run() {
 	mavlink_message_t msg;
+	mavlink_status_t status;
 	uint8_t byte;
 
 	while(1)
 	{
-		gamma->receive(1, &byte); //Blocking function
+		gamma->receive(&byte, 1); //Blocking function
 
-		if (mavlink_parse_char(MAVLINK_COMM_0, byte, &msg))
+		if (mavlink_parse_char(MAVLINK_COMM_0, byte, &msg, &status))
 		{
 			printf("Received message with ID %d, sequence: %d from component %d of system %d",
-								 msg.msgid, msg.seq, msg.compid, msg.sysid);
+								msg.msgid, msg.seq, msg.compid, msg.sysid);
+
+			switch(msg.msgid) {
+			    case MAVLINK_MSG_ID_PING:
+			    {
+			        mavlink_ping_t ping;
+			        mavlink_msg_ping_decode(&msg ,&ping);
+			        printf("Received ping\n");
+			        break;
+			    }
+			    case MAVLINK_MSG_ID_TEST_MSG:
+			    {
+			        mavlink_test_msg_t ack;
+			        mavlink_msg_test_msg_decode(&msg, &ack);
+			        printf("Received ack\n");
+			        break;
+			    }
+			}
+
 		}
 	}
 }
 
 
-/* Run() function of the Sender: read from the out buffer and write on the driver */
-void Sender::run(){
-   uint8_t msg[100];
+/* 
+ * Run() function of the Sender: 
+ * read from the out buffer and write on the RF serial. 
+ */
+void Sender::run() {
+   uint8_t msgTemp[MAX_PKT_SIZE];
+   uint32_t readBytes;
+   bool sent;
 
-	while(1){
-	    while (outBuffer->size() > 0) 
-	    {
-	        outBuffer->read(msg, outBuffer->size());
-	        printf ("%d\n", msg[0]);
+	while(1) {
+	    if (outBuffer->occupiedSize() > 0) {
+	    	/* Read from the buffer at maximum MAX_PKT_SIZE bytes */
+	        readBytes = outBuffer->read(msgTemp, MAX_PKT_SIZE);
+
+	        /* Try sending the packet for a limited number of times */
+	        for (int i = 0; i < MAX_TRIES_PER_PACKET; i++) {
+	        	sent = gamma->send(msgTemp, readBytes);
+	        	if(sent) break;
+	    	}
 	    }
+	    Thread::sleep(TMTC_SEND_TIMEOUT);
 	}
+
 }
 
 
-/* TMTCManager Constructor: has some memory allocation */
-TMTCManager::TMTCManager(){
+/* TMTCManager Constructor: initialise objects (has memory allocation) */
+TMTCManager::TMTCManager() {
     gamma = new Gamma868("/dev/tty");
     //TODO: check gamma status and configuration
 
     sender = new Sender(gamma);
     receiver = new Receiver(gamma);
+}
+
+/* TMTCManager non-blocking send() function */
+bool TMTCManager::send(uint8_t* msg, uint8_t len) {
+	/* Check if there's enough free space in the Sender's outBuffer */
+    if(sender->outBuffer->freeSize() >= len){
+        sender->outBuffer->write(msg, len);
+        return true;
+    } else {
+        return false;
+    }   
 }
