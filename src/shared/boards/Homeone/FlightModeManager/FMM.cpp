6ec2ab54f4a6b967d573d87379730532ae881cdb
Luca Erbetta <lucaerbetta105@gmail.com>
2018-04-16 23:47:03 +0200

    Updated FlightModeManager with new states and logic

--- src/shared/boards/Homeone/FlightModeManager/FMM.cpp
+++ src/shared/boards/Homeone/FlightModeManager/FMM.cpp
@@ -20,11 +20,11 @@
  * THE SOFTWARE.
  */
 
+#include <boards/Homeone/FlightModeManager/FMM.h>
 #include "events/EventBroker.h"
 
 #include "boards/Homeone/Events.h"
 #include "boards/Homeone/FlightModeManager/FMM_Config.h"
-#include "boards/Homeone/FlightModeManager/FSM.h"
 #include "boards/Homeone/Topics.h"
 
 namespace HomeoneBoard
@@ -48,10 +48,27 @@ void FlightModeManagerFSM::disarmed(const Event& e)
         case EV_EXIT:
             printf("Disarmed state exit\n");
             break;
-        case EV_FMM_ARM:  // Transition to armed
-            printf("EV_FMM_ARM\n");
+
+        // Transition to armed
+        case EV_ARM:
+            printf("EEV_ARM\n");
             transition(&FlightModeManagerFSM::armed);
             break;
+
+        // Transition to testing
+        case EV_ENABLE_TEST_MODE:
+            printf("EV_ENABLE_TEST_MODE\n");
+            transition(&FlightModeManagerFSM::testing);
+            break;
+
+        // Transition to aborted
+        case EV_ABORT_LAUNCH:
+            printf("EV_ABORT_LAUNCH\n");
+            transition(&FlightModeManagerFSM::aborted);
+
+            // TODO: Start abort procedure
+            break;
+
         default:
             printf("Unknown event received.\n");
             break;
@@ -68,14 +85,65 @@ void FlightModeManagerFSM::armed(const Event& e)
         case EV_EXIT:
             printf("Armed state exit\n");
             break;
-        case EV_FMM_DISARM:
-            printf("EV_FMM_DISARM\n");
+
+        // Transition to disarmed
+        case EV_DISARM:
+            printf("EV_DISARM\n");
             transition(&FlightModeManagerFSM::disarmed);
             break;
-        case EV_FMM_LAUNCH:
-            printf("EV_FMM_LAUNCH\n");
+
+        // Transition to ascending
+        case EV_UNBILICAL_DETATCHED:
+            printf("EV_UNBILICAL_DETATCHED\n");
             transition(&FlightModeManagerFSM::ascending);
             break;
+
+        // Transition to aborted
+        case EV_ABORT_LAUNCH:
+            printf("EV_ABORT_LAUNCH\n");
+            transition(&FlightModeManagerFSM::aborted);
+            break;
+
+        default:
+            printf("Unknown event received.\n");
+            break;
+    }
+}
+
+void FlightModeManagerFSM::testing(const Event& e)
+{
+    switch (e.sig)
+    {
+        case EV_ENTRY:
+            printf("Testing state entry\n");
+            break;
+        case EV_EXIT:
+            printf("Testing state exit\n");
+            break;
+
+        // Reset the board
+        case EV_RESET_BOARD:
+            printf("EV_RESET_BOARD\n");
+            // TODO: Reset board
+            break;
+
+        default:
+            printf("Unknown event received.\n");
+            break;
+    }
+}
+
+void FlightModeManagerFSM::aborted(const Event& e)
+{
+    switch (e.sig)
+    {
+        case EV_ENTRY:
+            printf("Aborted state entry\n");
+            break;
+        case EV_EXIT:
+            printf("ERROR: Aborted state exit\n");
+            break;
+
         default:
             printf("Unknown event received.\n");
             break;
@@ -92,12 +160,18 @@ void FlightModeManagerFSM::ascending(const Event& e)
             delayed_event_id = sEventBroker->postDelayed(
                 ev_ascent_timeout, TOPIC_FMM_FSM, ASCENDING_TIMEOUT_MS);
 
-            // TODO: Start ADA in passive mode
+            // Start the ada in passive mode
+            sEventBroker->post(ev_ada_start, TOPIC_FMM_ADA);
             break;
         case EV_EXIT:
             printf("Ascending state exit\n");
+
+            // Always remove delayed events to avoid errors.
             sEventBroker->removeDelayed(delayed_event_id);
+
             break;
+
+        // Transition to apogeeDetection
         case EV_FMM_ASCENT_TIMEOUT:
             printf("EV_FMM_ASCENT_TIMEOUT\n");
             transition(&FlightModeManagerFSM::apogeeDetection);
@@ -116,18 +190,23 @@ void FlightModeManagerFSM::apogeeDetection(const Event& e)
             printf("Apogee state entry\n");
             // State timeout
             delayed_event_id = sEventBroker->postDelayed(
-                ev_apogee, TOPIC_FMM_FSM, APOGEE_DETECTION_TIMEOUT_MS);
+                ev_apogee_detected, TOPIC_FMM_FSM, APOGEE_DETECTION_TIMEOUT_MS);
 
-            // TODO: ADA active mode
+            // ADA active mode
+            sEventBroker->post(ev_ada_active_mode, TOPIC_FMM_ADA);
             break;
         case EV_EXIT:
             printf("Apogee state exit\n");
-            // TODO: Stop ADA
+
+            // Stop the ada
+            sEventBroker->post(ev_ada_stop, TOPIC_FMM_ADA);
 
             sEventBroker->removeDelayed(delayed_event_id);
             break;
-        case EV_FMM_APOGEE:
-            printf("EV_FMM_APOGEE\n");
+
+        // Transition to descendingPhase_1
+        case EV_APOGEE_DETECTED:
+            printf("EV_APOGEE_DETECTED\n");
             transition(&FlightModeManagerFSM::descendingPhase_1);
             break;
         default:
@@ -151,15 +230,17 @@ void FlightModeManagerFSM::descendingPhase_1(const Event& e)
             break;
         case EV_EXIT:
             printf("Descending_1 state exit\n");
-            // TODO: Activate thermocutters
             // TODO: Stop altitude detection
 
             sEventBroker->removeDelayed(delayed_event_id);
             break;
-        case EV_FMM_MAIN_PARACHUTE_DEPLOY:
+
+        // Transition to descendingPhase_2
+        case EV_DEPLOY_MAIN_PARACHUTE:
             printf("EV_FMM_MAIN_PARACHUTE_DEPLOY\n");
             transition(&FlightModeManagerFSM::descendingPhase_2);
             break;
+
         default:
             printf("Unknown event received.\n");
             break;
@@ -176,6 +257,34 @@ void FlightModeManagerFSM::descendingPhase_2(const Event& e)
         case EV_EXIT:
             printf("Descending_2 state exit\n");
             break;
+
+        // Transition to landed
+        case EV_STOP_SAMPLING:
+            printf("Descending_2 state exit\n");
+
+            // Post the event on TOPIC_SAMPLING
+            sEventBroker->post(e, TOPIC_SAMPLING);
+
+            transition(&FlightModeManagerFSM::landed);
+            break;
+
+        default:
+            printf("Unknown event received.\n");
+            break;
+    }
+}
+
+void FlightModeManagerFSM::landed(const Event& e)
+{
+    switch (e.sig)
+    {
+        case EV_ENTRY:
+            printf("Landed state entry\n");
+            break;
+        case EV_EXIT:
+            printf("ERROR: Landed state exit\n");
+            break;
+
         default:
             printf("Unknown event received.\n");
             break;
