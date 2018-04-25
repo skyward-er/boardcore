ad560bead20c7f34753b3349f2a6d37beda18a32
Terraneo Federico <fede.tft@miosix.org>
2018-04-25 15:19:54 +0200

    [LOG] Print true class name, add better formatting

--- src/shared/logger/LogBase.cpp
+++ src/shared/logger/LogBase.cpp
@@ -18,7 +18,7 @@ LogBase::LogBase() : timestamp(0) {}
 
 void LogBase::print(ostream& os) const
 {
-    os<<"type="<<typeid(this).name()<<" timestamp="<<timestamp<<' ';
+    os<<"type="<<typeid(*this).name()<<" timestamp="<<timestamp<<' ';
 }
 
 LogBase::~LogBase() {}
@@ -33,13 +33,13 @@ void LogStats::print(ostream& os) const
 {
     LogBase::print(os);
     os<<"ls: "<<statTooLargeSamples
-      <<"ds: "<<statDroppedSamples
-      <<"qs: "<<statQueuedSamples
-      <<"bf: "<<statBufferFilled
-      <<"bw: "<<statBufferWritten
-      <<"wf: "<<statWriteFailed
-      <<"wt: "<<statWriteTime
-      <<"mwt: "<<statMaxWriteTime;
+      <<" ds: "<<statDroppedSamples
+      <<" qs: "<<statQueuedSamples
+      <<" bf: "<<statBufferFilled
+      <<" bw: "<<statBufferWritten
+      <<" wf: "<<statWriteFailed
+      <<" wt: "<<statWriteTime
+      <<" mwt: "<<statMaxWriteTime;
 }
 
 CEREAL_REGISTER_TYPE(LogStats);
