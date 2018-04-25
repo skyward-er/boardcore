95f9da4fa8b5e709bb08a020f73c3f28bd7b349c
Federico Terraneo <fede.tft@miosix.org>
2018-04-25 15:29:24 +0200

    [LOG] Demangle C++ class names

--- src/shared/logger/LogBase.cpp
+++ src/shared/logger/LogBase.cpp
@@ -2,6 +2,8 @@
 #include "LogBase.h"
 #ifdef _MIOSIX
 #include <miosix.h>
+#else //_MIOSIX
+#include <cxxabi.h>
 #endif //_MIOSIX
 
 using namespace std;
@@ -18,7 +20,16 @@ LogBase::LogBase() : timestamp(0) {}
 
 void LogBase::print(ostream& os) const
 {
+    #ifdef _MIOSIX
     os<<"type="<<typeid(*this).name()<<" timestamp="<<timestamp<<' ';
+    #else //_MIOSIX
+    const char *name=typeid(*this).name();
+    int status;
+    char *demangled=abi::__cxa_demangle(name,NULL,0,&status);
+    if(status==0 && demangled) name=demangled;
+    os<<"type="<<name<<" timestamp="<<timestamp<<' ';
+    if(demangled) free(demangled);
+    #endif //_MIOSIX
 }
 
 LogBase::~LogBase() {}
@@ -32,14 +43,14 @@ LogStats::LogStats() {}
 void LogStats::print(ostream& os) const
 {
     LogBase::print(os);
-    os<<"ls: "<<statTooLargeSamples
-      <<" ds: "<<statDroppedSamples
-      <<" qs: "<<statQueuedSamples
-      <<" bf: "<<statBufferFilled
-      <<" bw: "<<statBufferWritten
-      <<" wf: "<<statWriteFailed
-      <<" wt: "<<statWriteTime
-      <<" mwt: "<<statMaxWriteTime;
+    os<<"ls="<<statTooLargeSamples
+      <<" ds="<<statDroppedSamples
+      <<" qs="<<statQueuedSamples
+      <<" bf="<<statBufferFilled
+      <<" bw="<<statBufferWritten
+      <<" wf="<<statWriteFailed
+      <<" wt="<<statWriteTime
+      <<" mwt="<<statMaxWriteTime;
 }
 
 CEREAL_REGISTER_TYPE(LogStats);
