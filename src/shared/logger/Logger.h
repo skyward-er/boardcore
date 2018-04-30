a3d314db386ec644da582cee2906a8afc2d0cf0b
Federico Terraneo <fede.tft@miosix.org>
2018-04-30 16:29:52 +0200

    [LOG] Implemented alternative to cereal, added copyrights

--- src/shared/logger/Logger.h
+++ src/shared/logger/Logger.h
@@ -1,3 +1,29 @@
+/***************************************************************************
+ *   Copyright (C) 2018 by Terraneo Federico                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   As a special exception, if other files instantiate templates or use   *
+ *   macros or inline functions from this file, or you compile this file   *
+ *   and link it with other works to produce a work based on this file,    *
+ *   this file does not by itself cause the resulting work to be covered   *
+ *   by the GNU General Public License. However the source code for this   *
+ *   file must still be made available in accordance with the GNU General  *
+ *   Public License. This exception does not invalidate any other reasons  *
+ *   why a work based on this file might be covered by the GNU General     *
+ *   Public License.                                                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
+ ***************************************************************************/ 
 
 #pragma once
 
@@ -5,6 +31,7 @@
 #include <cstdio>
 #include <list>
 #include <queue>
+#include <type_traits>
 #include "LogBase.h"
 
 /**
@@ -57,15 +84,34 @@ public:
      */
     bool isStarted() const { return started; }
 
+#ifdef USE_CEREAL
+
     /**
      * Nonblocking call. Call this function to log a class.
-     * \param lb The class to be logged, read the LogBase documentation for
+     * \param lb The class to be logged, read the LogBase.h documentation for
      * requirements.
      * \return whether the class has been logged
      *
      * \throws cereal exceptions
      */
     LogResult log(const LogBase &lb);
+    
+#else //USE_CEREAL
+
+    /**
+     * Nonblocking call. Call this function to log a class.
+     * \param lb The class to be logged, read the LogBase.h documentation for
+     * requirements.
+     * \return whether the class has been logged
+     */
+    template<typename T>
+    LogResult log(const T& t)
+    {
+        //static_assert(std::is_trivially_copyable<T>::value);
+        return logImpl(typeid(t).name(),&t,sizeof(t));
+    }
+
+#endif //USE_CEREAL
 
 private:
     Logger();
@@ -75,6 +121,18 @@ private:
     static void packThreadLauncher(void *argv);
     static void writeThreadLauncher(void *argv);
     static void statsThreadLauncher(void *argv);
+    
+#ifndef USE_CEREAL
+    
+    /**
+     * Non-template dependente part of log
+     * \param name class anem
+     * \param data pointer to class data
+     * \param size class size
+     */
+    LogResult logImpl(const char *name, const void *data, unsigned int size);
+    
+#endif //USE_CEREAL
 
     /**
      * This thread packs logged data into buffers
