2f4728dd9ff1123b1587ee7314bfb6c9bc7ed40d
Luca Erbetta <lucaerbetta105@gmail.com>
2018-11-01 20:33:28 +0100

    Updated state machine. Now with state transitions, event generations and build errors!

--- src/shared/boards/Homeone/FlightModeManager/FlightModeManager.cpp
+++ src/shared/boards/Homeone/FlightModeManager/FlightModeManager.cpp
@@ -53,6 +53,13 @@ void FlightModeManager::stateInit(const Event& ev)
             TRACE("FMM: Exiting stateInit\n");
             break;
 
+        case EV_INIT_ERROR:
+            transition(&FlightModeManager::stateError);
+            break;
+        case EV_INIT_OK:
+            transition(&FlightModeManager::stateDisarmed);
+            break;
+
         default:
             TRACE("stateInit: Event %d not handled.\n", ev.sig);
             break;
@@ -70,6 +77,10 @@ void FlightModeManager::stateTesting(const Event& ev)
             break;
         case EV_EXIT:
             TRACE("FMM: Exiting stateTesting\n");
+            break;
+
+        case EV_BOARD_RESET_TC:
+            // TODO: Reboot the board
             break;
 
         default:
@@ -91,27 +102,12 @@ void FlightModeManager::stateError(const Event& ev)
             TRACE("FMM: Exiting stateError\n");
             break;
 
+        case EV_BOARD_RESET_TC:
+            // TODO: Reboot the board
+            break;
+
         default:
             TRACE("stateError: Event %d not handled.\n", ev.sig);
-            break;
-    }
-}
-
-void FlightModeManager::stateAborted(const Event& ev)
-{
-    switch (ev.sig)
-    {
-        case EV_ENTRY:
-            TRACE("FMM: Entering stateAborted\n");
-            status.state = FMMState::ABORTED;
-            logger.log(status);
-            break;
-        case EV_EXIT:
-            TRACE("FMM: Exiting stateAborted\n");
-            break;
-
-        default:
-            TRACE("stateAborted: Event %d not handled.\n", ev.sig);
             break;
     }
 }
@@ -129,6 +125,20 @@ void FlightModeManager::stateDisarmed(const Event& ev)
             TRACE("FMM: Exiting stateDisarmed\n");
             break;
 
+        case EV_TEST_MODE_TC:
+            transition(&FlightModeManager::stateTesting);
+            break;
+
+        case EV_NC_OFFLINE:
+        case EV_IGN_OFFLINE:
+        case EV_IGN_ABORTED:
+            transition(&FlightModeManager::stateError);
+            break;
+
+        case EV_ARM_TC:
+            transition(&FlightModeManager::stateArmed);
+            break;
+
         default:
             TRACE("stateDisarmed: Event %d not handled.\n", ev.sig);
             break;
@@ -143,9 +153,34 @@ void FlightModeManager::stateArmed(const Event& ev)
             TRACE("FMM: Entering stateArmed\n");
             status.state = FMMState::ARMED;
             logger.log(status);
+
+            // Notify everyone that we are armed.
+            sEventBroker->post({EV_ARMED}, TOPIC_FLIGHT_EVENTS);
+
+            // Automatically disarm after TIMEOUT_MS_AUTO_DISARM milliseconds.
+            delayed_event_id = sEventBroker->postDelayed(
+                {EV_TIMEOUT_ARM}, TOPIC_FMM, TIMEOUT_MS_AUTO_DISARM);
             break;
         case EV_EXIT:
             TRACE("FMM: Exiting stateArmed\n");
+
+            // Remove disarm timeout
+            sEventBroker->removeDelayed(delayed_event_id);
+            break;
+
+        case EV_NC_OFFLINE:
+        case EV_IGN_OFFLINE:
+        case EV_IGN_ABORTED:
+            transition(&FlightModeManager::stateError);
+            break;
+
+        case EV_DISARM_TC:
+        case EV_TIMEOUT_ARM:
+            transition(&FlightModeManager::stateDisarmed);
+            break;
+
+        case EV_LAUNCH_TC:
+            transition(&FlightModeManager::stateLaunching);
             break;
 
         default:
@@ -167,6 +202,18 @@ void FlightModeManager::stateLaunching(const Event& ev)
             TRACE("FMM: Exiting stateLaunching\n");
             break;
 
+        case EV_UMBILICAL_DETACHED:
+            transition(&FlightModeManager::stateAscending);
+            break;
+
+        case EV_DISARM_TC:
+            transition(&FlightModeManager::stateDisarmed);
+            break;
+
+        case EV_IGN_ABORTED:
+            transition(&FlightModeManager::stateError);
+            break;
+
         default:
             TRACE("stateLaunching: Event %d not handled.\n", ev.sig);
             break;
@@ -181,9 +228,25 @@ void FlightModeManager::stateAscending(const Event& ev)
             TRACE("FMM: Entering stateAscending\n");
             status.state = FMMState::ASCENDING;
             logger.log(status);
+
+            // Notify liftoff
+            sEventBroker->post({EV_LIFTOFF}, TOPIC_FLIGHT_EVENTS);
+
+            // Set apogee timeout
+            delayed_event_id = sEventBroker->postDelayed(
+                {EV_TIMEOUT_APOGEE}, TOPIC_FMM, TIMEOUT_MS_APOGEE_DETECTION);
+
             break;
         case EV_EXIT:
             TRACE("FMM: Exiting stateAscending\n");
+
+            // Remove apogee timeout
+            sEventBroker->removeDelayed(delayed_event_id);
+            break;
+
+        case EV_ADA_APOGEE_DETECTED:
+        case EV_TIMEOUT_APOGEE:
+            transition(&FlightModeManager::stateFirstDescentPhase);
             break;
 
         default:
@@ -200,9 +263,28 @@ void FlightModeManager::stateFirstDescentPhase(const Event& ev)
             TRACE("FMM: Entering stateFirstDescentPhase\n");
             status.state = FMMState::FIRST_DESCENT_PHASE;
             logger.log(status);
+
+            // Generate apogee event
+            sEventBroker->post({EV_APOGEE}, TOPIC_FLIGHT_EVENTS);
+
+            // Set deployment timeut
+            delayed_event_id = sEventBroker->postDelayed(
+                {EV_TIMEOUT_DPL_ALT}, TOPIC_FMM, TIMEOUT_MS_DPL_ALTITUDE);
             break;
         case EV_EXIT:
             TRACE("FMM: Exiting stateFirstDescentPhase\n");
+
+            // Remove dpl timeout
+            sEventBroker->removeDelayed(delayed_event_id);
+            break;
+
+        case EV_ADA_DPL_ALT_DETECTED:
+        case EV_TIMEOUT_DPL_ALT:
+            transition(&FlightModeManager::stateSecondDescentPhase);
+            break;
+
+        case EV_MANUAL_MODE_TC:
+            transition(&FlightModeManager::stateManualDescent);
             break;
 
         default:
@@ -219,9 +301,24 @@ void FlightModeManager::stateSecondDescentPhase(const Event& ev)
             TRACE("FMM: Entering stateSecondDescentPhase\n");
             status.state = FMMState::SECOND_DESCENT_PHASE;
             logger.log(status);
+
+            // Notify deploymnet altitude
+            sEventBroker->post({EV_DPL_ALTITUDE}, TOPIC_FLIGHT_EVENTS);
+
+            // Set landing timeout
+            delayed_event_id = sEventBroker->postDelayed(
+                {EV_TIMEOUT_END_MISSION}, TOPIC_FMM, TIMEOUT_MS_END_MISSION);
             break;
         case EV_EXIT:
             TRACE("FMM: Exiting stateSecondDescentPhase\n");
+
+            // Remove end mission timeout
+            sEventBroker->removeDelayed(delayed_event_id);
+            break;
+
+        case EV_END_MISSION_TC:
+        case EV_TIMEOUT_END_MISSION:
+            transition(&FlightModeManager::stateLanded);
             break;
 
         default:
@@ -238,9 +335,18 @@ void FlightModeManager::stateManualDescent(const Event& ev)
             TRACE("FMM: Entering stateManualDescent\n");
             status.state = FMMState::MANUAL_DESCENT;
             logger.log(status);
+
+            // Set landing timeout
+            delayed_event_id = sEventBroker->postDelayed(
+                {EV_TIMEOUT_END_MISSION}, TOPIC_FMM, TIMEOUT_MS_END_MISSION);
             break;
         case EV_EXIT:
             TRACE("FMM: Exiting stateManualDescent\n");
+            break;
+
+        case EV_END_MISSION_TC:
+        case EV_TIMEOUT_END_MISSION:
+            transition(&FlightModeManager::stateLanded);
             break;
 
         default:
@@ -257,6 +363,9 @@ void FlightModeManager::stateLanded(const Event& ev)
             TRACE("FMM: Entering stateLanded\n");
             status.state = FMMState::LANDED;
             logger.log(status);
+
+            // Notify end of mission
+            sEventBroker->post({EV_LANDED}, TOPIC_FLIGHT_EVENTS);
             break;
         case EV_EXIT:
             TRACE("FMM: Exiting stateLanded\n");
