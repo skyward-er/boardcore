26d90b95e23704034cae919f120dae8e21d0c5b6
Matteo Piazzolla <matteo.piazzolla@gmail.com>
2018-11-17 10:40:17 +0100

    modified linter.sh to return an exit code on important checks, added cppcheck suppression list file to ingore cppcheck on tests

--- scripts/linter.sh
+++ scripts/linter.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+declare -i RESULT=0
+
 function check {
     B=$(eval "$3")
     A=$?
@@ -14,17 +16,23 @@ function check {
         echo -e "FAIL\n------------ OUTPUT ------------"
         echo "$B"
         echo "--------------------------------"
+        return 1
     else
         echo "OK"
+        return 0
     fi;
 }
 
-check 0 "Checking for files with lines longer than 80 characters" 'egrep -r ".{81}" src/' 
+#check 0 "Checking for files with lines longer than 80 characters" 'egrep --include="*.cpp" --include="*.h" -nr ".{81}" src/' 
+check 0 "Checking for files with lines longer than 120 characters" 'egrep --include="*.cpp" --include="*.h" -nr ".{121}" src/' 
+RESULT+=$?
 check OUT "Checking for files having \n\n\n" "grep -HPcrz '(\\r?\\n){4,}' src | egrep -v ':0$' | cut -d ':' -f 1"
 check OUT "Checking for files not having the copyright" "grep -rL '* Permission is hereby granted, free of charge' src/"
 check OUT "Checking for tabulations instead of spaces" "grep -Pr '\t' src"
 check OUT "MMP wants his full name" "grep -rl 'Matteo Piazzolla' src/"
-check OUT "Launching cppcheck" "cppcheck -q --suppress=unusedFunction --suppress=missingInclude --std=c++11 --enable=all src/ 2>&1"
+check OUT "Launching cppcheck" "cppcheck  --suppressions-list=./scripts/cppcheck_suppressions_list.txt --template=gcc -q --suppress=unusedFunction --suppress=missingInclude --std=c++11 --enable=all src/ 2>&1"
+RESULT+=$?
 check OUT "Checking for using namespace in header files" "grep -rl 'using namespace' src | egrep '.h(pp)?$'"
+RESULT+=$?
 
 exit 0
