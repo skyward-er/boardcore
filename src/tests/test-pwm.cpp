291314b6598fd9292437d9807e6df967f304c955
Luca Erbetta <luca.erbetta@skywarder.eu>
2018-11-23 21:30:36 +0100

    [PWM] Refactor PWM class

--- src/tests/test-pwm.cpp
+++ src/tests/test-pwm.cpp
@@ -1,20 +1,41 @@
-#include "drivers/pwm/pwm.h"
 #include "Common.h"
-
+#include "drivers/HardwareTimer.h"
+#include "drivers/pwm/pwm.h"
 
 using namespace miosix;
 
+typedef Gpio<GPIOD_BASE, 12> pwmOut;   // ch1
+typedef Gpio<GPIOD_BASE, 13> pwmOut2;  // ch2
+
 int main()
 {
-	Pwm* pwm = new Pwm(1, 0.9);
+    {
+        FastInterruptDisableLock dLock;
 
-	pwm->configure(1, 1);
-	pwm->start();
+        pwmOut::alternateFunction(2);
+        pwmOut::mode(Mode::ALTERNATE);
+    }
 
-    while(1)
+    PWM::Timer t{
+        TIM4, &(RCC->APB1ENR), RCC_APB1ENR_TIM4EN,
+        TimerUtils::getPrescalerInputFrequency(TimerUtils::InputClock::APB1)};
+
+    PWM pwm{t};
+
+    pwm.enable(0, 1);
+
+    pwm.start();
+
+    Thread::sleep(20000);
+    pwm.setDutyCycle(0.1);
+    Thread::sleep(20000);
+    pwm.stop();
+    pwm.disable();
+
+    while (1)
     {
         printf("End\n");
-        Thread::sleep(1000);
+        Thread::sleep(10000);
     }
 
     return 0;
