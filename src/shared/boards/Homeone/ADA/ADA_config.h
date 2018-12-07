f3981ab53b07da8cf6169902f063fc900f316e26
Luca Mozzarelli <lucamozzarelli@gmail.com>
2018-12-07 16:59:43 +0100

    [ADA] Added ADA FMM (incomplete)

---
+++ src/shared/boards/Homeone/ADA/ADA_config.h
@@ -0,0 +1,45 @@
+/* Copyright (c) 2018 Skyward Experimental Rocketry
+ * Authors: Luca Mozzarelli
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#warning "ADA COSTANTS ARE ONLY PLACEHOLDER VALUES"
+
+namespace HomeoneBoard
+{
+namespace FMM
+{
+// TODO: Change with real values
+
+// State timeouts
+static const unsigned int TIMEOUT_MS_CALIBRATION      = 15 * 1000;
+static const unsigned int DESCENT_DPL_ALTITUDE        = 5000;
+
+
+// Kalman parameters
+float P_data[9] = {0.1, 0, 0, 0, 0.1, 0, 0, 0, 0.1}; // Initial error covariance matrix
+
+float R_data[1] = {10}; // Measurement variance  
+
+float Q_data[9] = {0.01, 0, 0, 0, 0.01, 0, 0, 0, 0.01}; // Model variance matrix
+
+float samplingPeriod = 0.01; // In seconds
+}
+}
