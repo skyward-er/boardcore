fec0d62742b14a730c9deb9d9ce3046826268142
AlviseDeFaveri <elviside@gmail.com>
2018-12-14 20:15:58 +0100

    [IGN] Added ignition board

---
+++ src/shared/boards/Ignition/IgnManager.cpp
@@ -0,0 +1,57 @@
+/* Copyright (c) 2015-2019 Skyward Experimental Rocketry
+ * Authors: Benedetta Margrethe Cattani
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#ifndef SRC_SHARED_BOARDS_IGNITION_IGNMANAGER_H
+#define SRC_SHARED_BOARDS_IGNITION_IGNMANAGER_H
+
+#include "Singleton.h"
+#include "MotorDriver.h"
+
+#include "events/Event.h"
+#include "Events.h"
+#include "events/FSM.h"
+
+namespace IgnBoard
+{
+/**
+ * Implementation of the Ignition Board logic.
+ */
+class IgnitionManager : public FSM<IgnitionManager>,
+{
+
+public:
+    NoseconeManager();
+    ~NoseconeManager() {}
+
+private:
+    // States declarations
+    void state_close(const Event& e);
+    void state_opening(const Event& e);
+    void state_open(const Event& e);
+    void state_closing(const Event& e);
+
+    MotorDriver driver;
+
+};
+
+}
+
+#endif /* SRC_SHARED_BOARDS_IGNITION_IGNMANAGER_H */
