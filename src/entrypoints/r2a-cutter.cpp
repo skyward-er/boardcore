f2b83978e26753f58f1dbfe318ed1050f24858ee
Luca Erbetta <luca.erbetta105@gmail.com>
2018-12-22 18:35:18 +0100

    [CUT] R2A cutter test entrypoint

--- src/entrypoints/r2a-cutter.cpp
+++ src/entrypoints/r2a-cutter.cpp
@@ -22,56 +22,101 @@
  */
 
 #include <miosix.h>
+
+#include "ActiveObject.h"
+#include "drivers/adc/ADC.h"
+
 #include "boards/Homeone/DeploymentController/ThermalCutter/Cutter.h"
 
 using namespace HomeoneBoard;
+using namespace miosix;
 
-typedef miosix::Gpio<GPIOG_BASE, 11> btn;
+typedef miosix::Gpio<GPIOA_BASE, 0> btn;
+typedef miosix::Gpio<GPIOA_BASE, 5> csense;
 
-static const int BUTTON_SLEEP = 10;
+static const int BUTTON_SLEEP  = 10;
+static const int SAMPLE_FREQ   = 10;
+static const int SAMPLE_PERIOD = 1000 / SAMPLE_FREQ;
 
 using miosix::Thread;
+
+using ADC_t = SensorADC<1, 5, csense>;
+float adcToI(uint16_t adc_in)
+{
+    float v    = (adc_in * 3.3f) / 4096;
+    float iout = v / 525;
+    return (iout - 0.000030) * 10000;
+}
 
 void awaitButton(int time)
 {
     int pressed_for = 0;
+
+    // Wait until button released
+    while (btn::value() == 1)
+    {
+        miosix::Thread::sleep(BUTTON_SLEEP);
+    }
+
     do
     {
-        if (btn::value() == 0)
-        {   
-            pressed_for += BUTTON_SLEEP;        
+        if (btn::value() == 1)
+        {
+            pressed_for += BUTTON_SLEEP;
         }
         else
         {
+            if (pressed_for >= time)
+            {
+                break;
+            }
             pressed_for = 0;
         }
-        miosix::Thread::sleep(BUTTON_SLEEP);
-    } while (pressed_for < time);
+        Thread::sleep(BUTTON_SLEEP);
+    } while (true);
 }
 
+class Runner : public ActiveObject
+{
+protected:
+    void run() override
+    {
+        Cutter cutter;
+
+        printf("Press the button for 1 second to enable the cutter\n");
+
+        awaitButton(1000);
+
+        printf("Cutter enabled\n");
+        miosix::ledOn();
+        cutter.startCutDrogue();  // TIM4-CH1
+
+        awaitButton(50);
+        printf("Cutter disabled\n");
+        cutter.stopCutDrogue();
+        miosix::ledOff();
+    }
+};
 int main()
 {
+    // csense::mode(Mode::INPUT_ANALOG);
+
+    ADC_t adc(SAMPLE_FREQ);
+    adc.init();
+
     miosix::ledOff();
     btn::mode(miosix::Mode::INPUT);
-    Cutter cutter;
 
-    printf("Press the button for 3 seconds to enable the cutter\n");
+    Runner r;
+    r.start();
 
-    awaitButton(3000);
-
-    printf("Cutter enabled\n");
-    miosix::ledOn();
-    cutter.startCutDrogue();  // TIM4-CH1
-
-    Thread::sleep(5000);
-
-    awaitButton(50);
-    printf("Cutter disabled\n");
-    cutter.stopCutDrogue();
-    miosix::ledOff();
-    for(;;)
+    for (;;)
     {
-        printf("End\n");
-        Thread::sleep(10000);
+        adc.updateParams();
+        uint16_t adcval = adc.getValue();
+        float ival = adcToI(adcval);
+
+        printf("%f,\t%d\n", ival, (int)adcval);
+        Thread::sleep(SAMPLE_PERIOD);
     }
 }
