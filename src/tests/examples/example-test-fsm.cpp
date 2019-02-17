221ddf0755cb874e19160234fd644a44897808e5
Luca Erbetta <luca.erbetta@skywarder.eu>
2019-02-17 15:37:44 +0100

    [Examples] Added state machine testing examples & helpers

--- src/tests/examples/example-test-fsm.cpp
+++ src/tests/examples/example-test-fsm.cpp
@@ -30,17 +30,17 @@
 #include "catch1-tests-entry.cpp"
 #endif
 
-
 // We need access to the handleEvent(...) function in state machines in order to
 // test them synchronously
 #define protected public
 
-#include <catch.hpp>
 #include <miosix.h>
+#include <catch.hpp>
 
 #include "example-test-fsm.h"
 #include "state_machine_test_helper.h"
 
+using miosix::Thread;
 
 TEST_CASE("Testing S1 transitions")
 {
@@ -54,7 +54,6 @@ TEST_CASE("Testing S1 transitions")
     SECTION("S1 -> EV_A -> S4 if we go to S3 before")
     {
         // Go to S3 and then back to S1
-
         REQUIRE(testFSMTransition(fsm, Event{EV_A}, &FSMExample::state_S2));
         REQUIRE(testFSMTransition(fsm, Event{EV_C}, &FSMExample::state_S3));
         REQUIRE(testFSMTransition(fsm, Event{EV_D}, &FSMExample::state_S1));
@@ -121,8 +120,69 @@ TEST_CASE("Testing S4 transitions")
         REQUIRE(testFSMTransition(fsm, Event{EV_D}, &FSMExample::state_S4));
     }
 
-    SECTION("S4 -> EV_A -> S1 (FAIL)")  // This will fail
+    SECTION("S4 -> EV_A -> S1 (Should not happen)")
     {
-        REQUIRE(testFSMTransition(fsm, Event{EV_A}, &FSMExample::state_S1));
+        REQUIRE_FALSE(
+            testFSMTransition(fsm, Event{EV_A}, &FSMExample::state_S1));
+    }
+}
+
+/**
+ * @brief Test fixture to perform creation / termination of the FSMExample
+ * thread, RAII style
+ */
+class FSMTestFixture
+{
+public:
+    // Start the thread in the constructor
+    FSMTestFixture()
+    {
+        sEventBroker->start();
+        fsm.start();
+    }
+    // Stop the thread in the destructor
+    ~FSMTestFixture() { fsm.stop(); }
+
+protected:
+    FSMExample fsm;
+};
+
+// Incredibly, this method is part of a class derived from the "FSMTestFixture"
+// class. So it can access all its protected members. For each  section in this
+// method, the class is created and destroyed, so all sections start exactly at
+// the same state.
+TEST_CASE_METHOD(FSMTestFixture, "Testing async transitions")
+{
+    REQUIRE(testFSMAsyncTransition(fsm, Event{EV_A}, TOPIC_T1,
+                                   &FSMExample::state_S2));
+
+    REQUIRE(testFSMAsyncTransition(fsm, Event{EV_C}, TOPIC_T1,
+                                   &FSMExample::state_S3));
+
+    SECTION("Automatic transition")
+    {
+        // Should automatically move to S1 after 1 second. Sleep 1 ms more just
+        // to be sure
+        Thread::sleep(1001);
+        REQUIRE(fsm.testState(&FSMExample::state_S1));
+
+        // Now EV_A should bring us to S4
+        REQUIRE(testFSMAsyncTransition(fsm, Event{EV_A}, TOPIC_T1,
+                                       &FSMExample::state_S4));
+    }
+
+    SECTION("Delayed event must be removed if a transition occurs earlier")
+    {
+        EventCounter counter(*sEventBroker);
+
+        // Move to S1 before the automatic transition
+        REQUIRE(testFSMAsyncTransition(fsm, Event{EV_D}, TOPIC_T1,
+                                       &FSMExample::state_S1));
+        counter.subscribe(TOPIC_T1);
+
+        Thread::sleep(1001);
+
+        // The delayed event should not have been sent
+        REQUIRE(counter.getCount(EV_D) == 0);
     }
 }
