82ae520c4f97f8b12adc4f5fa17f777f97d79869
Luca Mozzarelli <lucamozzarelli@gmail.com>
2019-03-20 11:31:13 +0100

    [ADA] Converted matrix testing to new class

--- src/tests/catch1/test-matrix.cpp
+++ src/tests/catch1/test-matrix.cpp
@@ -25,58 +25,26 @@
 #endif
 
 #include  <catch.hpp>
-#include "kalman/Matrix.h"
+#include "kalman/matrix.h"
 
 
-/** TESTS LIST
- * Constructors             ?
- * Static: eye              DONE
- * Static: multiply         DONE  
- * Static: sum              DONE
- * Static: subtract         DONE
- * Static: transpose        DONE
- * Static: inverse          DONE
- * Static: minor            DONE
- * Static: cofactorMatrix   DONE
- * Static: determinant      DONE
- * Static: LU decomposition DONE
- * set
-*/
 TEST_CASE("Multiply test")
 {
-    float dataA[] = { 1.0f, -2.0f, 3.0f, 4.5f, 5.0f, -6.0f, 7.0f, 8.0f, -9.0f };
-    Matrix A{3,3, dataA};
-    float dataB[] = { 1.0f, 2.0f, 3.7f};
-    Matrix B{3,1, dataB};
+    MatrixBase<float, 3, 3> A{1.0f, -2.0f, 3.0f, 4.5f, 5.0f, -6.0f, 7.0f, 8.0f, -9.0f};
+    MatrixBase<float, 3, 1> B{1.0f, 2.0f, 3.7f};
+    MatrixBase<float, 3, 1> C{};
 
-    Matrix C{3,1};
-    REQUIRE( Matrix::multiply(A, B, C) == true );
-
-    REQUIRE( C.rows    == 3 );
-    REQUIRE( C.columns == 1 );
-
-    REQUIRE( C(0) == Approx( 8.1f)  );
-    REQUIRE( C(1) == Approx(-7.7f)  );
-    REQUIRE( C(2) == Approx(-10.3f) );
-
-    // Wrong dimensions
-    Matrix D{2,2};
-    REQUIRE( Matrix::multiply(A, D, A) == false );
-    REQUIRE( Matrix::multiply(B, B, B) == false );
+    REQUIRE( C(0,0) == Approx( 8.1f)  );
+    REQUIRE( C(1,0) == Approx(-7.7f)  );
+    REQUIRE( C(2,0) == Approx(-10.3f) );
 }
 
 TEST_CASE("Sum test")
 {
-    float dataA[] = { 1.0f, -2.0f, 3.0f, 4.5f, 5.0f, -6.0f, 7.0f, 8.0f, -9.0f };
-    Matrix A{3,3, dataA};
-    float dataB[] = { 1.0f, 2.0f, 3.0f, 5.0f, 7.0f, 9.0f, 11.0f, 13.7f, 15.0f};
-    Matrix B{3,3, dataB};
+    MatrixBase<float, 3,3> A{1.0f, -2.0f, 3.0f, 4.5f, 5.0f, -6.0f, 7.0f, 8.0f, -9.0f};
+    MatrixBase<float, 3, 3> B{ 1.0f, 2.0f, 3.0f, 5.0f, 7.0f, 9.0f, 11.0f, 13.7f, 15.0f};
 
-    Matrix C{A.rows, A.columns};
-    REQUIRE( Matrix::sum(A, B, C) == true );
-
-    REQUIRE( C.rows    == 3 );
-    REQUIRE( C.columns == 3 );
+    MatrixBase<float, 3, 3> C{};
 
     REQUIRE( C(0,0) == Approx(2.0f ));
     REQUIRE( C(0,1) == Approx(0.0f ));
@@ -87,25 +55,14 @@ TEST_CASE("Sum test")
     REQUIRE( C(2,0) == Approx(18.0f));
     REQUIRE( C(2,1) == Approx(21.7f));
     REQUIRE( C(2,2) == Approx(6.0f ));
-
-    // Wrong dimensions
-    B = Matrix(2,2);
-    REQUIRE( Matrix::sum(A, B, A) == false );
-    REQUIRE( Matrix::sum(A, A, B) == false );
 }
 
 TEST_CASE("Subtract test")
 {
-    float dataA[] = { 1.0f, -2.0f, 3.0f, 4.5f, 5.0f, -6.0f, 7.0f, 8.0f, -9.0f };
-    Matrix A{3,3, dataA};
-    float dataB[] = { 1.0f, 2.0f, 3.0f, 5.0f, 7.0f, 9.0f, 11.0f, 13.7f, 15.0f};
-    Matrix B{3,3, dataB};
+    MatrixBase<float, 3, 3> A{ 1.0f, -2.0f, 3.0f, 4.5f, 5.0f, -6.0f, 7.0f, 8.0f, -9.0f };
+    MatrixBase<float, 3, 3> B{ 1.0f, 2.0f, 3.0f, 5.0f, 7.0f, 9.0f, 11.0f, 13.7f, 15.0f};
 
-    Matrix C{A.rows, A.columns};
-    REQUIRE( Matrix::subtract(A, B, C) == true );
-
-    REQUIRE( C.rows    == 3 );
-    REQUIRE( C.columns == 3 );
+    MatrixBase<float, 3, 3> C{};
 
     REQUIRE( C(0,0) == Approx(0.0f  ));
     REQUIRE( C(0,1) == Approx(-4.0f ));
@@ -116,22 +73,13 @@ TEST_CASE("Subtract test")
     REQUIRE( C(2,0) == Approx(-4.0f ));
     REQUIRE( C(2,1) == Approx(-5.7f ));
     REQUIRE( C(2,2) == Approx(-24.0f));
-
-    // Wrong dimensions
-    B = Matrix(2,2);
-    REQUIRE( Matrix::subtract(A, B, A) == false );
-    REQUIRE( Matrix::subtract(A, A, B) == false );
 }
 
 TEST_CASE("Transpose test")
 {
-    float dataA[] = { 1.0f, -2.0f, 3.0f, 4.5f, 5.0f, -6.0f, 7.0f, 8.0f, -9.0f };
-    Matrix A = Matrix(3,3, dataA);
+    MatrixBase <float, 3, 3> A{ 1.0f, -2.0f, 3.0f, 4.5f, 5.0f, -6.0f, 7.0f, 8.0f, -9.0f };
 
-    REQUIRE( Matrix::transpose(A, A) == true );
-
-    REQUIRE( A.rows    == 3);
-    REQUIRE( A.columns == 3);
+    A = transpose(A);
 
     REQUIRE( A(0,0) == Approx( 1.0f ));
     REQUIRE( A(0,1) == Approx( 4.5f ));
@@ -143,81 +91,25 @@ TEST_CASE("Transpose test")
     REQUIRE( A(2,1) == Approx(-6.0f ));
     REQUIRE( A(2,2) == Approx(-9.0f ));
 
-    float dataB[] = { 1, 2, 3.7};
-    Matrix B{3,1, dataB};
-    Matrix C{1,3};
+    MatrixBase <float, 3, 1> B{ 1, 2, 3.7};
+    MatrixBase <float, 1, 3> C;
 
-    REQUIRE( Matrix::transpose(B, C) == true );
-
-    REQUIRE( C.rows    == 1);
-    REQUIRE( C.columns == 3);
+    C = transpose(B);
     
-    REQUIRE( C(0) == Approx(1  ));
-    REQUIRE( C(1) == Approx(2  ));
-    REQUIRE( C(2) == Approx(3.7));
-
-    // Wrong dimensions
-    REQUIRE( Matrix::transpose(A, B) == false);
-}
-
-TEST_CASE("LU Decomposition test") {
-    float dataA[] = { 1.0f, -2.0f, 3.0f, 4.5f, 5.0f, -6.0f, 7.0f, 8.0f, -9.0f };
-    Matrix A = Matrix(3,3, dataA);
-    Matrix L{3,3};
-    Matrix U{3,3};
-
-    REQUIRE( Matrix::luDecomposition(A, L, U) == true );
-
-    REQUIRE( L(0,0) == Approx( 1.0f ));
-    REQUIRE( L(0,1) == Approx( 0.0f ));
-    REQUIRE( L(0,2) == Approx( 0.0f ));
-    REQUIRE( L(1,0) == Approx( 4.5f ));
-    REQUIRE( L(1,1) == Approx( 1.0f ));
-    REQUIRE( L(1,2) == Approx( 0.0f ));
-    REQUIRE( L(2,0) == Approx( 7.0f ));
-    REQUIRE( L(2,1) == Approx( 1.57f).epsilon(0.01));
-    REQUIRE( L(2,2) == Approx( 1.0f ));
-
-    REQUIRE( U(0,0) == Approx( 1.0f ));
-    REQUIRE( U(0,1) == Approx(-2.0f ));
-    REQUIRE( U(0,2) == Approx( 3.0f ));
-    REQUIRE( U(1,0) == Approx( 0.0f ));
-    REQUIRE( U(1,1) == Approx( 14.0f));
-    REQUIRE( U(1,2) == Approx(-19.5f));
-    REQUIRE( U(2,0) == Approx( 0.0f ));
-    REQUIRE( U(2,1) == Approx( 0.0f ));
-    REQUIRE( U(2,2) == Approx( 0.64f).epsilon(0.01));
-
-    Matrix L2{1,3};
-    REQUIRE( Matrix::luDecomposition(A, L2, U) == false );
-    REQUIRE( Matrix::luDecomposition(A, L, L2) == false );
+    REQUIRE( C(0,0) == Approx(1  ));
+    REQUIRE( C(0,1) == Approx(2  ));
+    REQUIRE( C(0,2) == Approx(3.7));
 }
 
 TEST_CASE("Determinant test") {
-    float dataA[] = { 1.0f, -2.0f, 3.0f, 4.5f, 5.0f, -6.0f, 7.0f, 8.0f, -9.0f };
-    Matrix A{3,3, dataA};
-    float det;
-
-    REQUIRE( Matrix::determinant(A, det) == true );
-    REQUIRE( det == Approx( 9.0 ));
-
-    Matrix B{5, 4};
-    REQUIRE( Matrix::determinant(B, det) == false);
-
-    float dataC[] = { 1.0f, -2.0f, 3.0f, 4.5f, 5.0f, -6.0f, 7.0f, 8.0f, -9.0f, 1.0f, -2.0f, 3.0f, 4.5f, 5.0f, -6.0f, 7.0f, 8.0f, -9.0f, 1.0f, -2.0f, 3.0f, 4.5f, 5.0f, -6.0f, 7.0f };
-    Matrix C{5, 5, dataC};
-    Matrix::determinant(C, det);
-    REQUIRE( det == Approx(-46544.0f));
+    MatrixBase<float, 3, 3> A{ 1.0f, -2.0f, 3.0f, 4.5f, 5.0f, -6.0f, 7.0f, 8.0f, -9.0f };
+    REQUIRE( det(A) == Approx( 9.0 ));
 }
 
 TEST_CASE("Inverse test") {
-    float dataA[] = { 1.0f, -2.0f, 3.0f, 4.5f, 5.0f, -6.0f, 7.0f, 8.0f, -9.0f };
-    Matrix A = Matrix(3,3, dataA);
+    MatrixBase<float, 3, 3>  A{ 1.0f, -2.0f, 3.0f, 4.5f, 5.0f, -6.0f, 7.0f, 8.0f, -9.0f };
     
-    REQUIRE( Matrix::invert(A, A) == true);
-
-    REQUIRE( A.rows    == 3);
-    REQUIRE( A.columns == 3);
+    A = inv(A);
 
     REQUIRE( A(0,0) == Approx( 0.33f ).epsilon(0.01));
     REQUIRE( A(0,1) == Approx( 0.66f ).epsilon(0.01));
@@ -228,58 +120,4 @@ TEST_CASE("Inverse test") {
     REQUIRE( A(2,0) == Approx( 0.11f ).epsilon(0.01));
     REQUIRE( A(2,1) == Approx(-2.44f ).epsilon(0.01));
     REQUIRE( A(2,2) == Approx( 1.55f ).epsilon(0.01));
-
-    Matrix B{1, 5};
-    REQUIRE( Matrix::invert(A, B) == false);
-    REQUIRE( Matrix::invert(B, B) == false);
-}
-
-TEST_CASE("Eye test") {
-    Matrix I = Matrix::eye(5);
-    for (int i = 0; i < 5; i++)
-    {
-        for (int j = 0; j < 5; j++) {
-            if (i == j)
-            {
-                REQUIRE( I.data[i * I.columns + j] == 1);
-            }
-            else
-            {
-                REQUIRE( I.data[i * I.columns + j] == 0);
-            }
-        }
-    }
-}
-
-TEST_CASE("Minors test") {
-    float dataA[] = { 1.0f, -2.0f, 3.0f, 4.5f, 5.0f, -6.0f, 7.0f, 8.0f, -9.0f };
-    Matrix A = Matrix(3,3, dataA);
-    float minor;
-    REQUIRE( Matrix::minor(A, 2, 2, minor) == true );
-    REQUIRE(minor == Approx(14.0f) );
-
-    Matrix B{1,4};
-    REQUIRE( Matrix::minor(B, 1, 1, minor) == false );
-
-    float dataC[] = { 1.0f, -2.0f, 3.0f, 4.5f, 5.0f, -6.0f, 7.0f, 8.0f, -9.0f, 1.0f, -2.0f, 3.0f, 4.5f, 5.0f, -6.0f, 7.0f, 8.0f, -9.0f, 1.0f, -2.0f, 3.0f, 4.5f, 5.0f, -6.0f, 7.0f };
-    Matrix C{5, 5, dataC};
-    REQUIRE( Matrix::minor(C, 1, 3, minor) == true );
-    REQUIRE(minor == Approx(-1415.3f) );
-}
-
-TEST_CASE("Cofactor matrix") {
-    float dataA[] = { 1.0f, -2.0f, 3.0f, 4.5f, 5.0f, -6.0f, 7.0f, 8.0f, -9.0f };
-    Matrix A = Matrix(3,3, dataA);
-
-    REQUIRE( Matrix::cofactorMatrix(A, A) );
-
-    REQUIRE( A(0,0) == Approx( 3.0f ));
-    REQUIRE( A(0,1) == Approx(-1.5f ));
-    REQUIRE( A(0,2) == Approx( 1.0f ));
-    REQUIRE( A(1,0) == Approx( 6.0f ));
-    REQUIRE( A(1,1) == Approx(-30.0f));
-    REQUIRE( A(1,2) == Approx(-22.0f));
-    REQUIRE( A(2,0) == Approx(-3.0f ));
-    REQUIRE( A(2,1) == Approx( 19.5f));
-    REQUIRE( A(2,2) == Approx( 14.0f ));
 }
