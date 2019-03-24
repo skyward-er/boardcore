c85c2ac71902407e158e8216cce7bf1e104c22c0
Luca Erbetta <luca.erbetta@skywarder.eu>
2019-03-24 16:05:35 +0100

    [BME280] Added BME280 driver from rogallina

---
+++ src/shared/sensors/BME280/BME280Data.h
@@ -0,0 +1,45 @@
+/* Copyright (c) 2015-2018 Skyward Experimental Rocketry
+ * Authors: Luca Erbetta
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#ifndef SRC_SHARED_SENSORS_BME280DATA_H
+#define SRC_SHARED_SENSORS_BME280DATA_H
+
+#include <ostream>
+#include <string>
+
+struct BME280Data
+{
+    uint32_t timestamp;
+    float pressure, temperature;
+    uint32_t raw_pressure, raw_temperature;
+    static std::string header()
+    {
+        return "timestamp,pressure,temperature,raw_pressure,raw_temperature\n";
+    }
+
+    void print(std::ostream& os) const
+    {
+        os << timestamp << "," << pressure << "," << temperature << ","
+           << raw_pressure << "," << raw_temperature << "\n";
+    }
+};
+
+#endif /* SRC_SHARED_SENSORS_BME280DATA_H */
