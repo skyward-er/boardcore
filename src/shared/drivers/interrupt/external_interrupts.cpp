5a374d6b02a6250e1747dafe7ec2c3147c62065b
elvis <elvisilde@gmail.com>
2019-04-10 11:03:11 +0200

    [exti] Clear flag BEFORE calling IRQhandler (+ documentation)

--- src/shared/drivers/interrupt/external_interrupts.cpp
+++ src/shared/drivers/interrupt/external_interrupts.cpp
@@ -1,12 +1,66 @@
-#include <miosix.h>
+/*
+ * Copyright (c) 2019 Skyward Experimental Rocketry
+ * Authors: Alvise de' Faveri Tron
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
+#include <miosix.h>
 using namespace miosix;
 
 /**
- * All unused interrupts call this function.
- */
-extern void unexpectedInterrupt();
+ * Since in the stm32 interrupts greater than 5 are handled in groups
+ * (from 5 to 9 and from 10 to 15), this class provides a way to address single
+ * interrupts in this range.
+ *
+ * In particular, an EXTIxx_IRQHandlerImpl() function is declared for every
+ * interrupt from 5 to 15, and defined in a similar fashion as miosix's
+ * 'stage_1_boot', i.e. handlers are "weak" and, if not overridden, will be 
+ * assigned to a default handler.
+ *
+ * NOTE: you will still have to enable the corresponding interrupt if you
+ * want to use it, defining an IRQHandlerImpl is not enough.
+ *
+ * USAGE EXAMPLE:
 
+    // Handle External Interrupt 9
+    void __attribute__((used)) EXTI9_IRQHandlerImpl()
+    {
+        // Do some small operation, e.g. set a boolean flag
+        // No need to clear interrupt
+        // No need to use naked function
+    }
+
+    int main()
+    {
+        // 1. Set SYSCFG and EXTI registers (IMR, FTSR/RTSR)
+        ...
+
+        // 2. NVIC settings
+        NVIC_EnableIRQ(EXTI9_5_IRQn);
+        NVIC_SetPriority(EXTI9_5_IRQn, choose_a_priority);
+    }
+
+*/
+
+// All unused interrupts call this function.
+extern void unexpectedInterrupt();
 extern "C" void Default_EXTI_Handler() 
 {
     unexpectedInterrupt();
@@ -73,28 +127,28 @@ void __attribute__((used)) EXTI9_5_IRQHandlerImpl()
 {
     if(EXTI->PR & EXTI_PR_PR5)
     {
-        EXTI5_IRQHandlerImpl();
         EXTI->PR |= EXTI_PR_PR5;   // Clear pending flag
+        EXTI5_IRQHandlerImpl();
     }
     else if(EXTI->PR & EXTI_PR_PR6)
     {
-        EXTI6_IRQHandlerImpl();
         EXTI->PR |= EXTI_PR_PR6;
+        EXTI6_IRQHandlerImpl();
     }
     else if(EXTI->PR & EXTI_PR_PR7)
     {
-        EXTI7_IRQHandlerImpl();
         EXTI->PR |= EXTI_PR_PR7;
+        EXTI7_IRQHandlerImpl();
     }
     else if(EXTI->PR & EXTI_PR_PR8)
     {
-        EXTI8_IRQHandlerImpl();
         EXTI->PR |= EXTI_PR_PR8;
+        EXTI8_IRQHandlerImpl();
     }
     else if(EXTI->PR & EXTI_PR_PR9)
     {
-        EXTI9_IRQHandlerImpl();
         EXTI->PR |= EXTI_PR_PR9;
+        EXTI9_IRQHandlerImpl();
     }
     else
     {
@@ -106,60 +160,36 @@ void __attribute__((used)) EXTI15_10_IRQHandlerImpl()
 {
     if(EXTI->PR & EXTI_PR_PR10)
     {
-        EXTI10_IRQHandlerImpl();
         EXTI->PR |= EXTI_PR_PR10;   // Clear pending flag
+        EXTI10_IRQHandlerImpl();
     }
     else if(EXTI->PR & EXTI_PR_PR11)
     {
-        EXTI11_IRQHandlerImpl();
         EXTI->PR |= EXTI_PR_PR11;
+        EXTI11_IRQHandlerImpl();
     }
     else if(EXTI->PR & EXTI_PR_PR12)
     {
-        EXTI12_IRQHandlerImpl();
         EXTI->PR |= EXTI_PR_PR12;
+        EXTI12_IRQHandlerImpl();
     }
     else if(EXTI->PR & EXTI_PR_PR13)
     {
-        EXTI13_IRQHandlerImpl();
         EXTI->PR |= EXTI_PR_PR13;
+        EXTI13_IRQHandlerImpl();
     }
     else if(EXTI->PR & EXTI_PR_PR14)
     {
-        EXTI14_IRQHandlerImpl();
         EXTI->PR |= EXTI_PR_PR14;
+        EXTI14_IRQHandlerImpl();
     }
     else if(EXTI->PR & EXTI_PR_PR15)
     {
-        EXTI15_IRQHandlerImpl();
         EXTI->PR |= EXTI_PR_PR15;
+        EXTI15_IRQHandlerImpl();
     }
     else
     {
         Default_EXTI_Handler();
     }
 }
-
-
-
-/** USAGE EXAMPLE:
-
-    // Handle External Interrupt 9
-    void __attribute__((used)) EXTI9_IRQHandler()
-    {
-        // Do some small operation, e.g. set a boolean flag
-        // No need to clear interrupt
-        // No need to use naked function
-    }
-
-    int main()
-    {
-        // 1. Set SYSCFG and EXTI registers (IMR, FTSR/RTSR)
-        ...
-
-        // 2. NVIC settings
-        NVIC_EnableIRQ(EXTI9_5_IRQn);
-        NVIC_SetPriority(EXTI9_5_IRQn, choose_a_priority);
-    }
-
-*/
