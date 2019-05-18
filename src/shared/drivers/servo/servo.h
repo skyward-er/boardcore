eaeb0b2762484d3d234f169e819d06dea97e8b04
Luca Erbetta <luca.erbetta@skywarder.eu>
2019-05-18 09:57:02 +0200

    [Servo] New servo implementation

---
+++ src/shared/drivers/servo/servo.h
@@ -0,0 +1,118 @@
+/**
+ *
+ * Copyright (c) 2019 Skyward Experimental Rocketry
+ * Authors: Luca Erbetta
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include <drivers/HardwareTimer.h>
+#include <drivers/pwm/pwm.h>
+
+/**
+ * Class used to control servomotors. This impementation differs from the
+ * one in miosix (in servo_stm32.h)  as it can control servos connected to any
+ * set of pins & timers, but it is not thread safe.
+ */
+class Servo
+{
+public:
+    Servo(PWM::Timer t);
+    ~Servo();
+
+    /**
+     * Start producing the output PWM waveforms for the enabled channels
+     * @return
+     */
+    void start();
+
+    /**
+     * Stop producing the output waveforms.
+     * @return
+     */
+    void stop();
+
+    /**
+     * Enable the servo. The driver will start to output the PWM waveform to
+     * drive the motor.
+     *
+     * @param channel Which servo to enable
+     */
+    void enable(PWMChannel channel);
+
+    /**
+     * Disable the servo. No output PWM waveform is generated until the servo is
+     * enabled again.
+     *
+     * @param channel Which servo to disable
+     */
+    void disable(PWMChannel channel);
+
+    /**
+     * Sets the position of the servomotor.
+     * Accepts a value from 0 to 1.
+     * @param channel Which servo to control
+     * @param pos Position in range [0, 1]
+     */
+    void setPosition(PWMChannel channel, float pos);
+
+    /**
+     * Returns the current position of the specified servo
+     * @param channel Which servo
+     * @return Position of the selected servo
+     */
+    float getPosition(PWMChannel channel);
+
+    /**
+     * Sets the output PWM frequency. Default value is 50 Hz, must be between 10
+     * Hz and 100 Hz
+     * @param freq Frequency of the output PWM
+     */
+    void setFrequency(unsigned int freq);
+
+    /**
+     * Sets the minimum duration of the PWM pulse.
+     * That is, how long the PWM waveform stays high when the position is 0.
+     *
+     * Must be between 500 and 1300. Default value is 1000.
+     * @param min_pulse Pulse width in microseconds.
+     */
+    void setMinPulseWidth(float min_pulse);
+
+    /**
+     * Sets the mmaximum duration of the PWM pulse.
+     * That is, how long the PWM waveform stays high when the position is 1.
+     * Must be between 1700 and 2500. Default value is 1000.
+     * @param min_pulse Pulse width in microseconds.
+     */
+    void setMaxPulseWidth(float max_pulse);
+
+private:
+    float calculateDutyCycle(float position);
+
+    void updateParameters();
+
+    PWM pwm;
+
+    float positions[4];
+
+    float frequency = 50;
+    float min_pulse = 1000;
+    float max_pulse = 2000;
+};
---
+++ src/tests/drivers/test-servo.cpp
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2019 Skyward Experimental Rocketry
+ * Authors: Luca Erbetta
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include <drivers/servo/servo.h>
+
+#include <miosix.h>
+using namespace miosix;
+
+using ps1 = Gpio<GPIOD_BASE, 12>;
+using ps2 = Gpio<GPIOD_BASE, 13>;
+using ps3 = Gpio<GPIOC_BASE, 7>;
+
+PWMChannel s1_ch = PWMChannel::CH1;
+PWMChannel s2_ch = PWMChannel::CH2;
+PWMChannel s3_ch = PWMChannel::CH2;
+
+int main()
+{
+    {
+        FastInterruptDisableLock dLock;
+        ps1::mode(Mode::ALTERNATE);
+        ps2::mode(Mode::ALTERNATE);
+        ps3::mode(Mode::ALTERNATE);
+
+        ps1::alternateFunction(2);
+        ps2::alternateFunction(2);
+
+        ps3::alternateFunction(3);
+    }
+
+    PWM::Timer tim4{
+        TIM4, &(RCC->APB1ENR), RCC_APB1ENR_TIM4EN,
+        TimerUtils::getPrescalerInputFrequency(TimerUtils::InputClock::APB1)};
+
+    PWM::Timer tim8{
+        TIM8, &(RCC->APB2ENR), RCC_APB2ENR_TIM8EN,
+        TimerUtils::getPrescalerInputFrequency(TimerUtils::InputClock::APB2)};
+
+    Servo s12{tim4};
+    Servo s3{tim4};
+
+    s12.start();
+    s3.start();
+
+    s12.enable(s1_ch);
+    s12.enable(s2_ch);
+    s3.enable(s3_ch);
+
+    float pos[] = {0, 0.5f, 1.0f};
+
+    int i = 0;
+    for (;;)
+    {
+        // Cycle each servo between the 3 predefined positions
+        s12.setPosition(s1_ch, pos[i % 3]);
+        s12.setPosition(s1_ch, pos[(i + 1) % 3]);
+        s12.setPosition(s1_ch, pos[(i + 2) % 3]);
+
+        ++i;
+
+        Thread::sleep(5000);
+    }
+}
