edcc4567f9930e30e13cea72c49a1f0d32dd6aab
Nuno Barcellos <nuno.barcellos@skywarder.eu>
2019-05-21 16:09:43 +0200

    [mpu9250] Added accel calibration handler and entrypoint

---
+++ src/tests/drivers/calibrate-mpu9250.cpp
@@ -0,0 +1,92 @@
+/* Copyright (c) 2019 Skyward Experimental Rocketry
+ * Authors: Nuno Barcellos
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include <Common.h>
+#include <drivers/BusTemplate.h>
+#include <interfaces-impl/hwmapping.h>
+#include <sensors/MPU9250/MPU9250.h>
+
+#include <drivers/spi/SensorSpi.h>
+#include <sensors/SensorSampling.h>
+
+using namespace miosix;
+using namespace miosix::interfaces;
+
+typedef BusSPI<1,spi1::mosi,spi1::miso,spi1::sck> busSPI1;
+typedef ProtocolSPI<busSPI1,sensors::mpu9250::cs> spiMPU9250_a;
+typedef MPU9250<spiMPU9250_a> mpu_t;
+
+int main()
+{   
+    float bias[3];
+
+    SimpleSensorSampler sampler;
+
+    spiMPU9250_a::init();
+    mpu_t* mpu = new mpu_t(1, 1);
+
+    Thread::sleep(100);
+    
+    if(mpu->init()){
+        printf("MPU9250 Init succeeded\n" );
+        sampler.AddSensor(mpu);
+    }
+    else {
+        printf("MPU9250 Init failed\n");
+
+        while(!mpu->init()) {
+            printf("MPU9250 Init failed\n");
+            Thread::sleep(1000);
+        }
+    }
+
+    Thread::sleep(100);
+
+    printf("This script calculates the accelerometer's offsets, follow the instructions:\n");
+    printf("Leave the MPU9250 pointing up (aligned with the gravity vector) and steady, then press ENTER ");
+    getchar();
+    mpu->calibrateAccel();
+
+    printf("Calibration succeeded\n");
+    printf("Copy the following parameters into the MPU9250 class constructor: \n\n");
+
+    mpu->getAccelCalibParams(bias);
+    printf("_axb = %f;\n",bias[0]);
+    printf("_ayb = %f;\n",bias[1]);
+    printf("_azb = %f;\n",bias[2]);
+
+    printf("Press ENTER to read the accelerometer data and to check if the calibration is good\n");
+    getchar();
+    
+    mpu->setAccelCalibParams(bias);
+
+    while(true)
+    {
+        sampler.Update();
+
+        const Vec3* last_data = mpu->accelDataPtr();
+        printf("%f %f %f\n", last_data->getX(), last_data->getY(),
+               last_data->getZ());
+
+        Thread::sleep(100);
+    }
+}
