54c698ae0356be094107a053c59b2cd2e09f7172
Luca Erbetta <luca.erbetta@skywarder.eu>
2020-03-02 01:59:43 +0100

    [SPIDriver] Unit tests (part 1)

---
+++ src/tests/catch/spidriver/FakeSPIBus.h
@@ -0,0 +1,277 @@
+/**
+ * Copyright (c) 2020 Skyward Experimental Rocketry
+ * Authors: Luca Erbetta (luca.erbetta@skywarder.eu)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include "FakeSpiTypedef.h"
+#include "drivers/spi/SPIInterface.h"
+
+#pragma once
+
+/**
+ * @brief SPIBus modified to accept a fake Chip select & spi peripheral struct.
+ * Not ideal as modifications to SPIBus have to be manually applied here too,
+ * but that's the only way I found to test it effectively
+ */
+class FakeSPIBus : public SPIBusInterface
+{
+public:
+    /**
+     * @brief Instantiates a new FakeSPIBus
+     *
+     * @param spi Pointer to the SPI peripheral to be used
+     */
+    FakeSPIBus(FakeSpiTypedef* spi);
+    ~FakeSPIBus() {}
+
+    // Delete copy/move contructors/operators
+    FakeSPIBus(const FakeSPIBus&) = delete;
+    FakeSPIBus& operator=(const FakeSPIBus&) = delete;
+
+    FakeSPIBus(FakeSPIBus&&) = delete;
+    FakeSPIBus& operator=(FakeSPIBus&&) = delete;
+
+    /**
+     * @brief Wether to apply slave-specific bus configuration before each
+     * transaction (BusTemplate compatibility mode).
+     * Only set to false to use SPIDriver alongside BusTemplate.h.
+     * Default value is true.
+     *
+     * @param value True: The slave configuration is applied to the SPI
+     * peripheral before each transaction. False: No configuration is ever
+     * applied to the SPI peripheral. The SPI peripheral retains the
+     * configuration set by BusTemplate.h
+     */
+    void enableSlaveConfiguration(bool value) { config_enabled = value; }
+
+    /**
+     * @brief See SPIBusInterface::write()
+     */
+    void write(uint8_t byte) override;
+
+    /**
+     * @brief See SPIBusInterface::write()
+     */
+    void write(uint8_t* data, size_t size) override;
+
+    /**
+     * @brief See SPIBusInterface::read()
+     */
+    uint8_t read() override;
+
+    /**
+     * @brief See SPIBusInterface::read()
+     */
+    void read(uint8_t* data, size_t size) override;
+
+    /**
+     * @brief See SPIBusInterface::transfer()
+     */
+    uint8_t transfer(uint8_t data) override;
+
+    /**
+     * @brief See SPIBusInterface::transfer()
+     */
+    void transfer(uint8_t* data, size_t size) override;
+
+    /**
+     * @brief See SPIBusInterface::select()
+     */
+    void select(GpioPin& cs) override;
+
+    /**
+     * @brief See SPIBusInterface::deselect()
+     */
+    void deselect(GpioPin& cs) override;
+
+    /**
+     * @brief See SPIBusInterface::configure()
+     */
+    void configure(SPIBusConfig config) override;
+
+private:
+    /**
+     * Writes a single byte on the SPI bus.
+     *
+     * @param    byte Pointer to the byte to be written
+     */
+    void write(uint8_t* byte);
+
+    /**
+     * Reads a single byte from the SPI bus.
+     *
+     * @param    byte Pointer to the byte where the read data will be stored
+     */
+    void read(uint8_t* byte);
+
+    /**
+     * Full duplex transfer. Writes a single byte on the SPI bus and replaces
+     * its content with the received data
+     *
+     * @param    byte Pointer to the byte to be transfered
+     */
+    void transfer(uint8_t* byte);
+
+    FakeSpiTypedef* spi;
+
+    SPIBusConfig config;
+    bool config_enabled       = true;
+    bool first_config_applied = false;
+};
+
+FakeSPIBus::FakeSPIBus(FakeSpiTypedef* spi) : spi(spi) {}
+
+void FakeSPIBus::configure(SPIBusConfig new_config)
+{
+    // Reconfigure the bus only if config enabled. Do not reconfigure if already
+    // in the correct configuration.
+    if (config_enabled && (!first_config_applied || new_config != config))
+    {
+        first_config_applied = true;
+        config               = new_config;
+
+        // Clean CR1
+        spi->CR1 = 0;
+
+        // Configure clock division (BR bits)
+        spi->CR1 |= (static_cast<uint16_t>(config.br) & 0x0007) << 3;
+        // Configure CPOL & CPHA bits
+        spi->CR1 |= ((uint16_t)config.cpol & 0x0001) << 1 |
+                    ((uint16_t)config.cpha & 0x0001);
+
+        // Configure LSBFIRST bit
+        spi->CR1 |= (uint16_t)config.lsb_first << 7;
+
+        spi->CR1 |= SPI_CR1_SSI | SPI_CR1_SSM  // Use software chip-select
+                    | SPI_CR1_MSTR             // Master mode
+                    | SPI_CR1_SPE;             // Enable SPI
+    }
+}
+
+inline void FakeSPIBus::write(uint8_t data) { write(&data); }
+
+inline void FakeSPIBus::write(uint8_t* data, size_t size)
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        write(data + i);
+    }
+}
+
+inline uint8_t FakeSPIBus::read()
+{
+    uint8_t data;
+    read(&data);
+
+    return data;
+}
+
+inline void FakeSPIBus::read(uint8_t* data, size_t size)
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        read(data + i);
+    }
+}
+
+inline uint8_t FakeSPIBus::transfer(uint8_t data)
+{
+    transfer(&data);
+    return data;
+}
+
+inline void FakeSPIBus::transfer(uint8_t* data, size_t size)
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        transfer(data + i);
+    }
+}
+
+inline void FakeSPIBus::select(GpioPin& rcs)
+{
+    FakeGpioPin& cs = static_cast<FakeGpioPin&>(rcs);
+
+    cs.low();
+    if (config.cs_setup_time_us > 0)
+    {
+        delayUs(config.cs_setup_time_us);
+    }
+}
+
+inline void FakeSPIBus::deselect(GpioPin& rcs)
+{
+    FakeGpioPin& cs = static_cast<FakeGpioPin&>(rcs);
+
+    if (config.cs_hold_time_us > 0)
+    {
+        delayUs(config.cs_hold_time_us);
+    }
+    cs.high();
+}
+
+inline void FakeSPIBus::write(uint8_t* byte)
+{
+    // Wait until the peripheral is ready to transmit
+    while ((spi->SR & SPI_SR_TXE) == 0)
+        ;
+    // Write the byte in the transmit buffer
+    spi->DR = *byte;
+
+    // Wait until byte is transmitted
+    while ((spi->SR & SPI_SR_RXNE) == 0)
+        ;
+
+    // Clear the RX buffer by accessing the DR register
+    spi->DR;
+}
+
+inline void FakeSPIBus::transfer(uint8_t* byte)
+{
+    // Wait until the peripheral is ready to transmit
+    while ((spi->SR & SPI_SR_TXE) == 0)
+        ;
+    // Write the byte in the transmit buffer
+    spi->DR = *byte;
+
+    // Wait until byte is transmitted
+    while ((spi->SR & SPI_SR_RXNE) == 0)
+        ;
+
+    // Store the received data in the byte
+    *byte = (uint8_t)spi->DR;
+}
+
+inline void FakeSPIBus::read(uint8_t* byte)
+{
+    // Wait until the peripheral is ready to transmit
+    while ((spi->SR & SPI_SR_TXE) == 0)
+        ;
+    // Write the byte in the transmit buffer
+    spi->DR = 0;
+
+    // Wait until byte is transmitted
+    while ((spi->SR & SPI_SR_RXNE) == 0)
+        ;
+
+    // Store the received data in the byte
+    *byte = (uint8_t)spi->DR;
+}
---
+++ src/tests/catch/spidriver/test-spidriver.cpp
@@ -0,0 +1,346 @@
+/**
+ * Copyright (c) 2020 Skyward Experimental Rocketry
+ * Authors: Luca Erbetta (luca.erbetta@skywarder.eu)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifdef STANDALONE_CATCH1_TEST
+#include "../catch-tests-entry.cpp"
+#endif
+
+#include <utils/testutils/catch.hpp>
+
+#include "FakeSPIBus.h"
+#include "drivers/spi/MockSPIBus.h"
+#include "drivers/spi/SPIDriver.h"
+
+template <typename T1, typename T2>
+bool bufcmp(T1* buf1, T2* buf2, size_t size)
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        if (*buf1 != *buf2)
+            return false;
+
+        buf1++;
+        buf2++;
+    }
+    return true;
+}
+
+TEST_CASE("SPIBus - Bus Configuration")
+{
+    FakeSpiTypedef spi;
+
+    FakeSPIBus bus{&spi};
+
+    REQUIRE(spi.CR1 == 0);
+
+    SECTION("Configure & check CR1")
+    {
+        SPIBusConfig config;
+        config.br = SPIBaudRate::DIV_16;
+
+        config.cpha      = 1;
+        config.cpol      = 1;
+        config.lsb_first = true;
+
+        uint32_t expected_CR1 = 0x03DF;
+
+        bus.configure(config);
+        REQUIRE(spi.CR1 == expected_CR1);
+
+        // Change config
+        config.br = SPIBaudRate::DIV_256;
+
+        config.cpha      = 0;
+        config.cpol      = 0;
+        config.lsb_first = false;
+
+        expected_CR1 = 0x037C;
+
+        bus.configure(config);
+        REQUIRE(spi.CR1 == expected_CR1);
+
+        config.cpha      = 0;
+        config.cpol      = 1;
+        config.lsb_first = false;
+
+        expected_CR1 = 0x037E;
+
+        bus.configure(config);
+        REQUIRE(spi.CR1 == expected_CR1);
+
+        config.cpha      = 1;
+        config.cpol      = 0;
+        config.lsb_first = false;
+
+        expected_CR1 = 0x037D;
+
+        bus.configure(config);
+        REQUIRE(spi.CR1 == expected_CR1);
+
+        // Reapply same config
+        bus.configure(config);
+        REQUIRE(spi.CR1 == expected_CR1);
+    }
+
+    SECTION("Disable configuration")
+    {
+        SPIBusConfig config;
+        config.br = SPIBaudRate::DIV_16;
+
+        config.cpha      = 1;
+        config.cpol      = 1;
+        config.lsb_first = true;
+
+        bus.enableSlaveConfiguration(false);
+        bus.configure(config);
+        REQUIRE(spi.CR1 == 0);
+    }
+
+    SECTION("Wrong parameters")
+    {
+        SPIBusConfig config;
+        config.br = SPIBaudRate::DIV_16;
+
+        config.cpha      = 8;
+        config.cpol      = 9;
+        config.lsb_first = true;
+
+        uint32_t expected_CR1 = 0x03DE;
+
+        bus.configure(config);
+        REQUIRE(spi.CR1 == expected_CR1);
+    }
+}
+
+TEST_CASE("SPIBus - Chip select")
+{
+    FakeSpiTypedef spi;
+
+    FakeSPIBus bus{&spi};
+
+    REQUIRE(spi.cs.value() == 1);
+
+    bus.select(spi.cs);
+    REQUIRE(spi.cs.value() == 0);
+
+    bus.deselect(spi.cs);
+    REQUIRE(spi.cs.value() == 1);
+}
+
+TEST_CASE("SPIBus - One byte operations")
+{
+    FakeSpiTypedef spi;
+
+    spi.DR.in_buf    = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    spi.CR1_expected = 0x03DF;
+
+    FakeSPIBus bus{&spi};
+
+    SPIBusConfig config;
+    config.br = SPIBaudRate::DIV_16;
+
+    config.cpha      = 1;
+    config.cpol      = 1;
+    config.lsb_first = true;
+
+    bus.configure(config);
+    bus.select(spi.cs);
+
+    SECTION("Write")
+    {
+        bus.write(1);
+        REQUIRE(spi.DR.out_buf.back() == 1);
+        REQUIRE(spi.DR.out_buf.size() == 1);
+
+        bus.write(2);
+        REQUIRE(spi.DR.out_buf.back() == 2);
+        REQUIRE(spi.DR.out_buf.size() == 2);
+    }
+    SECTION("Read")
+    {
+        REQUIRE(bus.read() == spi.DR.in_buf[0]);
+        REQUIRE(spi.DR.out_buf.size() == 1);
+        REQUIRE(spi.DR.out_buf.back() == 0);
+
+        REQUIRE(bus.read() == spi.DR.in_buf[1]);
+        REQUIRE(spi.DR.out_buf.size() == 2);
+        REQUIRE(spi.DR.out_buf.back() == 0);
+    }
+
+    SECTION("Transfer")
+    {
+        REQUIRE(bus.transfer(55) == spi.DR.in_buf[0]);
+        REQUIRE(spi.DR.out_buf.back() == 55);
+        REQUIRE(spi.DR.out_buf.size() == 1);
+
+        REQUIRE(bus.transfer(255) == spi.DR.in_buf[1]);
+        REQUIRE(spi.DR.out_buf.back() == 255);
+        REQUIRE(spi.DR.out_buf.size() == 2);
+    }
+}
+
+TEST_CASE("SPIBus - Multi byte operations")
+{
+    FakeSpiTypedef spi;
+
+    spi.DR.in_buf    = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    spi.CR1_expected = 0x03DF;
+
+    FakeSPIBus bus{&spi};
+
+    SPIBusConfig config;
+    config.br = SPIBaudRate::DIV_16;
+
+    config.cpha      = 1;
+    config.cpol      = 1;
+    config.lsb_first = true;
+
+    bus.configure(config);
+    bus.select(spi.cs);
+
+    // 2 identical buffers
+    uint8_t buf[]  = {4, 3, 2, 1};
+    uint8_t bufc[] = {4, 3, 2, 1};
+
+    SECTION("Write")
+    {
+        bus.write(buf, 0);
+        REQUIRE(spi.DR.out_buf.size() == 0);
+
+        bus.write(buf, 4);
+        REQUIRE(spi.DR.out_buf.size() == 4);
+        REQUIRE(bufcmp(bufc, spi.DR.out_buf.data(), 4));
+    }
+
+    SECTION("Read")
+    {
+        bus.read(buf, 0);
+        // Nothing read
+        REQUIRE(bufcmp(bufc, buf, 4));
+
+        bus.read(buf, 4);
+
+        REQUIRE(bufcmp(buf, spi.DR.in_buf.data(), 4));
+    }
+
+    SECTION("Transfer")
+    {
+        bus.transfer(buf, 0);
+        // Nothing read
+        REQUIRE(bufcmp(bufc, buf, 4));
+        // Nothing written
+        REQUIRE(spi.DR.out_buf.size() == 0);
+
+        bus.transfer(buf, 4);
+        REQUIRE(spi.DR.out_buf.size() == 4);
+
+        REQUIRE(bufcmp(bufc, spi.DR.out_buf.data(), 4));
+        REQUIRE(bufcmp(buf, spi.DR.in_buf.data(), 4));
+    }
+}
+
+TEST_CASE("SPITransaction - writes")
+{
+    MockSPIBus bus;
+    SPIBusConfig config1;
+    SPIBusConfig config2;
+
+    config1.cpha = 1;
+    config1.br   = SPIBaudRate::DIV_32;
+
+    config2.lsb_first = true;
+    config2.cpol      = 1;
+
+    bus.expected_config = config1;
+
+    SECTION("Correct config")
+    {
+        SPISlave slave{bus, GpioPin(GPIOA_BASE, 1), config1};
+
+        SPITransaction spi{slave};
+
+        REQUIRE(bus.out_buf.size() == 0);
+
+        SECTION("cmd write")
+        {
+            spi.write(9);
+            REQUIRE(bus.out_buf.size() == 1);
+            REQUIRE(bus.out_buf.back() == 9);
+        }
+
+        SECTION("1 byte reg write")
+        {
+            spi.write(10, 77);
+            REQUIRE(bus.out_buf.size() == 2);
+            REQUIRE(bus.out_buf[0] == 10);
+            REQUIRE(bus.out_buf[1] == 77);
+        }
+
+        SECTION("multi byte reg write")
+        {
+            uint8_t buf[] = {1, 2, 3, 4, 5, 6};
+
+            SECTION("0 size write")
+            {
+                spi.write(10, buf, 0);
+                REQUIRE(bus.out_buf.size() == 1);
+                REQUIRE(bus.out_buf[0] == 10);
+            }
+
+            SECTION("2 writes")
+            {
+                spi.write(10, buf, 4);
+                REQUIRE(bus.out_buf.size() == 5);
+
+                REQUIRE(bus.out_buf[0] == 10);
+                REQUIRE(bufcmp(buf, bus.out_buf.data() + 1, 4));
+
+                spi.write(99, buf, 6);
+                REQUIRE(bus.out_buf.size() == 12);
+
+                REQUIRE(bus.out_buf[5] == 99);
+                REQUIRE(bufcmp(buf, bus.out_buf.data() + 6, 6));
+            }
+        }
+
+        SECTION("raw write")
+        {
+            uint8_t buf[] = {1, 2, 3, 4, 5, 6};
+
+            spi.write(buf, 0);
+            REQUIRE(bus.out_buf.size() == 0);
+
+            spi.write(buf, 4);
+            REQUIRE(bus.out_buf.size() == 4);
+
+            CAPTURE(bus.out_buf.data());
+
+            REQUIRE(bufcmp(buf, bus.out_buf.data(), 4));
+
+            spi.write(buf, 6);
+            REQUIRE(bus.out_buf.size() == 10);
+
+            REQUIRE(bufcmp(buf, bus.out_buf.data() + 4, 6));
+        }
+    }
+}
