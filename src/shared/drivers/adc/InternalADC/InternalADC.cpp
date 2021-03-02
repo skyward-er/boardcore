879fda68e277c48d6f73faeaacfda5a3c36ba37a
Alberto Nidasio <alberto.nidasio@skywarder.eu>
2021-03-02 18:07:37 +0000

    [internal-adc] Implemented simple selfTest and commented code
    But DMA not working other than stream 2 with ADC2!!!

---
+++ src/shared/drivers/adc/InternalADC/InternalADC.cpp
@@ -0,0 +1,336 @@
+/* Copyright (c) 2020 Skyward Experimental Rocketry
+ * Authors: Alberto Nidasio
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include "InternalADC.h"
+
+InternalADC::InternalADC(ADC_TypeDef& ADCx_) : ADCx(ADCx_) { resetRegisters(); }
+
+InternalADC::InternalADC(ADC_TypeDef& ADCx_, const float V_SUPPLY_)
+    : ADCx(ADCx_), V_SUPPLY(V_SUPPLY_)
+{
+    resetRegisters();
+}
+
+InternalADC::InternalADC(ADC_TypeDef& ADCx_, const float V_SUPPLY_,
+                         DMA_Stream_TypeDef& DMAx_Streamx_)
+    : ADCx(ADCx_), V_SUPPLY(V_SUPPLY_), DMAx_Streamx(DMAx_Streamx_),
+      isUsingDMA(true)
+{
+    resetRegisters();
+}
+
+InternalADC::~InternalADC() { resetRegisters(); }
+
+bool InternalADC::init()
+{
+    // Turn on the ADC
+    ADCx.CR2 |= ADC_CR2_ADON;
+
+    // Set single conversion mode
+    ADCx.CR2 &= ~ADC_CR2_CONT;
+
+    // Set scan mode
+    ADCx.CR1 |= ADC_CR1_SCAN;
+
+    // Data alignment
+    ADCx.CR2 &= ~ADC_CR2_ALIGN;  // right
+
+    if (isUsingDMA)
+    {
+        // Set the DMA peripheral address
+        DMAx_Streamx.PAR = (uint32_t) & (ADCx.DR);
+
+        // Set the DMA memory address
+        DMAx_Streamx.M0AR = (uint32_t)values;
+
+        // Enable DMA on ADC
+        ADCx.CR2 |= ADC_CR2_DMA;
+
+        // Enable DMA stream
+        DMAx_Streamx.CR |= DMA_SxCR_EN;
+
+        // Check if we are using the DMA2 controller, otherwise it's DMA1
+        if (((uint32_t)&DMAx_Streamx & ~0xFF) == (uint32_t)DMA2_BASE)
+        {
+            DMAx = DMA2;
+        }
+
+        // Find the DMA stream number
+        streamNum = ((((uint32_t)&DMAx_Streamx) & 0xFF) - 0x10) / 0x18;
+
+        // Check which registers to use
+        statusReg    = (streamNum < 4 ? &(DMAx->LISR) : &(DMAx->HISR));
+        clearFlagReg = (streamNum < 4 ? &(DMAx->LIFCR) : &(DMAx->HIFCR));
+
+        // Create the masks for the status bits (this will do for both high and
+        // low registers as well as status and reset status registers)
+        switch (streamNum % 4)
+        {
+            case 0:
+                transferCompleteMask = DMA_LISR_TCIF0;
+                transferErrorMask    = DMA_LISR_TEIF0;
+                break;
+            case 1:
+                transferCompleteMask = DMA_LISR_TCIF1;
+                transferErrorMask    = DMA_LISR_TEIF1;
+                break;
+            case 2:
+                transferCompleteMask = DMA_LISR_TCIF2;
+                transferErrorMask    = DMA_LISR_TEIF2;
+                break;
+            case 3:
+                transferCompleteMask = DMA_LISR_TCIF3;
+                transferErrorMask    = DMA_LISR_TEIF3;
+                break;
+        }
+
+        // All this is because the status bits are not stored in the
+        // DMAx_Streamx registers but in the main DMA controller status
+        // register. We could not care about the transfer status but the
+        // timestamp would not match the values read after calling sample().
+
+        // We calculate this values to avoid doing it every time in
+        // sampleImpl().
+    }
+
+    return true;
+}
+
+bool InternalADC::enableChannel(Channel channel)
+{
+    return enableChannel(channel, CYCLES_3);
+}
+
+bool InternalADC::enableChannel(Channel channel, SampleTime sampleTime)
+{
+    // Check channel number
+    if (channel < CH0 || (channel >= INJECTED_CHANNEL_N && !isUsingDMA) ||
+        (channel >= CH_NUM && isUsingDMA))
+    {
+        return false;
+    }
+
+    // Add channel to the sequence
+    if (!isUsingDMA)
+    {
+        addInjectedChannel(channel);
+    }
+    else
+    {
+        addRegularChannel(channel);
+
+        // Update the DMA number of data
+        DMAx_Streamx.NDTR = activeChannels;
+    }
+
+    // Set channel's sample time
+    setChannelSampleTime(channel, sampleTime);
+
+    return true;
+}
+
+ADCData InternalADC::getVoltage(Channel channel)
+{
+    float voltage = 0;
+
+    if (indexMap[channel] >= 0 && indexMap[channel] < CH_NUM)
+    {
+        voltage = values[indexMap[channel]] * V_SUPPLY / RESOLUTION;
+    }
+
+    return ADCData{timestamp, voltage};
+}
+
+bool InternalADC::selfTest()
+{
+    // Try a single sample anche check for error
+    sample();
+
+    if (last_error != NO_ERRORS)
+    {
+        return false;
+    }
+
+    return true;
+}
+
+ADCData InternalADC::sampleImpl()
+{
+    if (!isUsingDMA)
+    {
+        startInjectedConversion();
+
+        // Wait for end of conversion
+        while (!(ADCx.SR & ADC_SR_JEOC))
+            ;
+
+        // Read all 4 channels (faster than read only the enabled ones)
+        values[0] = ADCx.JDR1;
+        values[1] = ADCx.JDR2;
+        values[2] = ADCx.JDR3;
+        values[3] = ADCx.JDR4;
+    }
+    else
+    {
+        // Rewrite the DMA bit in ADC CR2 (reference manual chapter 13.8.1)
+        ADCx.CR2 &= ~ADC_CR2_DMA;
+        ADCx.CR2 |= ADC_CR2_DMA;
+
+        startRegularConversion();
+
+        // This should trigger the DMA stream for each channel's conversion
+
+        // Wait for tranfer end
+        while (!(*statusReg & (transferCompleteMask | transferErrorMask)))
+            ;
+
+        // Clear the transfer complete flag
+        *clearFlagReg |= transferCompleteMask;
+
+        // If and error has occurred (probaly due to a higher priority stream)
+        // don't update the timestamp, the values should not have been updated
+        if (*statusReg & transferErrorMask)
+        {
+            // Clear the transfer error flag
+            *clearFlagReg |= transferErrorMask;
+
+            // Signal an error
+            last_error = DMA_ERROR;
+            return last_sample;
+        }
+    }
+
+    timestamp = miosix::getTick();
+
+    return last_sample;
+}
+
+inline void InternalADC::resetRegisters()
+{
+    // Reset the ADC configuration
+    ADCx.CR1   = 0;
+    ADCx.CR2   = 0;
+    ADCx.SMPR1 = 0;
+    ADCx.SMPR2 = 0;
+    ADCx.JOFR1 = 0;
+    ADCx.JOFR2 = 0;
+    ADCx.JOFR3 = 0;
+    ADCx.JOFR4 = 0;
+    ADCx.HTR   = 0;
+    ADCx.LTR   = 0;
+    ADCx.SQR1  = 0;
+    ADCx.SQR2  = 0;
+    ADCx.SQR3  = 0;
+    ADCx.JSQR  = 0;
+}
+
+inline void InternalADC::startInjectedConversion()
+{
+    ADCx.CR2 |= ADC_CR2_JSWSTART;
+}
+
+inline void InternalADC::startRegularConversion()
+{
+    ADCx.CR2 |= ADC_CR2_SWSTART;
+}
+
+inline bool InternalADC::addInjectedChannel(Channel channel)
+{
+    // Check active channels number
+    if (activeChannels >= 4)
+    {
+        return false;
+    }
+
+    // Add the channel to the sequence, starting from position 4
+    ADCx.JSQR |= channel << (15 - activeChannels * 5);
+
+    // Update the channels number in the register
+    ADCx.JSQR |= activeChannels << 20;
+
+    // Increment the index of all enabled channels
+    for (auto i = 0; i < CH_NUM; i++)
+    {
+        if (indexMap[i] >= 0)
+        {
+            indexMap[i]++;
+        }
+    }
+
+    // Set this channel index to 0
+    indexMap[channel] = 0;
+
+    // Update the counter
+    activeChannels++;
+
+    return true;
+}
+
+inline bool InternalADC::addRegularChannel(Channel channel)
+{
+    // Check active channels number
+    if (activeChannels >= 16)
+    {
+        return false;
+    }
+
+    // Add the channel to the sequence
+    volatile uint32_t* sqrPtr;
+    switch (activeChannels % 6)
+    {
+        case 1:
+            sqrPtr = &(ADCx.SQR3);
+            break;
+        case 2:
+            sqrPtr = &(ADCx.SQR2);
+            break;
+        default:
+            sqrPtr = &(ADCx.SQR1);
+    }
+    *sqrPtr = channel << ((activeChannels % 6) * 5);
+
+    // Update the channels number in the register
+    ADCx.SQR1 |= activeChannels << 20;
+
+    // Save the index of the channel in the ADC's regular sequence
+    indexMap[channel] = activeChannels;
+
+    // Update the counter
+    activeChannels++;
+
+    return true;
+}
+
+inline void InternalADC::setChannelSampleTime(Channel channel,
+                                              SampleTime sampleTime)
+{
+    volatile uint32_t* smprPtr;
+    if (channel <= 9)
+    {
+        smprPtr = &(ADCx.SMPR2);
+    }
+    else
+    {
+        smprPtr = &(ADCx.SMPR1);
+    }
+    *smprPtr = sampleTime << (channel * 3);
+}
---
+++ src/tests/drivers/test-internal-adc-dma.cpp
@@ -0,0 +1,106 @@
+#include <drivers/adc/InternalADC/InternalADC.h>
+#include <miosix.h>
+
+ADC_TypeDef& ADCx                = *ADC3;
+DMA_Stream_TypeDef& DMAx_Streamx = *DMA2_Stream1;
+
+int main()
+{
+    // Enable DMA clock
+    RCC->AHB1ENR |= RCC_AHB1ENR_DMA2EN;  // <- CHANGE THIS!
+
+    // DMA INIT ---------------------------------------------------------------
+    {
+        // 1: Disable the stream
+
+        // Disable stream
+        DMAx_Streamx.CR &= ~DMA_SxCR_EN;
+
+        // Wait for the stream to be disabled
+        while (DMAx_Streamx.CR & DMA_SxCR_EN)
+            ;
+
+        // Reset DMA configuration
+        DMAx_Streamx.CR = 0;
+
+        // Ensures all the status bits are cleared by explicitly clearing them
+        DMA2->LIFCR = 0x0F7D0F7D;  // Mask excluding reserved bits
+        DMA2->HIFCR = 0x0F7D0F7D;
+
+        // 2-4: The memory and peripheral addresses, also the number of data
+        // items will be set by the driver
+
+        // 5: Select the DMA channel
+        DMAx_Streamx.CR |= DMA_SxCR_CHSEL_1;  // <- CHANGE THIS!
+
+        // 6: The peripheral is not intended to be the flow controller
+        // (it will function only as a trigger)
+        DMAx_Streamx.CR &= ~DMA_SxCR_PFCTRL;
+
+        // 7: Configure the stream priority to very high
+        DMAx_Streamx.CR |= DMA_SxCR_PL;
+
+        // 8: Configure fifo usage (disable => direct mode)
+        DMAx_Streamx.FCR &= ~DMA_SxFCR_DMDIS;
+
+        // 9: Other configuration
+
+        // Data transfer peripheral-to-memory
+        DMAx_Streamx.CR &= ~DMA_SxCR_DIR;
+
+        // Address increment mode
+        DMAx_Streamx.CR &= ~DMA_SxCR_PINC;  // Fixed
+        DMAx_Streamx.CR |= DMA_SxCR_MINC;   // Incremented
+
+        // Single or burst transaction (single)
+        DMAx_Streamx.CR &= ~DMA_SxCR_MBURST;
+
+        // Data width (16 bit)
+        DMAx_Streamx.CR |= DMA_SxCR_PSIZE_0;
+        DMAx_Streamx.CR |= DMA_SxCR_MSIZE_0;
+
+        // Circular mode (on)
+
+        DMAx_Streamx.CR |= DMA_SxCR_CIRC;
+
+        // Double buffer mode (off)
+        DMAx_Streamx.CR &= ~DMA_SxCR_DBM;
+
+        // Interrupt not used
+
+        // 10: Activate the stream after peripheral configuration (see warning)
+    }
+
+    // Set pins PA0 PA1 PA2 PA3 as analog input
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+    GPIOA->MODER = 0xFF;
+
+    // Enable ADC clock
+    RCC->APB2ENR |= RCC_APB2ENR_ADC3EN;  // <-- CHANGE THIS!
+
+    // Set the clock divider for the analog circuitry (/8)
+    ADC->CCR |= ADC_CCR_ADCPRE_0 | ADC_CCR_ADCPRE_1;
+    // In this case I've set the maximum value, check the datasheet for the
+    // maximum frequency the analog circuitry supports and compare it with the
+    // parent clock
+
+    InternalADC adc(ADCx, 3.0, DMAx_Streamx);
+    adc.enableChannel(InternalADC::CH0);
+    adc.enableChannel(InternalADC::CH3);
+    adc.init();
+
+    printf("Configuration completed\n");
+
+    while (1)
+    {
+        adc.sample();
+
+        printf("%8lld:%.2f\t%8lld:%.2f\n",
+               adc.getVoltage(InternalADC::CH0).adc_timestamp,
+               adc.getVoltage(InternalADC::CH0).voltage,
+               adc.getVoltage(InternalADC::CH0).adc_timestamp,
+               adc.getVoltage(InternalADC::CH3).voltage);
+
+        miosix::delayMs(1000);
+    }
+}
