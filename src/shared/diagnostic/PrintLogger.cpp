5420532324d5daf19dcf462a941a0ffa5b504c9d
Luca Erbetta <luca.erbetta@skywarder.eu>
2021-05-11 14:12:48 +0200

    [PrintLogger] Added print logger using fmt

---
+++ src/shared/diagnostic/PrintLogger.cpp
@@ -0,0 +1,124 @@
+#include "PrintLogger.h"
+
+using miosix::Lock;
+
+static string getLevelString(uint8_t level)
+{
+    switch (level)
+    {
+        case LOGL_DEBUG:
+            return "DEBUG";
+        case LOGL_INFO:
+            return "INFO";
+        case LOGL_WARNING:
+            return "WARNING";
+        case LOGL_ERROR:
+            return "ERROR";
+        case LOGL_CRITICAL:
+            return "CRITICAL";
+        default:
+            return std::to_string(level);
+    }
+}
+void LogSink::log(const LogRecord& record)
+{
+    using namespace fmt::literals;
+    if (record.level >= min_level)
+    {
+        float ts      = miosix::getTick() / 1000.0f;
+        int min       = ts / 60;
+        string ts_str = fmt::format("{:02d}:{:06.3f}", min, (ts - min * 60));
+        logImpl(fmt::format(format, "ts"_a = ts_str, "file"_a = record.file,
+                            "line"_a = record.line, "fun"_a = record.function,
+                            "lvl"_a  = getLevelString(record.level),
+                            "name"_a = record.name, "msg"_a = record.message));
+    }
+}
+
+void FileLogSink::logImpl(string l)
+{
+    Lock<FastMutex> lock(mutex);
+    fputs(l.c_str(), f);
+}
+
+PrintLogger PrintLogger::getChild(string name)
+{
+    return PrintLogger(parent, this->name + "." + name);
+}
+
+LogRecord PrintLogger::buildLogRecord(uint8_t level, string function,
+                                      string file, int line,
+                                      fmt::string_view format,
+                                      fmt::format_args args)
+{
+    LogRecord record;
+    record.level    = level;
+    record.function = function;
+    record.file     = file;
+    record.line     = line;
+    record.name     = name;
+    try
+    {
+        record.message = fmt::vformat(format, args);
+    }
+    catch (const std::exception& e)
+    {
+        level          = ERROR;
+        record.message = "FMT Formatting error! " + string(e.what());
+    }
+
+    return record;
+}
+
+void PrintLogger::vlog(uint8_t level, string function, string file, int line,
+                       fmt::string_view format, fmt::format_args args)
+{
+    parent.log(buildLogRecord(level, function, file, line, format, args));
+}
+
+void PrintLogger::vlogAsync(uint8_t level, string function, string file, int line,
+                       fmt::string_view format, fmt::format_args args)
+{
+    parent.logAsync(buildLogRecord(level, function, file, line, format, args));
+}
+
+void Logging::log(const LogRecord& record)
+{
+    for (auto& s : sinks)
+    {
+        s->log(record);
+    }
+}
+
+void Logging::logAsync(const LogRecord& record) { async_log.log(record); }
+
+Logging::AsyncLogger::AsyncLogger(Logging& parent) : parent(parent) {}
+
+void Logging::AsyncLogger::log(const LogRecord& record)
+{
+    {
+        Lock<FastMutex> l(mutex);
+        records.put(record);
+    }
+
+    cv.signal();
+}
+
+void Logging::AsyncLogger::run()
+{
+    while (!shouldStop())
+    {
+        LogRecord rec;
+        {
+            Lock<FastMutex> l(mutex);
+            while (records.isEmpty())
+            {
+                cv.wait(mutex);
+            }
+
+            rec = records.pop();
+        }
+
+        parent.log(rec);
+    }
+}
---
+++ src/shared/diagnostic/PrintLogger.h
@@ -0,0 +1,240 @@
+/**
+ * Copyright (c) 2021 Luca Erbetta
+ * Authors: Luca Erbetta (luca.erbetta105@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#pragma once
+
+#include <fmt/format.h>
+#include <miosix.h>
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "ActiveObject.h"
+#include "Singleton.h"
+#include "utils/collections/CircularBuffer.h"
+
+using std::string;
+using std::unique_ptr;
+using std::vector;
+
+using miosix::ConditionVariable;
+using miosix::FastMutex;
+
+#ifndef DEFAULT_STDOUT_LOG_LEVEL
+#define DEFAULT_STDOUT_LOG_LEVEL 0
+#endif
+
+static constexpr unsigned int ASYNC_LOG_BUFFER_SIZE = 100;
+
+
+class Logging;
+
+struct LogRecord
+{
+    int level;
+    string function;
+    string file;
+    int line;
+    string name;
+    string message;
+};
+
+enum LogLevel : uint8_t
+{
+    LOGL_NOTSET   = 0,
+    LOGL_DEBUG    = 10,
+    LOGL_INFO     = 20,
+    LOGL_WARNING  = 30,
+    LOGL_ERROR    = 40,
+    LOGL_CRITICAL = 50
+};
+
+class LogSink
+{
+public:
+    LogSink() {}
+    LogSink(const LogSink&) = delete;
+    LogSink& operator=(const LogSink&) = delete;
+
+    void log(const LogRecord& record);
+
+    void enable() { enabled = false; }
+
+    void disable() { enabled = false; }
+
+    void setLevel(uint8_t level) { min_level = level; }
+
+    int getLevel() { return min_level; }
+
+    void setFormatString(string format) { this->format = format; }
+
+protected:
+    virtual void logImpl(string l) = 0;
+
+private:
+    bool enabled      = true;
+    uint8_t min_level = LOGL_NOTSET;
+    string format     = "{ts} {file}:{line} {fun} {lvl} [{name}] {msg}\n";
+};
+
+class FileLogSink : public LogSink
+{
+public:
+    FileLogSink(FILE* f) : f(f) {}
+
+protected:
+    void logImpl(string l);
+
+private:
+    FILE* f;
+    FastMutex mutex;
+};
+
+class PrintLogger
+{
+public:
+    PrintLogger(Logging& logging, string name) : parent(logging), name(name) {}
+
+    PrintLogger getChild(string name);
+
+    template <typename... Args>
+    void log(uint8_t level, string function, string file, int line,
+             string format, Args&&... args)
+    {
+        vlog(level, function, file, line, format,
+                           fmt::make_args_checked<Args...>(format, args...));        
+    }
+
+    template <typename... Args>
+    void logAsync(uint8_t level, string function, string file, int line,
+                  string format, Args&&... args)
+    {
+        vlogAsync(level, function, file, line, format,
+                           fmt::make_args_checked<Args...>(format, args...));  
+    }
+
+private:
+    void vlog(uint8_t level, string function, string file,
+                             int line, fmt::string_view format,
+                             fmt::format_args args);
+    void vlogAsync(uint8_t level, string function, string file,
+                             int line, fmt::string_view format,
+                             fmt::format_args args);
+
+    LogRecord buildLogRecord(uint8_t level, string function, string file,
+                             int line, fmt::string_view format,
+                             fmt::format_args args);
+
+    Logging& parent;
+    string name;
+};
+
+class Logging : public Singleton<Logging>
+{
+    friend class Singleton<Logging>;
+    friend class PrintLogger;
+
+public:
+    PrintLogger getLogger(string name) { return PrintLogger(*this, name); }
+
+    void addLogSink(unique_ptr<LogSink>& sink)
+    {
+        sinks.push_back(std::move(sink));
+    }
+
+    LogSink& getSerialLogSink() { return *sinks.at(0); }
+
+    void startAsyncLogger()
+    {
+        async_log.start();
+    }
+
+    void log(const LogRecord& record);
+    void logAsync(const LogRecord& record);
+
+private:
+    class AsyncLogger : public ActiveObject
+    {
+    public:
+        AsyncLogger(Logging& parent);
+        void log(const LogRecord& record);
+
+    protected:
+        void run() override;
+
+    private:
+        Logging& parent;
+        CircularBuffer<LogRecord, ASYNC_LOG_BUFFER_SIZE> records;
+        FastMutex mutex;
+        ConditionVariable cv;
+    };
+
+    Logging() : async_log(*this)
+    {
+        unique_ptr<FileLogSink> serial = std::make_unique<FileLogSink>(stdout);
+        serial->setLevel(DEFAULT_STDOUT_LOG_LEVEL);
+        sinks.push_back(std::move(serial));
+    }
+
+    AsyncLogger async_log;
+    vector<unique_ptr<LogSink>> sinks;
+};
+
+#define LOG(logger, level, ...) \
+    logger.log(level, __FUNCTION__, __FILE__, __LINE__, __VA_ARGS__)
+
+#define LOG_DEBUG(logger, ...) \
+    LOG(logger, LOGL_DEBUG, __VA_ARGS__)
+
+#define LOG_INFO(logger, ...) \
+    LOG(logger, LOGL_INFO, __VA_ARGS__)
+
+#define LOG_WARN(logger, ...) \
+    LOG(logger, LOGL_WARNING, __VA_ARGS__)
+
+#define LOG_ERR(logger, ...) \
+    LOG(logger, LOGL_ERROR, __VA_ARGS__)
+
+#define LOG_CRIT(logger, ...) \
+    LOG(logger, LOGL_CRITICAL, __VA_ARGS__)
+
+
+
+#define LOG_ASYNC(logger, level, ...) \
+    logger.logAsync(level, __FUNCTION__, __FILE__, __LINE__, __VA_ARGS__)
+
+#define LOG_DEBUG_ASYNC(logger, ...) \
+    LOG_ASYNC(logger, LOGL_DEBUG, __VA_ARGS__)
+
+#define LOG_INFO_ASYNC(logger, ...) \
+    LOG_ASYNC(logger, LOGL_INFO, __VA_ARGS__)
+
+#define LOG_WARN_ASYNC(logger, ...) \
+    LOG_ASYNC(logger, LOGL_WARNING, __VA_ARGS__)
+
+#define LOG_ERR_ASYNC(logger, ...) \
+    LOG_ASYNC(logger, LOGL_ERROR, __VA_ARGS__)
+
+#define LOG_CRIT_ASYNC(logger, ...) \
+    LOG_ASYNC(logger, LOGL_CRITICAL, __VA_ARGS__)
