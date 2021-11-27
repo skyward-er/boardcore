ddd1a9330f839e73e85798b0bdba510486ecd3b6
Luca Erbetta <luca.erbetta@skywarder.eu>
2021-11-27 17:59:52 +0100

    [Canbus] Fixed filters

--- src/shared/drivers/canbus/Filters.h
+++ src/shared/drivers/canbus/Filters.h
@@ -54,18 +54,52 @@ struct Filter
         : fifo(fifo), scale(scale), mode(mode)
     {
     }
+protected:
+    static uint32_t packRegister32(uint32_t id, uint8_t ext, uint8_t rtr, bool is_ext)
+    {
+        uint32_t reg = 0;
+        if (is_ext)
+        {
+            reg = ((id & 0x1FFFFFFF) << 3);
+        }
+        else
+        {
+            reg = ((id & 0x7FF) << 21);
+        }
+
+        reg |= ((ext & 0x1) << 2) | ((rtr & 0x1) << 1);
+        return reg;
+    }
+
+    static uint16_t packRegister16(uint32_t id, uint8_t ext, uint8_t rtr, bool is_ext)
+    {
+        uint16_t reg = 0;
+        if (is_ext)
+        {
+            reg = ((id >> 18) & 0x7FF) << 5;
+            reg |= ((id >> 15) & 0x07);
+        }
+        else
+        {
+            reg = ((id & 0x7FF) << 5);
+        }
+
+        reg |= ((ext & 0x1) << 3) | ((rtr & 0x1) << 4);
+        return reg;
+    }
 };
 
 struct Mask32Filter : public Filter
 {
-    Mask32Filter(uint32_t id, uint32_t mask, uint8_t fifo)
+    Mask32Filter(uint32_t id, uint32_t id_mask, uint8_t ext, uint8_t ext_mask,
+                 uint8_t rtr, uint8_t rtr_mask, uint8_t fifo)
         : Filter(FilterScale::SINGLE32, FilterMode::MASK, fifo)
     {
         scale = FilterScale::SINGLE32;
         mode  = FilterMode::MASK;
 
-        FR1 = id;
-        FR2 = mask;
+        FR1 = packRegister32(id, ext, rtr, ext);
+        FR2 = packRegister32(id_mask, ext_mask, rtr_mask, ext);
     }
 };
 
@@ -76,14 +110,14 @@ struct ID32Filter : public Filter
     {
     }
 
-    bool addID(uint32_t id)
+    bool addID(uint32_t id, bool ext, uint8_t rtr)
     {
         if (id_cnt == 2)
             return false;
 
         uint32_t* reg = id_cnt < 1 ? &FR1 : &FR2;
 
-        *reg = id;
+        *reg = packRegister32(id, ext, rtr, ext);
         ++id_cnt;
         return true;
     }
@@ -99,14 +133,15 @@ struct Mask16Filter : public Filter
     {
     }
 
-    bool addID(uint16_t id, uint16_t mask)
+    bool addID(uint32_t id, uint32_t id_mask, uint8_t ext, uint8_t ext_mask,
+               uint8_t rtr, uint8_t rtr_mask)
     {
         if (id_cnt == 2)
             return false;
 
         uint32_t* reg = id_cnt < 1 ? &FR1 : &FR2;
 
-        *reg = id | mask << 16;
+        *reg = packRegister16(id, ext, rtr, ext) | (packRegister16(id_mask, ext_mask, rtr_mask, ext) << 16);
         ++id_cnt;
         return true;
     }
@@ -121,7 +156,7 @@ struct ID16Filter : public Filter
     {
     }
 
-    bool addID(uint16_t id)
+    bool addID(uint32_t id, uint8_t ext, uint8_t rtr)
     {
         if (id_cnt == 4)
             return false;
@@ -131,7 +166,7 @@ struct ID16Filter : public Filter
         const uint32_t bitmask = 0xFFFF;
 
         *reg &= ~(bitmask << (16 * (id_cnt % 2)));
-        *reg |= id << (16 * (id_cnt % 2));
+        *reg |= packRegister16(id, ext, rtr, ext) << (16 * (id_cnt % 2));
         ++id_cnt;
         return true;
     }
