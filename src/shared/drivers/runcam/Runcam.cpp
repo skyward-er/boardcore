56d43fe89658623fedf8fd8193756045b982df0d
Federico Mandelli <federico.mandelli@skywarder.eu>
2021-12-15 18:40:52 +0100

    [Runcam] Fixed merging errors

--- src/shared/drivers/runcam/Runcam.cpp
+++ src/shared/drivers/runcam/Runcam.cpp
@@ -24,6 +24,8 @@
 #include <Common.h>
 #include <miosix.h>
 
+using namespace Boardcore;
+
 Runcam::Runcam() : Runcam(defaultPortNumber) {}
 
 Runcam::Runcam(unsigned int portNumber)
@@ -50,14 +52,14 @@ bool Runcam::close()
     // Sensor not init
     if (!isInit)
     {
-        // Boardcore::TRACE("Runcam not initilized");
+        TRACE("Runcam not initilized");
         return true;
     }
 
     // Close the serial
     if (!(serialInterface->closeSerial()))
     {
-        // Boardcore::TRACE("Unable to close serial communication");
+        TRACE("Unable to close serial communication");
         return false;
     }
 
@@ -78,13 +80,13 @@ bool Runcam::init()
     // If already initialized
     if (isInit)
     {
-        // Boardcore::TRACE("Connection with the camera already initialized");
+        TRACE("Connection with the camera already initialized");
         return true;
     }
 
     if (!configureSerialCommunication())
     {
-        // Boardcore::TRACE("Unable to config camera port");
+        TRACE("Unable to config camera port");
         return false;
     }
 
