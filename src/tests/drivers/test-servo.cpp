4a56b658889b7a3daf5ae0118775ee89e7a91e04
Alberto Nidasio <alberto.nidasio@skywarder.eu>
2022-04-06 14:10:32 +0200

    [Servo] Updated test entrypoint and some comments

--- src/tests/drivers/test-servo.cpp
+++ src/tests/drivers/test-servo.cpp
@@ -22,45 +22,80 @@
 
 #include <drivers/servo/Servo.h>
 #include <miosix.h>
+#include <scheduler/TaskScheduler.h>
 #include <utils/ClockUtils.h>
+
+#include <iostream>
+
+/**
+ * The test uses 4 gpio pins:
+ * - PB4: TIM3-CH1
+ * - PA7: TIM3-CH2
+ * - PC8: TIM3-CH3
+ * - PB7: TIM4-CH2
+ */
 
 using namespace Boardcore;
 using namespace miosix;
 
-using ps1 = Gpio<GPIOB_BASE, 4>;
-using ps2 = Gpio<GPIOA_BASE, 7>;
-using ps3 = Gpio<GPIOB_BASE, 8>;
+GpioPin pin1(GPIOB_BASE, 4);
+GpioPin pin2(GPIOA_BASE, 7);
+GpioPin pin3(GPIOC_BASE, 8);
+GpioPin pin4(GPIOB_BASE, 7);
+
+Servo s1(TIM3, TimerUtils::Channel::CHANNEL_1);
+Servo s2(TIM3, TimerUtils::Channel::CHANNEL_2);
+Servo s3(TIM3, TimerUtils::Channel::CHANNEL_3);
+Servo s4(TIM4, TimerUtils::Channel::CHANNEL_2);
+
+// Position to cycle through for the servo 1, 2 and 3
+float positions[] = {0, 0.5, 1.0};
+int lastPosition  = 0;
+
+void moveServo()
+{
+    s1.setPosition(positions[lastPosition % 3]);
+    s2.setPosition(positions[(lastPosition + 1) % 3]);
+    s3.setPosition(positions[(lastPosition + 2) % 3]);
+
+    lastPosition++;
+}
 
 int main()
 {
-    ps1::mode(Mode::ALTERNATE);
-    ps2::mode(Mode::ALTERNATE);
-    ps3::mode(Mode::ALTERNATE);
+    pin1.mode(Mode::ALTERNATE);
+    pin1.alternateFunction(2);
+    pin2.mode(Mode::ALTERNATE);
+    pin2.alternateFunction(2);
+    pin3.mode(Mode::ALTERNATE);
+    pin3.alternateFunction(2);
+    pin4.mode(Mode::ALTERNATE);
+    pin4.alternateFunction(2);
 
-    ps1::alternateFunction(2);
-    ps2::alternateFunction(2);
-    ps3::alternateFunction(3);
-
-    Servo s1(TIM3, TimerUtils::Channel::CHANNEL_1);
-    Servo s2(TIM3, TimerUtils::Channel::CHANNEL_2);
-    Servo s3(TIM10, TimerUtils::Channel::CHANNEL_1);
-
+    // Enable the timers
     s1.enable();
     s2.enable();
     s3.enable();
+    s4.enable();
 
-    float pos[] = {0, 0.5, 1.0};
+    // Start a periodic task to move the first three servos
+    TaskScheduler scheduler;
+    scheduler.addTask(&moveServo, 2 * 1000, 1);
+    scheduler.start();
 
-    s1.setPosition(pos[0]);
-    s2.setPosition(pos[1]);
-    s3.setPosition(pos[2]);
+    // Control the fourth servo manually
+    float percentage;
 
-    for (int i = 0;; i++)
+    printf("Please enter a percentage for the servo ");
+    printf("or anything else to exit: ");
+
+    while (std::cin >> percentage)
     {
-        s1.setPosition(pos[i % 3]);
-        s2.setPosition(pos[(i + 1) % 3]);
-        s3.setPosition(pos[(i + 2) % 3]);
+        s4.setPosition(percentage / 100);
 
-        Thread::sleep(2000);
+        printf("Position set to: %.2f%%\n", percentage);
+
+        printf("Please enter a percentage for the servo ");
+        printf("or anything else to exit: ");
     }
 }
