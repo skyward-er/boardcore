34023d063882d1a45024238371e7ea4febaa5e18
Matteo Pignataro <matteo.pignataro@skywarder.eu>
2022-04-07 13:54:27 +0200

    [SensorManager] Removed scheduler start, ideally i want to start the scheduler in OBSW

--- src/shared/sensors/SensorManager.cpp
+++ src/shared/sensors/SensorManager.cpp
@@ -50,7 +50,7 @@ SensorManager::~SensorManager()
         delete scheduler;
 }
 
-bool SensorManager::start() { return scheduler->start() && initResult; }
+bool SensorManager::start() { return initResult; }
 
 void SensorManager::stop() { scheduler->stop(); }
 
