9b19dede330cc4b168031ee51b6c999431c6674b
Alberto Nidasio <alberto.nidasio@skywarder.eu>
2022-05-08 21:19:28 +0200

    [mavlink] Updated submodule

--- src/shared/utils/collections/SyncPacketQueue.h
+++ src/shared/utils/collections/SyncPacketQueue.h
@@ -1,5 +1,5 @@
 /* Copyright (c) 2019-2022 Skyward Experimental Rocketry
- * Author: Alvise de'Faveri Tron, Davide Mor, Alberto Nidasio
+ * Authors: Alvise de'Faveri Tron, Davide Mor, Alberto Nidasio
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
