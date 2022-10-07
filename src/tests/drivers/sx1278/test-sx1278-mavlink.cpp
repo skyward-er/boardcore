e87d50f67572868d1beaa13938945c80e0299aab
Davide Mor <davide.mor@skywarder.eu>
2022-10-07 23:36:31 +0200

    [sx1278] Updated mavlink test entrypoint to simulate flight telemetries

--- src/tests/drivers/sx1278/test-sx1278-mavlink.cpp
+++ src/tests/drivers/sx1278/test-sx1278-mavlink.cpp
@@ -23,6 +23,8 @@
 #include <drivers/interrupt/external_interrupts.h>
 #include <radio/SX1278/SX1278.h>
 
+#include <thread>
+
 #include "common.h"
 
 // Ignore warnings, as we don't want to change third party generated files to
@@ -30,7 +32,7 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wcast-align"
 #pragma GCC diagnostic ignored "-Waddress-of-packed-member"
-#include <mavlink_lib/lynx/mavlink.h>
+#include <mavlink_lib/pyxis/mavlink.h>
 #pragma GCC diagnostic pop
 
 #include <radio/MavlinkDriver/MavlinkDriver.h>
@@ -38,125 +40,148 @@
 using namespace Boardcore;
 using namespace miosix;
 
-static const unsigned int queue_len          = 10;
-static const unsigned int packet_size        = 256;
-static const unsigned int silence_after_send = 250;
-static const unsigned int max_pkt_age        = 1000;
-static const unsigned int ping_period        = 1000;
+constexpr uint32_t RADIO_PKT_LENGTH     = 63;
+constexpr uint32_t RADIO_OUT_QUEUE_SIZE = 10;
+constexpr uint32_t RADIO_MAV_MSG_LENGTH = MAVLINK_MAX_DIALECT_PAYLOAD_SIZE;
+constexpr size_t MAV_OUT_BUFFER_MAX_AGE = 200;
+constexpr uint32_t FLIGHT_TM_PERIOD     = 250;
+constexpr uint32_t STATS_TM_PERIOD      = 2000;
 
 // Mavlink out buffer with 10 packets, 256 bytes each.
-using Mav = MavlinkDriver<packet_size, queue_len>;
+using Mav =
+    MavlinkDriver<RADIO_PKT_LENGTH, RADIO_OUT_QUEUE_SIZE, RADIO_MAV_MSG_LENGTH>;
 
 Mav* channel;
-
-SPIBus bus(SPI3);
-
-GpioPin sck(GPIOC_BASE, 10);
-GpioPin miso(GPIOC_BASE, 11);
-GpioPin mosi(GPIOC_BASE, 12);
-GpioPin cs(GPIOA_BASE, 1);
-GpioPin dio(GPIOC_BASE, 15);
-
 SX1278* sx1278 = nullptr;
 
-void __attribute__((used)) EXTI15_IRQHandlerImpl()
+#if defined _BOARD_STM32F429ZI_SKYWARD_GS_V2
+#include "interfaces-impl/hwmapping.h"
+
+#define USE_RA01_PC13
+
+#ifdef USE_RA01_PC13  // use ra01
+using cs   = peripherals::ra01::pc13::cs;
+using dio0 = peripherals::ra01::pc13::dio0;
+#else
+using cs   = peripherals::ra01::pe4::cs;
+using dio0 = peripherals::ra01::pe4::dio0;
+#endif
+
+#define SX1278_SPI SPI4
+
+#else
+#error "Target not supported"
+#endif
+
+#if defined _BOARD_STM32F429ZI_SKYWARD_GS_V2
+#ifdef USE_RA01_PC13
+void __attribute__((used)) EXTI6_IRQHandlerImpl()
+#else
+void __attribute__((used)) EXTI3_IRQHandlerImpl()
+#endif
+#else
+#error "Target not supported"
+#endif
 {
     if (sx1278)
         sx1278->handleDioIRQ();
 }
 
-/// Initialize stm32f407g board.
 void initBoard()
 {
-    {
-        miosix::FastInterruptDisableLock dLock;
-
-        // Enable SPI3
-        RCC->APB1ENR |= RCC_APB1ENR_SPI3EN;
-        RCC_SYNC();
-
-        // Setup SPI pins
-        sck.mode(miosix::Mode::ALTERNATE);
-        sck.alternateFunction(6);
-        miso.mode(miosix::Mode::ALTERNATE);
-        miso.alternateFunction(6);
-        mosi.mode(miosix::Mode::ALTERNATE);
-        mosi.alternateFunction(6);
-
-        cs.mode(miosix::Mode::OUTPUT);
-        dio.mode(miosix::Mode::INPUT);
-    }
-
-    cs.high();
-    enableExternalInterrupt(dio.getPort(), dio.getNumber(),
+#if defined _BOARD_STM32F429ZI_SKYWARD_GS_V2
+    GpioPin dio0 = dio0::getPin();
+    enableExternalInterrupt(dio0.getPort(), dio0.getNumber(),
                             InterruptTrigger::RISING_EDGE);
+#else
+#error "Target not supported"
+#endif
 }
 
-/**
- * @brief Receive function: print the received message id and send an ACK.
- */
-static void onReceive(Mav* channel, const mavlink_message_t& msg)
+void onReceive(Mav* channel, const mavlink_message_t& msg)
 {
-    printf("[TmtcTest] Received message, id: %d! Sending ack\n", msg.msgid);
-
     // Prepare ack messages
     mavlink_message_t ackMsg;
     mavlink_msg_ack_tm_pack(1, 1, &ackMsg, msg.msgid, msg.seq);
 
     // Send the ack back to the sender
-    bool ackSent = channel->enqueueMsg(ackMsg);
-
-    if (!ackSent)
-        printf("[Receiver] Could not enqueue ack\n");
+    channel->enqueueMsg(ackMsg);
 }
 
-/**
- * @brief This test enqueues a ping message every second and replies to every
- * received message with an ACK.
- */
+void flightTmLoop()
+{
+    while (1)
+    {
+        long long start = miosix::getTick();
+
+        mavlink_message_t msg;
+        mavlink_rocket_flight_tm_t tm = {0};
+        mavlink_msg_rocket_flight_tm_encode(171, 96, &msg, &tm);
+
+        channel->enqueueMsg(msg);
+
+        Thread::sleepUntil(start + FLIGHT_TM_PERIOD);
+    }
+}
+
+void statsTmLoop()
+{
+    while (1)
+    {
+        long long start = miosix::getTick();
+
+        mavlink_message_t msg;
+        mavlink_rocket_stats_tm_t tm = {0};
+        mavlink_msg_rocket_stats_tm_encode(171, 96, &msg, &tm);
+
+        channel->enqueueMsg(msg);
+
+        Thread::sleepUntil(start + STATS_TM_PERIOD);
+    }
+}
+
 int main()
 {
     initBoard();
 
-    SX1278::Config config;
+    // Run default configuration
+    SX1278::Config config = {
+        .freq_rf  = 412000000,
+        .freq_dev = 25000,
+        .bitrate  = 19200,
+        .rx_bw    = SX1278::RxBw::HZ_83300,
+        .afc_bw   = SX1278::RxBw::HZ_125000,
+        .ocp      = 120,
+        .power    = 17,
+        .shaping  = SX1278::Shaping::GAUSSIAN_BT_1_0,
+    };
+
     SX1278::Error err;
+
+    SPIBus bus(SX1278_SPI);
+    GpioPin cs = cs::getPin();
 
     sx1278 = new SX1278(bus, cs);
 
     printf("\n[sx1278] Configuring sx1278...\n");
-    printConfig(config);
     if ((err = sx1278->init(config)) != SX1278::Error::NONE)
     {
         printf("[sx1278] sx1278->init error: %s\n", stringFromErr(err));
-        return -1;
+
+        while (1)
+            Thread::wait();
     }
 
-    channel = new Mav(sx1278, &onReceive, silence_after_send, max_pkt_age);
+    printConfig(config);
 
+    channel = new Mav(sx1278, &onReceive, 0, MAV_OUT_BUFFER_MAX_AGE);
     channel->start();
 
-    // Send function: enqueue a ping every second
+    std::thread flight_tm_loop([]() { flightTmLoop(); });
+    std::thread stats_tm_loop([]() { statsTmLoop(); });
+
     while (1)
-    {
-        printf("[TmtcTest] Enqueueing ping\n");
-
-        // Create a Mavlink message
-        mavlink_message_t pingMsg;
-        mavlink_msg_ping_tc_pack(1, 1, &pingMsg, miosix::getTick());
-
-        // Send the message
-        bool ackSent = channel->enqueueMsg(pingMsg);
-
-        if (!ackSent)
-            printf("[TmtcTest] Could not enqueue ping\n");
-
-        // LED blink
-        ledOn();
-        miosix::delayMs(200);
-        ledOff();
-
-        miosix::Thread::sleep(ping_period);
-    }
+        Thread::wait();
 
     return 0;
 }
