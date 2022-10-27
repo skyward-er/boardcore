f24993bdd234129b1800ef981b70c78b381d44b9
Matteo Pignataro <matteo.pignataro@skywarder.eu>
2022-10-27 14:09:28 +0200

    [ModuleManager] First commit, needs to be tested

---
+++ src/shared/utils/ModuleManager/ModuleManager.hpp
@@ -0,0 +1,129 @@
+/* Copyright (c) 2022 Skyward Experimental Rocketry
+ * Authors: Matteo Pignataro, Davide Mor
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#pragma once
+
+#include <stdint.h>
+
+namespace Boardcore
+{
+namespace ModuleManagerVariables
+{
+/**
+ * @brief Unique id sequence for classes
+ */
+extern uint8_t currentId;
+}  // namespace ModuleManagerVariables
+
+class Module
+{
+public:
+    virtual ~Module() = default;
+};
+
+class ModuleManager
+{
+private:
+    /**
+     * @brief Array that contains all the possible modules created with the
+     * maximum number of modules
+     */
+    Module *modules[256] = {nullptr};
+
+    /**
+     * @brief This function "assigns" to every type a unique sequential id
+     * based on the already assigned ones
+     */
+    template <typename T>
+    uint8_t getId()
+    {
+        // This thing works because a new static variable newId is created for
+        // every type T and the initial assignment is "called" only when the
+        // static variable is created
+        static uint8_t newId = ModuleManagerVariables::currentId == 255
+                                   ? 255
+                                   : ModuleManagerVariables::currentId++;
+        return newId;
+    }
+
+public:
+    /**
+     * @brief Inserts the module inside the array if not already present
+     */
+    template <typename T>
+    void insert(T *element)
+    {
+        // Verify that T is a subclass of module
+        static_assert(std::is_base_of<Module, T>(),
+                      "Class must be subclass of Module");
+
+        // Take the module type
+        uint8_t id = getId<T>();
+
+        // Only if the module isn't already present i add it
+        if (modules[id] == nullptr)
+            modules[id] = static_cast<Module *>(element);
+    }
+
+    /**
+     * @brief Removes a particular module inside the array if present
+     */
+    template <typename T>
+    void remove()
+    {
+        // Take the module type
+        uint8_t id = getId<T>();
+
+        // Only if the module is actually present i remove it
+        if (modules[id] != nullptr)
+        {
+            // Call the destruction method
+            delete modules[id];
+
+            // Then remove the entry from the array
+            modules[id] = nullptr;
+        }
+    }
+
+    /**
+     * @brief Get the Module object if present. Otherwise it creates one
+     * @return Software module
+     */
+    template <class T>
+    T *get()
+    {
+        // Retrieve the module type
+        uint8_t id = getId<T>();
+
+        // If the module is actually present
+        if (modules[id] != nullptr)
+        {
+            // If the types are the same return the casted one
+            return static_cast<T *>(modules[id]);
+        }
+
+        // I don't have any module with that type in the array so i instantiate
+        // one
+        // insert<T>(new T());
+        return static_cast<T *>(modules[id]);
+    }
+};
+}  // namespace Boardcore
---
+++ src/tests/utils/test-softwaremodules.cpp
@@ -0,0 +1,98 @@
+/* Copyright (c) 2022 Skyward Experimental Rocketry
+ * Authors: Matteo Pignataro
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#include <iostream>
+#include <utils/ModuleManager/ModuleManager.hpp>
+
+using namespace Boardcore;
+using namespace std;
+
+namespace Boardcore
+{
+class SensorsModule : public Module
+{
+protected:
+    int dummy = 0;
+
+public:
+    virtual int getDummy() { return dummy; };
+    virtual void toggleDummy() = 0;
+};
+
+class HILSensors : public SensorsModule
+{
+public:
+    void toggleDummy() override { dummy = dummy == 0 ? 2000 : 0; }
+};
+
+class Sensors : public SensorsModule
+{
+public:
+    void toggleDummy() override { dummy = dummy == 0 ? 1000 : 0; }
+};
+
+class Radio : public Module
+{
+    int pippo = 0;
+
+public:
+    void setPippo(int p) { pippo = p; }
+    int getPippo() { return pippo; }
+};
+}  // namespace Boardcore
+
+int main()
+{
+    ModuleManager mod{};
+
+    mod.insert<SensorsModule>(new HILSensors());
+
+    // Ignored insertion because a sensorsModule is already present
+    mod.insert<SensorsModule>(new Sensors());
+
+    // First correct insertion using the same class
+    mod.insert<Radio>(new Radio());
+
+    // Test
+    cout << mod.get<SensorsModule>()->getDummy() << ","
+         << mod.get<Radio>()->getPippo() << endl;
+
+    mod.get<SensorsModule>()->toggleDummy();
+
+    cout << mod.get<SensorsModule>()->getDummy() << ","
+         << mod.get<Radio>()->getPippo() << endl;
+
+    mod.get<Radio>()->setPippo(1000);
+    mod.remove<SensorsModule>();
+
+    cout << mod.get<SensorsModule>()->getDummy() << ","
+         << mod.get<Radio>()->getPippo() << endl;
+
+    // mod.insert<SensorsModule>(new HILSensors());
+
+    // mod.get<SensorsModule>()->toggleDummy();
+
+    // mod.get<Radio>();
+
+    // cout << mod.get<SensorsModule>()->getDummy() << endl;
+
+    return 0;
+}
