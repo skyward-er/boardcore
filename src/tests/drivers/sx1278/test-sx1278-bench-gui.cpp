9d21c056b26b19e71ea5be26567c0d40b68f2b0c
Davide Mor <davide.mor@skywarder.eu>
2022-11-20 00:39:27 +0100

    [Groundstation] Initial extra interrupts setup and testing

--- src/tests/drivers/sx1278/test-sx1278-bench-gui.cpp
+++ src/tests/drivers/sx1278/test-sx1278-bench-gui.cpp
@@ -35,16 +35,13 @@ using namespace Boardcore;
 using namespace miosix;
 using namespace mxgui;
 
-#if defined _BOARD_STM32F429ZI_SKYWARD_GS
+#if defined _BOARD_STM32F429ZI_SKYWARD_GS_V2
 #include "interfaces-impl/hwmapping.h"
 
-#if 1  // use ra01
-using cs   = peripherals::ra01::cs;
-using dio0 = peripherals::ra01::dio0;
-#else
-using cs   = peripherals::sx127x::cs;
-using dio0 = peripherals::sx127x::dio0;
-#endif
+using cs   = peripherals::ra01::pc13::cs;
+using dio0 = peripherals::ra01::pc13::dio0;
+using dio1 = peripherals::ra01::pc13::dio1;
+using dio3 = peripherals::ra01::pc13::dio3;
 
 using sck  = interfaces::spi4::sck;
 using miso = interfaces::spi4::miso;
@@ -52,30 +49,47 @@ using mosi = interfaces::spi4::mosi;
 
 #define SX1278_SPI SPI4
 
+#define IRQ_DIO0 EXTI6_IRQHandlerImpl
+#define IRQ_DIO1 EXTI5_IRQHandlerImpl
+#define IRQ_DIO3 EXTI11_IRQHandlerImpl
+
 #else
 #error "Target not supported"
 #endif
 
-GUI *gui = nullptr;
-
-#if defined _BOARD_STM32F429ZI_SKYWARD_GS
-void __attribute__((used)) EXTI6_IRQHandlerImpl()
-#else
-#error "Target not supported"
-#endif
+void __attribute__((used)) IRQ_DIO0()
 {
     if (sx1278)
         sx1278->handleDioIRQ();
 }
 
+void __attribute__((used)) IRQ_DIO1()
+{
+    // if (sx1278)
+    //     sx1278->handleDioIRQ();
+}
+
+void __attribute__((used)) IRQ_DIO3()
+{
+    // if (sx1278)
+    //     sx1278->handleDioIRQ();
+}
+
 void initBoard()
 {
-#if defined _BOARD_STM32F429ZI_SKYWARD_GS
-    enableExternalInterrupt(GPIOF_BASE, 6, InterruptTrigger::RISING_EDGE);
-#else
-#error "Target not supported"
-#endif
+    GpioPin dio0_pin = dio0::getPin();
+    GpioPin dio1_pin = dio1::getPin();
+    GpioPin dio3_pin = dio3::getPin();
+
+    enableExternalInterrupt(dio0_pin.getPort(), dio0_pin.getNumber(),
+                            InterruptTrigger::RISING_EDGE);
+    enableExternalInterrupt(dio1_pin.getPort(), dio1_pin.getNumber(),
+                            InterruptTrigger::RISING_EDGE);
+    enableExternalInterrupt(dio3_pin.getPort(), dio3_pin.getNumber(),
+                            InterruptTrigger::RISING_EDGE);
 }
+
+GUI *gui = nullptr;
 
 void initGUI()
 {
@@ -96,7 +110,16 @@ int main()
     initGUI();
 
     // Run default configuration
-    SX1278::Config config;
+    SX1278::Config config = {
+        .freq_rf  = 422075000,
+        .freq_dev = 25000,
+        .bitrate  = 19200,
+        .rx_bw    = Boardcore::SX1278::RxBw::HZ_83300,
+        .afc_bw   = Boardcore::SX1278::RxBw::HZ_125000,
+        .ocp      = 120,
+        .power    = 17,
+        .shaping  = Boardcore::SX1278::Shaping::GAUSSIAN_BT_1_0,
+        .dc_free  = Boardcore::SX1278::DcFree::WHITENING};
     SX1278::Error err;
 
     SPIBus bus(SX1278_SPI);
