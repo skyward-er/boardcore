92cde41f3a9880e24fbc599e4f25fc03a0e3c51c
Radu Raul <raul.radu@skywarder.eu>
2023-02-26 00:55:17 +0100

    [Drivers] Fixed issues with copyrights and cppcheck compliance

--- src/tests/sensors/test-h3lis331dl.cpp
+++ src/tests/sensors/test-h3lis331dl.cpp
@@ -1,7 +1,30 @@
+/* Copyright (c) 2023 Skyward Experimental Rocketry
+ * Author: Radu Raul
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 #include <drivers/spi/SPIDriver.h>
 #include <drivers/timer/TimestampTimer.h>
 #include <sensors/H3LIS331DL/H3LIS331DL.h>
 #include <utils/Debug.h>
+
 #include "miosix.h"
 
 using namespace Boardcore;
@@ -31,8 +54,6 @@ int main()
     H3LIS331DL sensor(bus, cs, sensor.ODR_50, sensor.BDU_CONTINUOS_UPDATE,
                       sensor.FS_100);
 
-    H3LIS331DLData data;
-
     if (!sensor.init())
     {
         printf("Failed init!\n");
@@ -42,6 +63,8 @@ int main()
         }
         return -1;
     }
+
+    H3LIS331DLData data;
 
     // Print out the CSV header
     printf(H3LIS331DLData::header().c_str());
