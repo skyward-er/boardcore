be52bd809f53246d649984a0da03a7a71bf3fe72
Federico Lolli <federico.lolli@skywarder.eu>
2023-05-01 20:35:39 +0200

    [entrypoints] trying to log on SD card

--- src/entrypoints/sensortile-entry.cpp
+++ src/entrypoints/sensortile-entry.cpp
@@ -1,6 +1,16 @@
 #include <miosix.h>
+#include <utils/Debug.h>
+#include <diagnostic/PrintLogger.h>
+
+using namespace miosix;
+using namespace Boardcore;
 
 int main()
 {
-    printf("Hello world!");
+    PrintLogger logger = Logging::getLogger("main");
+
+    LOG_INFO(logger, "This is an info message");
+    LOG_ERR(logger, "This is an error message");
+
+    return 0;
 }
