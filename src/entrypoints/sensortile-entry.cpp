2bb65debfb2dcc074de8fc399462f1f9868205ed
Alberto Nidasio <alberto.nidasio@skywarder.eu>
2023-05-11 18:50:40 +0200

    TEST

--- src/entrypoints/sensortile-entry.cpp
+++ src/entrypoints/sensortile-entry.cpp
@@ -1,16 +1,41 @@
+#include <diagnostic/PrintLogger.h>
 #include <miosix.h>
 #include <utils/Debug.h>
-#include <diagnostic/PrintLogger.h>
+
+#include <fstream>
+#include <sstream>
 
 using namespace miosix;
 using namespace Boardcore;
 
 int main()
 {
-    PrintLogger logger = Logging::getLogger("main");
+    std::ofstream file("/sd/testfile.txt", std::ios::app);
 
-    LOG_INFO(logger, "This is an info message");
-    LOG_ERR(logger, "This is an error message");
+    if (file.is_open())
+    {
+        file << "Hi mom!" << std::endl;
+
+        file.close();
+
+        while (true)
+        {
+            ledOn();
+            Thread::sleep(500);
+            ledOff();
+            Thread::sleep(500);
+        }
+    }
+    else
+    {
+        while (true)
+        {
+            ledOn();
+            Thread::sleep(100);
+            ledOff();
+            Thread::sleep(100);
+        }
+    }
 
     return 0;
 }
