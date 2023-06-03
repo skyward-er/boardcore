438fb37f256cced0aa89607b3ea568d6aa141b2d
Davide Mor <davide.mor@skywarder.eu>
2023-06-03 19:38:16 +0200

    [ExternalInterrupts] Updated copyright

--- src/shared/drivers/interrupt/external_interrupts.cpp
+++ src/shared/drivers/interrupt/external_interrupts.cpp
@@ -1,5 +1,5 @@
 /* Copyright (c) 2019 Skyward Experimental Rocketry
- * Author: Alvise de'Faveri Tron
+ * Author: Alvise de'Faveri Tron, Luca Erbetta, Davide Mor
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
