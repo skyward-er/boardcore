a7fb7f6399c0f759b991d491ceab8f6ca758251c
Giulia <giulia.ghirardini@skywarder.eu>
2023-06-08 17:54:31 +0200

    [LPS22DF] Modified init, selfTest and setConfig functions

--- src/shared/sensors/LPS22DF/LPS22DF.cpp
+++ src/shared/sensors/LPS22DF/LPS22DF.cpp
@@ -23,43 +23,166 @@
 #include "LPS22DF.h"
 
 #include <drivers/timer/TimestampTimer.h>
+#include <miosix.h>
+#include <sensors/calibration/SensorDataExtra/SensorDataExtra.h>
+#include <utils/Debug.h>
+
+#include <iostream>
+
+using namespace Eigen;
 
 namespace Boardcore
 {
 
+static constexpr uint8_t WHO_AM_I_VALUE = 0xb4;
+static constexpr uint8_t ENABLE_ONESHOT = (0b01 << 0);
+
 LPS22DF::LPS22DF(SPIBusInterface& bus, miosix::GpioPin pin,
                  SPIBusConfig spiConfig, Config config)
-    : mSlave(bus, pin, spiConfig), mConfig(config), currDiv(0),
-      isInitialized(false)
+    : mSlave(bus, pin, spiConfig), mConfig(config), isInitialized(false)
 {
+}
+
+SPIBusConfig LPS22DF::getDefaultSPIConfig()
+{
+    SPIBusConfig spiConfig;
+    spiConfig.clockDivider = SPI::ClockDivider::DIV_256;
+    spiConfig.mode         = SPI::Mode::MODE_3;
+    spiConfig.bitOrder     = SPI::Order::LSB_FIRST;
+    return spiConfig;
 }
 
 bool LPS22DF::init()
 {
-    SPITransaction spi(mSlave);
+    if (isInitialized)
+    {
+        LOG_ERR(logger, "Attempted to initialized sensor twice but failed");
+        lastError = ALREADY_INIT;
+        return false;
+    }
 
-    // todo
+    {
+        SPITransaction spi(mSlave);
+        spi.writeRegister(IF_CTRL, if_ctrl::I2C_I3C_DIS);
+    }
+
+    // Checking the whoami value to assure communication
+    if (!selfTest())
+    {
+        LOG_ERR(logger, "Self-test failed");
+        lastError = SELF_TEST_FAIL;
+        return false;
+    }
+
+    // Setting the actual sensor configurations (Mode, ODR, AVG, FSR, DRDY)
+    if (!setConfig(mConfig))
+    {
+        LOG_ERR(logger, "Configuration not applied correctly");
+        lastError = SensorErrors::INIT_FAIL;
+        return false;
+    }
+
+    lastError     = SensorErrors::NO_ERRORS;
+    isInitialized = true;
+    return setConfig(mConfig);
+}
+
+bool LPS22DF::selfTest()
+{
+    if (!isInitialized)
+    {
+        LOG_ERR(logger, "Invoked selfTest() but sensor was uninitialized");
+        lastError = NOT_INIT;
+        return false;
+    }
+
+    // selfTest procedure does not exist for this sensor. WhoamiValue is checked
+    // to assure communication.
+    SPITransaction spi(mSlave);
+    uint8_t value = spi.readRegister(WHO_AM_I);
+
+    if (value != WHO_AM_I_VALUE)
+    {
+        LOG_ERR(logger,
+                "WHO_AM_I value differs from expectation: read 0x{:x} "
+                "but expected 0x{:x}",
+                value, WHO_AM_I_VALUE);
+        TRACE("\n%h", value);
+        lastError = INVALID_WHOAMI;
+        return false;
+    }
+
     return true;
 }
 
-bool LPS22DF::selfTest() { return true; }
-
-bool LPS22DF::applyConfig(Config config)
+bool LPS22DF::setConfig(const Config& config)
 {
-    // todo
+    SPITransaction spi(mSlave);
+
+    switch (config.mode)
+    {
+        case Mode::ONE_SHOT_MODE:
+            mConfig = {config.avg, Mode::ONE_SHOT_MODE, ODR::ONE_SHOT};
+            break;
+
+        case Mode::CONITNUOUS_MODE:
+            mConfig = config;
+            break;
+
+        default:
+            LOG_ERR(logger, "Mode not supported");
+            return false;
+    }
     return true;
+}
+
+float LPS22DF::convertPressure(uint8_t pressXL, uint8_t pressL, uint8_t pressH)
+{
+    uint32_t press_value =
+        ((int32_t)((pressH << 24) | (pressL << 16) | (pressXL << 8))) >> 8;
+    return ((float)press_value / pressureSensitivity);
+}
+
+float LPS22DF::convertTemperature(uint8_t tempL, uint8_t tempH)
+{
+    uint16_t temp_value = ((int16_t)((tempH << 8) | (tempL << 0)));
+    return ((float)temp_value / temperatureSensitivity);
 }
 
 LPS22DFData LPS22DF::sampleImpl()
 {
-    int16_t val;
+    if (!isInitialized)
+    {
+        LOG_ERR(logger, "Invoked sampleImpl() but sensor was uninitialized");
+        lastError = NOT_INIT;
+        return lastSample;
+    }
+
+    uint8_t status = 0;
+    uint8_t val[5] = {0};
+    SPITransaction spi(mSlave);
     LPS22DFData data;
 
-    data.pressureTimestamp = TimestampTimer::getTimestamp();
+    lastError              = NO_ERRORS;
+    data.pressureTimestamp = data.temperatureTimestamp =
+        TimestampTimer::getTimestamp();
 
-    SPITransaction spi(mSlave);
+    if (mConfig.odr == ODR::ONE_SHOT)
+    {
+        // // TODO
+        // if (!(spi.readRegister(CTRL_REG1) &&
+        //       spi.writeRegister(CTRL_REG2, ENABLE_ONESHOT)))
+        // {
+        //     lastError = BUS_FAULT;
+        //     return lastSample;
+        // }
 
-    // todo
+        data.pressure    = convertPressure(val[0], val[1], val[2]);
+        data.temperature = convertTemperature(val[3], val[4]);
+
+        return data;
+    }
+
     return data;
 }
 
