dd1ec29876a1260f4d036d5e76f81a3c575b11e8
EmilioCorigliano <emilio.corigliano@mail.polimi.it>
2023-06-16 07:56:42 +0200

    [LPS28DFW] Given default configuration and added documentation

--- src/shared/sensors/LPS28DFW/LPS28DFW.h
+++ src/shared/sensors/LPS28DFW/LPS28DFW.h
@@ -30,15 +30,28 @@
 namespace Boardcore
 {
 
+/**
+ * @brief Driver for LPS28DFW STMicroelectronics digital pressure sensor working
+ * in I2C.
+ */
 class LPS28DFW : public Sensor<LPS28DFWData>
 {
 public:
+    /**
+     * @brief Enumeration for the full scale range to set on the sensor.
+     * Available are 1260 hPa or 4060 hPa.
+     */
     enum FullScaleRange : uint8_t
     {
         FS_1260,
         FS_4060
     };
 
+    /**
+     * @brief Enumeration for the output data rate to set on the sensor.
+     * Available are One shot (only one sample calculated when signal sent), 1
+     * Hz to 200 Hz.
+     */
     enum ODR : uint8_t
     {
         ONE_SHOT = (0b0000 << 3),
@@ -52,6 +65,11 @@ public:
         ODR_200  = (0b1000 << 3)
     };
 
+    /**
+     * @brief Enumeration for the oversampling to set on the sensor. The value
+     * read from the sensor will actually be the average of multiple samples.
+     * Available are from 4 to 512 averaged samples.
+     */
     enum AVG : uint8_t
     {
         AVG_4   = 0b000,
@@ -63,6 +81,13 @@ public:
         AVG_512 = 0b111
     };
 
+    /**
+     * @brief Working mode of the sensor. Available ones are one shot mode
+     * (samples acquired by the sensors only upon an explicit request),
+     * continuous mode (samples calculated continuously) or fifo mode (the
+     * sensor keeps more samples and a read let you read them more than once in
+     * a single read burst).
+     */
     enum Mode
     {
         ONE_SHOT_MODE,    // BYPASS, ODR = ONE_SHOT
@@ -72,26 +97,54 @@ public:
 
     typedef struct
     {
-        FullScaleRange fsr;
-        AVG avg;
-        Mode mode;
-        ODR odr;
-        bool drdy;        // Enable Interrupt for Data Ready
-        size_t fifoSize;  // If fifo, length of fifo
+        FullScaleRange fsr;  ///< Full scale range
+        AVG avg;             ///< Average avg samples
+        Mode mode;           ///< Mode of operation
+        ODR odr;             ///< Output data rate
+        bool drdy;           ///< Enable Interrupt for Data Ready
+        size_t fifoSize;     ///< If fifo, length of fifo
     } SensorConfig;
 
-    LPS28DFW(I2C& i2c, bool sa0, SensorConfig sensorConfig);
+    /**
+     * @brief Constructor that stores the initial settings (without applying
+     * them to the sensor).
+     * @param i2c I2C Peripheral that will be used to communicate with the
+     * sensor.
+     * @param sa0 Last bit of the slave address; tells if the SA0 pin on the
+     * sensor is connected to GND or VDD (3.3V, not 5V!).
+     * @param sensorConfig Configuration of the sensor with ODR, AVG, FSR,
+     * interrupt enabled and fifo size.
+     */
+    LPS28DFW(I2C& i2c, bool sa0,
+             SensorConfig sensorConfig = {FullScaleRange::FS_1260, AVG::AVG_32,
+                                          Mode::CONTINUOUS_MODE, ODR::ODR_100,
+                                          false, 0});
 
+    /**
+     * @brief Initializes the sensor with the current settings.
+     */
     bool init() override;
 
+    /**
+     * @brief Sets and saves the configurations passed on the parameters.
+     */
     bool setConfig(const SensorConfig& config);
 
     bool selfTest() override;
 
+    /**
+     * @brief Sets and saves the oversampling on the sensor.
+     */
     bool setAverage(AVG avg);
 
+    /**
+     * @brief Sets and saves the output data rate.
+     */
     bool setOutputDataRate(ODR odr);
 
+    /**
+     * @brief Sets and saves the full scale range.
+     */
     bool setFullScaleRange(FullScaleRange fs);
 
 private:
@@ -100,9 +153,9 @@ private:
     I2C& i2c;
     I2CDriver::I2CSlaveConfig i2cConfig;
     SensorConfig sensorConfig;
-    bool isInitialized = false;
-    uint16_t pressureSensitivity;
-    uint16_t temperatureSensitivity = 100;  // [LSB/°C]
+    bool isInitialized              = false;
+    uint16_t pressureSensitivity    = 4096;  // [hPa]
+    uint16_t temperatureSensitivity = 100;   // [LSB/°C]
     PrintLogger logger              = Logging::getLogger("lps28dfw");
 };
 
