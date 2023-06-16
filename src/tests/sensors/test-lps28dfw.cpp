aa52d67c193253267ca4747b47eecce8d63ea2cf
EmilioCorigliano <emilio.corigliano@mail.polimi.it>
2023-06-16 07:56:42 +0200

    [LPS28DFW] Removed FIFO option, improved setting of config, simplified conversion of pressure and improved test

--- src/tests/sensors/test-lps28dfw.cpp
+++ src/tests/sensors/test-lps28dfw.cpp
@@ -40,13 +40,8 @@ typedef Gpio<GPIOB_BASE, 8> i1scl;
 typedef Gpio<GPIOB_BASE, 9> i1sda;
 
 I2C i2c(I2C1, i1scl::getPin(), i1sda::getPin());
-LPS28DFW::SensorConfig lps28dfwConfig{LPS28DFW::FullScaleRange::FS_1260,
-                                      LPS28DFW::AVG::AVG_4,
-                                      LPS28DFW::Mode::CONTINUOUS_MODE,
-                                      LPS28DFW::ODR::ODR_10,
-                                      true,
-                                      0};
-LPS28DFW lps28dfw(i2c, false, lps28dfwConfig);
+LPS28DFW lps28dfw(i2c, false);
+uint8_t nSamples = 10;
 /**
  * ONE_SHOT | AVG4   : 161.6 us/samp (W1+R1+W2+W1+R5)   @1Hz: 1.7  uA [FROM DS]
  * ONE_SHOT | AVG512 : 162 us/samp (W1+R1+W2+W1+R5)     @1Hz: 32.2 uA [FROM DS]
@@ -54,12 +49,99 @@ LPS28DFW lps28dfw(i2c, false, lps28dfwConfig);
  * ODR1     | AVG512 : 127.8 us/samp (W1+R1+W1+R5)      @1Hz: 32.8 uA [FROM DS]
  */
 miosix::Thread *waiting = 0;
+bool sampleAvailable    = false;
 void __attribute__((used)) EXTI5_IRQHandlerImpl()
 {
     if (waiting)
     {
-        waiting->wakeup();
+        sampleAvailable = true;
+        waiting->IRQwakeup();
     }
+}
+
+void sampleOneShotMode(LPS28DFW &lps28dfw)
+{
+    printf("Start One-Shot\n");
+    LPS28DFW::SensorConfig lps28dfwConfig{
+        LPS28DFW::FullScaleRange::FS_1260, LPS28DFW::AVG::AVG_64,
+        LPS28DFW::Mode::ONE_SHOT_MODE, LPS28DFW::ODR::ONE_SHOT, false};
+    lps28dfw.setConfig(lps28dfwConfig);
+
+    for (uint8_t i = 0; i < nSamples; i++)
+    {
+        lps28dfw.sample();
+
+        if (lps28dfw.getLastError() == SensorErrors::NO_ERRORS)
+        {
+            lps28dfw.getLastSample().print(std::cout);
+        }
+        else
+        {
+            printf("Error: %d\n", lps28dfw.getLastError());
+        }
+        Thread::sleep(100);
+    }
+
+    printf("End One-Shot\n\n");
+}
+
+void sampleContinuousMode(LPS28DFW &lps28dfw)
+{
+    printf("Start Continuous\n");
+    LPS28DFW::SensorConfig lps28dfwConfig{
+        LPS28DFW::FullScaleRange::FS_1260, LPS28DFW::AVG::AVG_64,
+        LPS28DFW::Mode::CONTINUOUS_MODE, LPS28DFW::ODR::ODR_10, false};
+    lps28dfw.setConfig(lps28dfwConfig);
+
+    for (uint8_t i = 0; i < nSamples; i++)
+    {
+        lps28dfw.sample();
+
+        if (lps28dfw.getLastError() == SensorErrors::NO_ERRORS)
+        {
+            lps28dfw.getLastSample().print(std::cout);
+        }
+        else
+        {
+            printf("Error: %d\n", lps28dfw.getLastError());
+        }
+
+        Thread::sleep(100);
+    }
+
+    printf("End Continuous\n\n");
+}
+
+void sampleInterruptMode(LPS28DFW &lps28dfw)
+{
+    printf("Start Interrupt\n");
+    LPS28DFW::SensorConfig lps28dfwConfig{
+        LPS28DFW::FullScaleRange::FS_1260, LPS28DFW::AVG::AVG_64,
+        LPS28DFW::Mode::CONTINUOUS_MODE, LPS28DFW::ODR::ODR_10, true};
+    lps28dfw.setConfig(lps28dfwConfig);
+
+    for (uint8_t i = 0; i < nSamples; i++)
+    {
+
+        while (!sampleAvailable)
+        {
+            waiting->wait();
+        }
+
+        sampleAvailable = false;
+        lps28dfw.sample();
+
+        if (lps28dfw.getLastError() == SensorErrors::NO_ERRORS)
+        {
+            lps28dfw.getLastSample().print(std::cout);
+        }
+        else
+        {
+            printf("Error: %d\n", lps28dfw.getLastError());
+        }
+    }
+
+    printf("End Interrupt\n\n");
 }
 
 int main()
@@ -75,17 +157,10 @@ int main()
 
     for (;;)
     {
-        lps28dfw.sample();
-
-        if (lps28dfw.getLastError() == SensorErrors::NO_ERRORS)
-        {
-            lps28dfw.getLastSample().print(std::cout);
-        }
-        else
-        {
-            printf("Error: %d\n", lps28dfw.getLastError());
-        }
-        waiting->wait();
+        printf("\nNew set of samples\n");
+        sampleOneShotMode(lps28dfw);
+        sampleContinuousMode(lps28dfw);
+        sampleInterruptMode(lps28dfw);
     }
 
     return 0;
