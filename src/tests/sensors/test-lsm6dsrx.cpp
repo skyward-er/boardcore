a5ffd1aafffbc0a3c6f17aba5619e13ec534744f
Fabrizio Monti <fabrizio.monti@skywarder.eu>
2023-06-16 08:01:39 +0200

    [LSM6DSRX] Updated code for testing.

--- src/tests/sensors/test-lsm6dsrx.cpp
+++ src/tests/sensors/test-lsm6dsrx.cpp
@@ -112,69 +112,14 @@ int main()
         }
     }
 
-    while (true)
-    {
-        // LSM6DSRXData data = sens.getSensorData();
-        // std::cout << data.header() << "\n";
-        // data.print(std::cout);
-        // std::cout << "\n\n";
+    // test data
 
-        // wait for fifo full interrupt
-        int dataReady = int2Pin.value();
-        while (dataReady != 1)
-        {
-            Thread::sleep(20);
-            dataReady = int2Pin.value();
-        }
-
-        sens->sample();
-
-        const std::array<LSM6DSRXData, LSM6DSRXDefs::FIFO_SIZE>& buf =
-            sens->getLastFifo();
-        // for (unsigned int i = 0; i < buf.size(); ++i)
-        // {
-        //     std::cout << "sample " << i << "\n";
-        //     std::cout << buf[i].header() << "\n";
-        //     buf[i].print(std::cout);
-        //     std::cout << "\n\n";
-        // }
-        // TRACE("main) lastFifoLevel: %u\n\n", sens.getLastFifoSize());
-
-        std::cout << buf[sens->getLastFifoSize() - 1].header() << "\n";
-        buf[sens->getLastFifoSize() - 1].print(std::cout);
-        std::cout << "\n\n";
-
-        // std::cout << "Press a key to continue...\n\n";
-        // char ch = 0;
-        // std::cin >> ch;
-
-        // TRACE(
-        //     "Interrupt ricevuto\n"
-        //     "dati non letti pre-lettura: %u\n",
-        //     sens->unreadDataInFifo());
-        // sens->sampleImpl();
-        // TRACE("dati non letti post-lettura: %u\n\n",
-        // sens->unreadDataInFifo());
-
-        // Thread::sleep(5000);
-    }
-
-    // std::vector<uint64_t> vetTimestamps;
-    // vetTimestamps.reserve(LSM6DSRXDefs::FIFO_SIZE);
     // while (true)
     // {
-    //     // skip some samples
-    //     for (int i = 0; i < 5; ++i)
-    //     {
-    //         // wait for fifo full interrupt
-    //         int dataReady = int2Pin.value();
-    //         while (dataReady != 1)
-    //         {
-    //             Thread::sleep(20);
-    //             dataReady = int2Pin.value();
-    //         }
-    //         sens->sampleImpl();
-    //     }
+    //     // LSM6DSRXData data = sens.getSensorData();
+    //     // std::cout << data.header() << "\n";
+    //     // data.print(std::cout);
+    //     // std::cout << "\n\n";
 
     //     // wait for fifo full interrupt
     //     int dataReady = int2Pin.value();
@@ -183,209 +128,110 @@ int main()
     //         Thread::sleep(20);
     //         dataReady = int2Pin.value();
     //     }
-    //     sens->sampleImpl();
 
-    //     // data extraction
+    //     sens->sample();
+
     //     const std::array<LSM6DSRXData, LSM6DSRXDefs::FIFO_SIZE>& buf =
     //         sens->getLastFifo();
-    //     vetTimestamps.clear();
-    //     for (int i = 0; i < sens->getLastFifoSize(); ++i)
-    //     {
-    //         vetTimestamps.push_back(buf[i].accelerationTimestamp);
-    //     }
-    //     std::sort(vetTimestamps.begin(), vetTimestamps.end());
+    //     // for (unsigned int i = 0; i < buf.size(); ++i)
+    //     // {
+    //     //     std::cout << "sample " << i << "\n";
+    //     //     std::cout << buf[i].header() << "\n";
+    //     //     buf[i].print(std::cout);
+    //     //     std::cout << "\n\n";
+    //     // }
+    //     // TRACE("main) lastFifoLevel: %u\n\n", sens.getLastFifoSize());
 
-    //     // test max difference between timestamps
-    //     uint64_t diffMax = 0, diff = 0;
-    //     for (unsigned int i = 1; i < vetTimestamps.size(); ++i)
-    //     {
-    //         if (vetTimestamps[i] == 0)
-    //         {
-    //             TRACE("Timestamp is 0\n");
-    //         }
-    //         if (vetTimestamps[i - 1] > 0)
-    //         {
-    //             diff = vetTimestamps[i] - vetTimestamps[i - 1];
-    //             if (diff > diffMax)
-    //             {
-    //                 diffMax = diff;
-    //             }
-    //         }
-    //     }
+    //     std::cout << buf[sens->getLastFifoSize() - 1].header() << "\n";
+    //     buf[sens->getLastFifoSize() - 1].print(std::cout);
+    //     std::cout << "\n\n";
 
-    //     // print data
-    //     std::cout << "diffMax: " << diffMax << "\n";
-    //     std::cout << "vetTimestamps:\n";
-    //     for (unsigned int i = 0; i < vetTimestamps.size(); ++i)
-    //     {
-    //         std::cout << vetTimestamps[i] << ", ";
-    //     }
-    //     std::cout << "\n\n\n";
+    //     // std::cout << "Press a key to continue...\n\n";
+    //     // char ch = 0;
+    //     // std::cin >> ch;
+
+    //     // TRACE(
+    //     //     "Interrupt ricevuto\n"
+    //     //     "dati non letti pre-lettura: %u\n",
+    //     //     sens->unreadDataInFifo());
+    //     // sens->sampleImpl();
+    //     // TRACE("dati non letti post-lettura: %u\n\n",
+    //     // sens->unreadDataInFifo());
+
+    //     // Thread::sleep(5000);
     // }
+
+    // test timestamps
+
+    std::vector<uint64_t> vetTimestamps;
+    vetTimestamps.reserve(LSM6DSRXDefs::FIFO_SIZE);
+    while (true)
+    {
+        // skip some samples
+        for (int i = 0; i < 5; ++i)
+        {
+            // wait for fifo full interrupt
+            int dataReady = int2Pin.value();
+            while (dataReady != 1)
+            {
+                Thread::sleep(20);
+                dataReady = int2Pin.value();
+            }
+            sens->sampleImpl();
+        }
+
+        // wait for fifo full interrupt
+        int dataReady = int2Pin.value();
+        while (dataReady != 1)
+        {
+            Thread::sleep(20);
+            dataReady = int2Pin.value();
+        }
+        sens->sampleImpl();
+
+        // data extraction
+        const std::array<LSM6DSRXData, LSM6DSRXDefs::FIFO_SIZE>& buf =
+            sens->getLastFifo();
+        vetTimestamps.clear();
+        for (int i = 0; i < sens->getLastFifoSize(); ++i)
+        {
+            vetTimestamps.push_back(buf[i].accelerationTimestamp);
+
+            if (buf[i].accelerationTimestamp == 0)
+            {
+                TRACE("Acc timestamp is 0\n");
+            }
+            if (buf[i].angularSpeedTimestamp == 0)
+            {
+                TRACE("Gyr timestamp is 0\n");
+            }
+        }
+        std::sort(vetTimestamps.begin(), vetTimestamps.end());
+
+        // test max difference between timestamps
+        uint64_t diffMax = 0, diff = 0;
+        for (unsigned int i = 1; i < vetTimestamps.size(); ++i)
+        {
+            if (vetTimestamps[i - 1] > 0)
+            {
+                diff = vetTimestamps[i] - vetTimestamps[i - 1];
+                if (diff > diffMax)
+                {
+                    diffMax = diff;
+                }
+            }
+        }
+
+        // print data
+        std::cout << "diffMax: " << diffMax << "\n";
+        // std::cout << "vetTimestamps:\n";
+        // for (unsigned int i = 0; i < vetTimestamps.size(); ++i)
+        // {
+        //     std::cout << vetTimestamps[i] << ", ";
+        // }
+        std::cout << "\n\n\n";
+    }
 
     delete sens;
     return 0;
 }
-
-// #include <ctime>
-// uint64_t differenza(const uint64_t val1, const uint64_t val2)
-// {
-//     if(val1 > val2)
-//     {
-//         return val1 - val2;
-//     }
-
-//     return val2 - val1;
-// }
-// int main()
-// {
-//     // timestamp test
-//     srand(time(NULL));
-
-//     SPIBus bus(SPI3);
-
-//     GpioPin csPin(GPIOE_BASE, 3);  // PE3 CS
-//     csPin.mode(Mode::OUTPUT);
-
-//     GpioPin clockPin(GPIOB_BASE, 3);  // PB3 CK (SCL)
-//     clockPin.mode(Mode::ALTERNATE);
-//     clockPin.alternateFunction(6);
-//     GpioPin misoPin(GPIOB_BASE, 4);  // PB4 MISO (SDO)
-//     misoPin.alternateFunction(6);
-//     misoPin.mode(Mode::ALTERNATE);
-//     GpioPin mosiPin(GPIOB_BASE, 5);  // PB5 MOSI (SDA)
-//     mosiPin.alternateFunction(6);
-//     mosiPin.mode(Mode::ALTERNATE);
-
-//     GpioPin int1Pin(GPIOC_BASE, 15);  // PC15 interrupt pin 1
-//     int1Pin.mode(Mode::INPUT);
-//     GpioPin int2Pin(GPIOC_BASE, 13);  // PC13 interrupt pin 2
-//     int2Pin.mode(Mode::INPUT);
-
-//     SPIBusConfig busConfiguration;  // Bus configuration for the sensor
-//     busConfiguration.clockDivider = SPI::ClockDivider::DIV_256;
-//     busConfiguration.mode =
-//         SPI::Mode::MODE_0;  // Set clock polarity to 0 and phase to 1
-
-//     LSM6DSRXConfig sensConfig;
-//     sensConfig.bdu = LSM6DSRXConfig::BDU::CONTINUOUS_UPDATE;
-
-//     // acc
-//     sensConfig.fsAcc     = LSM6DSRXConfig::ACC_FULLSCALE::G2;
-//     sensConfig.odrAcc    = LSM6DSRXConfig::ACC_ODR::HZ_1_6;
-//     sensConfig.opModeAcc = LSM6DSRXConfig::OPERATING_MODE::NORMAL;
-
-//     // gyr
-//     sensConfig.fsGyr     = LSM6DSRXConfig::GYR_FULLSCALE::DPS_125;
-//     sensConfig.odrGyr    = LSM6DSRXConfig::GYR_ODR::HZ_12_5;
-//     sensConfig.opModeGyr = LSM6DSRXConfig::OPERATING_MODE::NORMAL;
-
-//     // fifo
-//     sensConfig.fifoMode = LSM6DSRXConfig::FIFO_MODE::BYPASS;
-//     sensConfig.fifoTimestampDecimation =
-//         LSM6DSRXConfig::FIFO_TIMESTAMP_DECIMATION::DISABLED;
-//     sensConfig.fifoTemperatureBdr =
-//         LSM6DSRXConfig::FIFO_TEMPERATURE_BDR::DISABLED;
-
-//     // interrupt
-//     sensConfig.int1InterruptSelection = LSM6DSRXConfig::INTERRUPT::NOTHING;
-//     sensConfig.int2InterruptSelection =
-//         LSM6DSRXConfig::INTERRUPT::NOTHING;
-//     sensConfig.fifoWatermark = 0;
-
-//     LSM6DSRX sens(bus, csPin, busConfiguration, sensConfig);
-
-//     if (sens.init() == false)
-//     {
-//         while (true)
-//         {
-//             TRACE("Error, sensor not initialized\n\n");
-//             Thread::sleep(2000);
-//         }
-//     }
-
-//     TRACE("Inizio test\n\n");
-
-//     const int NUM_TEST = 10;
-//     int i = 0;
-
-//     uint64_t legacy0 = 0, legacy1 = 0;// misurazioni a t0 e t1
-//     uint64_t legacy1calcolato = 0;// valore legacy a t1 calcolato partendo
-//     dai dati del sensore uint32_t sens0 = 0, sens1 = 0;// misurazioni a t0 e
-//     t1 uint64_t deltaSens = 0, deltaLegacy = 0;// variazione tra la misura a
-//     t0 e quella a t1 uint64_t diff = 0;// differenza tra legacy1 e
-//     legacy1calcolato
-
-//     legacy0 = TimestampTimer::getTimestamp();
-//     sens0 = sens.getSensorTimestamp();
-
-//     const float sensResolution = sens.getSensorTimestampResolution() *
-//     1000;// la funzione restituisce in ms
-//     // TRACE("Resolution: %f\n", sensResolution);
-//     // for(i = 0; i < NUM_TEST; ++i)
-//     // {
-//     //     Thread::sleep((rand() % 2000) + 1000);
-
-//     //     legacy1 = TimestampTimer::getTimestamp();
-//     //     sens1 = sens.getSensorTimestamp();
-
-//     //     deltaSens = sens1 - sens0;
-//     //     deltaLegacy = deltaSens * sensResolution;
-
-//     //     legacy1calcolato = legacy0 + deltaLegacy;
-
-//     //     diff = differenza(legacy1, legacy1calcolato);
-
-//     //     TRACE("Campione %d:\n"
-//     //             "legacy1:\t\t%llu\n"
-//     //             "legacy1calcolato:\t%llu\n"
-//     //             "differenza:\t\t%llu\n\n", i, legacy1, legacy1calcolato,
-//     //             diff);
-
-//     //     // prepara per i cicli successivi
-//     //     legacy0 = legacy1;
-//     //     sens0 = sens1;
-//     // }
-
-//     // new test
-//     for(i = 0; i < NUM_TEST; ++i)
-//     {
-//         legacy0 = TimestampTimer::getTimestamp();
-//         sens0 = sens.getSensorTimestamp();
-
-//         // for(int k = 0; k < 3; ++k)
-//         // {
-//         //     Thread::sleep((rand() % 1000) + 1000);
-//         //     sens1 = sens.getSensorTimestamp();
-//         // }
-//         Thread::sleep(1000);
-
-//         legacy1 = TimestampTimer::getTimestamp();
-//         sens1 = sens.getSensorTimestamp();
-
-//         // deltaSens = sens1 - sens0;
-//         if(sens1 > sens0)
-//         {
-//             deltaSens = sens1 - sens0;
-//         }
-//         else
-//         {
-//             deltaSens = static_cast<uint64_t>(-1) - sens0 + sens1 + 1;
-//         }
-
-//         deltaLegacy = deltaSens * sensResolution;
-
-//         legacy1calcolato = legacy0 + deltaLegacy;
-
-//         diff = differenza(legacy1calcolato, legacy1);
-//         TRACE("Test %d\nDifferenza: %llu\n\n", i, diff);
-
-//     }
-
-//     while(true)
-//     {
-//         Thread::sleep(5000);
-//     }
-
-//     return 0;
-// }
