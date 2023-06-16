3a85e3d6ae46570abe69b9fd918a0b294fa92ec4
Fabrizio Monti <fabrizio.monti@skywarder.eu>
2023-06-16 08:01:39 +0200

    [LSM6DSRX] Added conversion between sensor timestamps and TimestampTimer, and commented configuration values for temperature batching in fifo (so that temperature can't be sampled in fifo).

--- src/shared/sensors/LSM6DSRX/LSM6DSRXDefs.h
+++ src/shared/sensors/LSM6DSRX/LSM6DSRXDefs.h
@@ -31,7 +31,13 @@ namespace LSM6DSRXDefs
 {
 
 /// @brief Fifo max size expressed as number of samples.
-const int FIFO_SIZE = 50;
+const uint16_t FIFO_SIZE = 50;
+
+/// @brief How many times the fifo is sampled before updating timestamps
+/// (m_timestamp0, m_sensorTimestamp0)
+const uint8_t TIMESTAMP_UPDATE_VALUE =
+    5;  // after n reading from the fifo a utility function is called to update
+        // timestamps
 
 /// @brief Sensor who_am_i register value.
 const uint8_t WHO_AM_I_VALUE = 0x6B;
