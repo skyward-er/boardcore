a082c953d31ce1563a7ad6ba44d3b37927d68ac3
Fabrizio Monti <fabrizio.monti@skywarder.eu>
2023-06-16 08:01:39 +0200

    [LSM6DSRX] Perform a simple reading from the accelerometer.

--- src/shared/sensors/LSM6DSRX/LSM6DSRX.h
+++ src/shared/sensors/LSM6DSRX/LSM6DSRX.h
@@ -48,6 +48,28 @@ public:
      */
     bool init();
 
+    /**
+     * @brief Simple function to read acc data from z axis
+     */
+    float getZAxisAccData()
+    {
+        int8_t low = 0, high = 0;
+        int16_t sample = 0;
+
+        SPITransaction transaction{spiSlave};
+
+
+        high = transaction.readRegister(REG_OUTZ_H_A);
+        low = transaction.readRegister(REG_OUTZ_L_A);
+
+        sample = combineHighLowBits(low, high);
+
+        float ret = static_cast<float>(sample) * 0.061;//fs fixed at 2g for now
+        return ret;
+    }
+
+
+
 private:
 
     bool isInit = false;
@@ -60,7 +82,27 @@ private:
      */
     enum Registers
     {
-        WHO_AM_I = 0x0F,
+        REG_WHO_AM_I = 0x0F,        // who_am_i register
+
+        REG_CTRL1_XL = 0x10,        // accelerometer control register
+        REG_CTRL3_C = 0x12,         // set bdu
+        REG_CTRL6_C = 0x15,         // enable/disable high performance mode for the accelerometer
+
+        REG_FIFO_CTRL4 = 0x0A,      // fifo control register 4 (fifo mode)
+
+        REG_FIFO_DATA_OUT_Z_L = 0x7D,
+        REG_FIFO_DATA_OUT_Z_H = 0x7E,
+
+        REG_OUTZ_L_A = 0x2C,
+        REG_OUTZ_H_A = 0x2D,
+    };
+
+    /**
+     * @brief Output data rate definitions for the accelerometer.
+     */
+    enum class ACC_ODR : uint8_t
+    {
+        HZ_104 = 4,
     };
 
     /**
@@ -69,6 +111,13 @@ private:
      * @return Returns false if not valid.
      */
     bool checkWhoAmI();
+
+    /**
+     * @brief Utility for combining two 8 bits numbers in one 16 bits number.
+     * @param low Low bits of the 16 bit number.
+     * @param high High bits of the 16 bit number.
+     */
+    int16_t combineHighLowBits(uint8_t low, uint8_t high);
 
 };
 
