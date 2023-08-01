236b78434194bd22fdaec919a457847fbba790d5
Federico Mandelli <federico.mandelli@skywarder.eu>
2023-08-01 10:01:33 +0000

    [PressureSensor] Added analog pressure sensors HSCMRNN015PAAA5 and SSCMRNN030PAAA5

---
+++ src/shared/sensors/analog/pressure/honeywell/HSCMRNN015PA.h
@@ -0,0 +1,44 @@
+/* Copyright (c) 2023 Skyward Experimental Rocketry
+ * Author: Federico Mandelli
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#pragma once
+
+#include "HSCMRNN015PAData.h"
+#include "HoneywellPressureSensor.h"
+
+namespace Boardcore
+{
+
+/**
+ * @brief Absolute pressure sensor with a 0-103kPa range (0-15psi)
+ */
+class HSCMRNN015PA final : public HoneywellPressureSensor<HSCMRNN015PAData>
+{
+public:
+    HSCMRNN015PA(std::function<ADCData()> getSensorVoltage,
+                 const float supplyVoltage = 5.0)
+        : HoneywellPressureSensor(getSensorVoltage, supplyVoltage, 103421.3594)
+    {
+    }
+};
+
+}  // namespace Boardcore
