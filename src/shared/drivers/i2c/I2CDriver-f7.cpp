f3e63a7bfba36a3af26432b4232af8dc4535a467
Matteo Pignataro <matteo.pignataro@skywarder.eu>
2023-08-10 11:23:29 +0200

    [I2C] Added all the correct boards for F7

--- src/shared/drivers/i2c/I2CDriver-f7.cpp
+++ src/shared/drivers/i2c/I2CDriver-f7.cpp
@@ -79,6 +79,8 @@ I2CTimings calculateTimings(uint32_t f, uint32_t fi2c)
 #elif defined(_BOARD_STM32F767ZI_COMPUTE_UNIT) ||         \
     defined(_BOARD_STM32F767ZI_GEMINI_GS) ||              \
     defined(_BOARD_STM32F767ZI_SKYWARD_DEATH_STACK_V4) || \
+    defined(_BOARD_STM32F767ZI_GEMINI_MOTOR) ||           \
+    defined(_BOARD_STM32F767ZI_AUTOMATED_ANTENNAS) ||     \
     defined(_BOARD_STM32F767ZI_NUCLEO)
     const uint16_t correction = 7;
 #else
