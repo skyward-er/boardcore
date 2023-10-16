fc725dfe6c4b09d66ac18031ccbffa7a66cf9906
Federico Lolli <federico.lolli@skywarder.eu>
2023-10-16 17:46:57 +0200

    [LowPass] added first draft of LowPass filter

---
+++ src/shared/algorithms/Filters/LowPass.cpp
@@ -0,0 +1,40 @@
+/* Copyright (c) 2023 Skyward Experimental Rocketry
+ * Author: Federico Lolli
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include "LowPass.h"
+
+namespace Boardcore
+{
+
+// TODO: WARNING! initialized at 0
+LowPass::LowPass(float gain, float cutoff_freq, float lambda)
+    : gain(gain), cutoff_freq(cutoff_freq), lambda(lambda), output(0)
+{
+}
+
+float LowPass::filter(float input)
+{
+    output = lambda * output + (gain / cutoff_freq) * (1 - lambda) * input;
+    return output;
+}
+
+}  // namespace Boardcore
