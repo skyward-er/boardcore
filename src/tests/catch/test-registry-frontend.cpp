c75102f8852a7691b378322d6489879254aa93a4
Nicolò Caruso <nicaschool13@gmail.com>
2024-02-10 12:07:30 +0100

    [Registry-Frontend] Serialization and deserialization, save/load removed structs
    Serialization and deserialization: Now the registry frontend have functions to serialize the configuration to a vector of uint8_t for the backend to save. Also save method that will save the configuration to the backend. Added load method that de-serializes the saved vector (for now the same since no get from the backend), performs checks and loads into the map the configuration.
    Removed structs: The specific structs are removed since they will be used by the OBSW and not in the Boardcore sw.
    Tests: Added arm/disarm tests, added a serialization/deserialization save/load test.
    Reserve: Added reserves to the dynamic data structures to limit the allocations.
    Minor fixes: coordinates was erroneously in uint32 and not float.

--- src/tests/catch/test-registry-frontend.cpp
+++ src/tests/catch/test-registry-frontend.cpp
@@ -30,47 +30,128 @@
 
 using namespace Boardcore;
 
-static constexpr uint8_t testValueUint8   = 20;
-static constexpr uint32_t testValueUint32 = 30;
-static constexpr float testValueFloat     = 1.45;
+/*! Magic numbers for testing the set/get for each data type */
+static constexpr uint8_t testValueUint8            = 20;
+static constexpr uint32_t testValueUint32          = 30;
+static constexpr float testValueFloat              = 1.45;
+static constexpr float testValueLatitude           = 45.50109;
+static constexpr float testValueLongitude          = 9.15633;
+static constexpr uint32_t coordinateId             = 8;
+static constexpr uint32_t deploymentAltitudeId     = 50;
+static constexpr uint32_t targetCoordinatesId      = 1;
+static constexpr uint32_t ventingValveAtomicTiming = 49;
+static constexpr uint32_t algorithmId              = 128;
 
-TEST_CASE("RegistryFrontend test - Set and get configuration entries")
+TEST_CASE(
+    "RegistryFrontend test - Set and get type Unsafe configuration entries")
 {
     RegistryFrontend registry;
     float floatValue;
     uint32_t uint32Value;
     uint8_t uint8Value;
-    Coordinates coordinatesValue;
+    Coordinates coordinatesValue(testValueLatitude, testValueLongitude);
     /*! Check that the registry is first empty */
     REQUIRE(registry.isConfigurationEmpty() == true);
     /*! Checks that there are effectively non-initialized entry configurations
      */
     REQUIRE(registry.getConfigurationUnsafe(
-                static_cast<uint32_t>(ConfigurationEnum::DEPLOYMENT_ALTITUDE),
-                floatValue) == false);
+                static_cast<uint32_t>(deploymentAltitudeId), floatValue) ==
+            false);
     REQUIRE(registry.getConfigurationUnsafe(
-                static_cast<uint32_t>(ConfigurationEnum::TARGET_COORDINATES),
-                coordinatesValue) == false);
+                static_cast<uint32_t>(targetCoordinatesId), coordinatesValue) ==
+            false);
     REQUIRE(registry.getConfigurationUnsafe(
-                static_cast<uint32_t>(
-                    ConfigurationEnum::VENTING_VALVE_ATOMIC_TIMING),
-                uint32Value) == false);
-    REQUIRE(registry.getConfigurationUnsafe(
-                static_cast<uint32_t>(ConfigurationEnum::ALGORITHM),
-                uint8Value) == false);
+                static_cast<uint32_t>(ventingValveAtomicTiming), uint32Value) ==
+            false);
+    REQUIRE(registry.getConfigurationUnsafe(static_cast<uint32_t>(algorithmId),
+                                            uint8Value) == false);
     /*! Check set configuration results in right get */
-    REQUIRE(registry.setConfigurationUnsafe(
-                static_cast<uint32_t>(ConfigurationEnum::ALGORITHM),
-                testValueUint8) == true);
+    REQUIRE(registry.setConfigurationUnsafe(static_cast<uint32_t>(algorithmId),
+                                            testValueUint8) == true);
     uint8Value = 0;
-    REQUIRE(registry.getConfigurationUnsafe(ConfigurationEnum::ALGORITHM,
-                                            uint8Value) == true);
+    REQUIRE(registry.getConfigurationUnsafe(algorithmId, uint8Value) == true);
     REQUIRE(uint8Value == testValueUint8);
     uint32Value = 0;
-    REQUIRE(registry.setConfigurationUnsafe(100, testValueUint32) == true);
-    REQUIRE(registry.getConfigurationUnsafe(100, uint32Value) == true);
+    REQUIRE(registry.setConfigurationUnsafe(coordinateId, testValueUint32) ==
+            true);
+    REQUIRE(registry.getConfigurationUnsafe(coordinateId, uint32Value) == true);
     REQUIRE(uint32Value == testValueUint32);
     /*! Checks that get configuration is false if the type is incorrect w.r.t.
      * the type of the set type */
-    REQUIRE(registry.getConfigurationUnsafe(100, floatValue) == false);
+    REQUIRE(registry.getConfigurationUnsafe(coordinateId, floatValue) == false);
+}
+
+TEST_CASE("RegistryFrontend test - Arm/Disarm test")
+{
+    RegistryFrontend registry;
+    uint8_t uint8Value;
+    Coordinates coordinatesValue(testValueLatitude, testValueLongitude),
+        coordinateGet;
+    REQUIRE(registry.setConfigurationUnsafe(coordinateId, coordinatesValue) ==
+            true);
+    registry.arm();
+    /*! If the registry is "armed" no set are allowed but gets are */
+    REQUIRE(registry.setConfigurationUnsafe(algorithmId, testValueUint8) ==
+            false);
+    REQUIRE(registry.getConfigurationUnsafe(algorithmId, uint8Value) == false);
+    REQUIRE(registry.getConfigurationUnsafe(coordinateId, coordinateGet) ==
+            true);
+    REQUIRE(coordinateGet.latitude == coordinatesValue.latitude);
+    REQUIRE(coordinateGet.longitude == coordinatesValue.longitude);
+    registry.disarm();
+    REQUIRE(registry.setConfigurationUnsafe(algorithmId, testValueUint8) ==
+            true);
+    REQUIRE(registry.getConfigurationUnsafe(algorithmId, uint8Value) == true);
+    REQUIRE(uint8Value == testValueUint8);
+}
+TEST_CASE("RegistryFrontend test - serialization/deserialization test")
+{
+    RegistryFrontend registry;
+    Coordinates coordinatesValue(testValueLatitude, testValueLongitude),
+        coordinateGet;
+    REQUIRE(registry.setConfigurationUnsafe(coordinateId, coordinatesValue) ==
+            true);
+    REQUIRE(registry.saveConfiguration() == false);
+    {
+        uint32_t valueInt;
+        std::vector<uint8_t>& vector = registry.getSerializedConfiguration();
+        for (int i = 0; i < 8; i++)
+            REQUIRE(vector.at(i) == 0);
+        REQUIRE(static_cast<int>(vector.at(8)) == 1);
+        REQUIRE(static_cast<int>(vector.at(9)) == 30);
+        /*! Checksum */
+        // REQUIRE(static_cast<int>(vector.at(10)) == boh);
+        // REQUIRE(static_cast<int>(vector.at(11)) == boh);
+        // REQUIRE(static_cast<int>(vector.at(12) == boh);
+        // REQUIRE(static_cast<int>(vector.at(13) == boh);
+        /*! ID 8*/
+        REQUIRE(static_cast<int>(vector.at(14)) == 0);
+        REQUIRE(static_cast<int>(vector.at(15)) == 0);
+        REQUIRE(static_cast<int>(vector.at(16)) == 0);
+        REQUIRE(static_cast<int>(vector.at(17)) == 8);
+        auto it = vector.begin() + 14;
+        EntryStructsUnion::getFromSerializedVector(valueInt, it, vector.end());
+        REQUIRE(valueInt == 8);
+        /*! ID TYPE 2:coordinates*/
+        REQUIRE(static_cast<int>(vector.at(18)) == 0);
+        REQUIRE(static_cast<int>(vector.at(19)) == 0);
+        REQUIRE(static_cast<int>(vector.at(20)) == 0);
+        REQUIRE(static_cast<int>(vector.at(21)) == 2);
+        /*! LATITUDE 01000010 00110110 00000001 00011110 */
+        REQUIRE(static_cast<int>(vector.at(22)) == 66);
+        REQUIRE(static_cast<int>(vector.at(23)) == 54);
+        REQUIRE(static_cast<int>(vector.at(24)) == 1);
+        REQUIRE(static_cast<int>(vector.at(25)) == 30);
+
+        /*! LONGITUDE 01000001 00010010 10000000 01010100 */
+        REQUIRE(static_cast<int>(vector.at(26)) == 65);
+        REQUIRE(static_cast<int>(vector.at(27)) == 18);
+        REQUIRE(static_cast<int>(vector.at(28)) == 128);
+        REQUIRE(static_cast<int>(vector.at(29)) == 84);
+    }
+    REQUIRE(registry.loadConfiguration() == true);
+    REQUIRE(registry.getConfigurationUnsafe(coordinateId, coordinateGet) ==
+            true);
+    REQUIRE(coordinateGet.latitude == coordinatesValue.latitude);
+    REQUIRE(coordinateGet.longitude == coordinatesValue.longitude);
 }
