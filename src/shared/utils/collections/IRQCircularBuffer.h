f2b7f00e5a0434f9aa1899461200f0e8393a833f
Niccolò Betto <niccolo.betto@skywarder.eu>
2024-02-16 12:40:05 +0100

    [Copyright] Minor fix to copyright notices

--- src/shared/utils/collections/IRQCircularBuffer.h
+++ src/shared/utils/collections/IRQCircularBuffer.h
@@ -1,5 +1,5 @@
 /* Copyright (c) 2015-2018 Skyward Experimental Rocketry
- * Author: Luca Erbetta, Davide Mor
+ * Authors: Luca Erbetta, Davide Mor
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
