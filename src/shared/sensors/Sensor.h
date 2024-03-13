1f1ecd07b00989d5eb9b11d85a545ded8158789b
Matteo Pignataro <matteo.pignataro@skywarder.eu>
2024-03-13 18:13:51 +0000

    [Sensor] Added mutex for synchronization purposes

--- src/shared/sensors/Sensor.h
+++ src/shared/sensors/Sensor.h
@@ -22,6 +22,8 @@
 
 #pragma once
 
+#include <miosix.h>
+
 #include <array>
 #include <type_traits>
 
@@ -97,15 +99,32 @@ protected:
      */
     virtual Data sampleImpl() = 0;
 
+    // Thread safe mutex to synchronize writes and reads
+    miosix::FastMutex mutex;
+
 public:
     virtual ~Sensor() {}
 
-    void sample() override { lastSample = sampleImpl(); }
+    void sample() override
+    {
+        // Sampling outside of the protected zone ensures that the sampling
+        // action cannot cause locks or delays
+        Data d = sampleImpl();
+
+        {
+            miosix::Lock<miosix::FastMutex> l(mutex);
+            lastSample = d;
+        }
+    }
 
     /**
      * @return last available sample from this sensor
      */
-    virtual const Data& getLastSample() { return lastSample; }
+    virtual const Data& getLastSample()
+    {
+        miosix::Lock<miosix::FastMutex> l(mutex);
+        return lastSample;
+    }
 };
 
 /**
