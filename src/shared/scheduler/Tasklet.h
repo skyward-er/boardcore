e6b2a1111f67758cf974ab92962dcd2886211c73
Davide Mor <davide.mor@skywarder.eu>
2024-03-23 13:09:44 +0100

    [Tasklet] Added documentation

--- src/shared/scheduler/Tasklet.h
+++ src/shared/scheduler/Tasklet.h
@@ -27,12 +27,22 @@
 namespace Boardcore
 {
 
+/**
+ * @brief Wrapper around ActiveObject to help with lightweight tasks.
+ */
 class Tasklet : public ActiveObject
 {
 public:
     using StopFlag = std::atomic<bool>;
     using Function = std::function<void(StopFlag&)>;
 
+    /**
+     * @brief Create a tasklet.
+     *
+     * @param function Function to be called.
+     * @param priority Priority of the tasklet.
+     * @param stacksize Size of the stack of the tasklet.
+     */
     explicit Tasklet(Function function,
                      miosix::Priority priority = miosix::MAIN_PRIORITY,
                      unsigned int stacksize = miosix::STACK_DEFAULT_FOR_PTHREAD)
@@ -46,17 +56,34 @@ private:
     Function function;
 };
 
+/**
+ * @brief Wrapper around ActiveObject to help with lightweight periodic tasks.
+ */
 class PeriodicTasklet : public ActiveObject
 {
 private:
     using Function = std::function<void()>;
 
+    /**
+     * @brief Task behavior policy.
+     */
     enum class Policy
     {
-        SKIP,
-        RECOVER
+        SKIP,    //< If the task misses it's deadline it's postponed at the next
+                 // deadline.
+        RECOVER  //< If the task misses it's deadline it's immediately
+                 // rescheduled.
     };
 
+    /**
+     * @brief Create a preiodic tasklet.
+     *
+     * @param function Function to be called.
+     * @param period Period of the tasklet.
+     * @param policy Policy of the tasklet.
+     * @param priority Priority of the tasklet.
+     * @param stacksize Size of the stack of the tasklet.
+     */
     explicit PeriodicTasklet(
         Function function, uint32_t period, Policy policy = Policy::RECOVER,
         miosix::Priority priority = miosix::MAIN_PRIORITY,
