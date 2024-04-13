7de13fc5312ed1a3bab2b560a26f18d2825922f8
Valerio Flamminii <valerio.flamminii@skywarder.eu>
2024-04-13 08:32:09 +0200

    versione funzionante testata

--- src/tests/drivers/QuadSpi-Flash/qspi-flash.cpp
+++ src/tests/drivers/QuadSpi-Flash/qspi-flash.cpp
@@ -75,7 +75,7 @@ void QSPI::init()
 
     RCC_SYNC();
 
-    Thread::sleep(200);
+    Thread::sleep(1);
 
     // abort possible ongoing command
     QUADSPI->CR |= QUADSPI_CR_ABORT;
@@ -128,8 +128,8 @@ void QSPI::abort_reset()
     // reset configuration register
     QUADSPI->CCR = 0;
 
-    // transfer flag (TCF) will reset automatically if a transaction has been
-    // aborted.
+    // transfer flag (TCF) will reset automatically and FIFO is flushed
+    // if a transaction has been aborted.
 }
 
 void QSPI::waitBusy()
@@ -156,10 +156,18 @@ void QSPI::waitTransfer()
 // constructor class qspi_flash
 qspi_flash::qspi_flash() { ; }
 
+bool qspi_flash::test()
+{
+    // init qspi peripheral and check the device ID of the flash
+
+    QSPI::init();
+    return readID() == DEVICE_ID ? true : false;
+}
+
 uint8_t qspi_flash::read_status_reg()
 {
 
-    // status register can be reaa at any time and during every kind of
+    // status register can be read at any time and during every kind of
     // operation. indirect read mode, la comunicazione comincia dopo aver
     // scritto l'istruzione nel CCR register. 1 invio Read status register
     // command 2 ricevo un byte di dati con il valore dello status register.
@@ -245,22 +253,32 @@ uint32_t qspi_flash::readID()
     // Expect to receive 3 bytes regarding ID of the flash
     QUADSPI->DLR = 2;
 
-    // DEVE ESSERE FATTO PRIMA DI SCRIVERE L'ISTRUZIONE NEL REGISTRO,
-    // PERCHè IN QUESTO CASO LA COMUNICAZIONE COMINCIA SCIVENDO L'ISTRUZIONE IN
-    // CCR
     QSPI::enable();
 
-    // Trigger communication start by writing the instruction
+    // Trigger communication by writing the instruction into CCR register
     QUADSPI->CCR |= Commands::READ_ID << QUADSPI_CCR_INSTRUCTION_Pos;
 
     // wait till communication is ended
     QSPI::waitTransfer();
 
+    // sus: even if some bytes have been received, the FIFO need some time to
+    // store them.
+    Thread::sleep(1);
+
     // if there are some bytes in the quadspi buffer (FIFO), read them
-    if (QUADSPI->SR & (63 << QUADSPI_SR_FLEVEL_Pos))
+    if ((QUADSPI->SR & QUADSPI_SR_FLEVEL) >> 8)
     {
         uint32_t myID = QUADSPI->DR;
+
         QSPI::disable();
+
+        // the ID bytes order must be inverted
+        uint8_t lb = myID & (255U);          // lowest byte
+        uint8_t mb = (myID >> 8) & (255U);   // middle byte
+        uint8_t hb = (myID >> 16) & (255U);  // highest byte
+
+        myID = (lb << 16) | (mb << 8) | (hb);
+
         return myID;
     }
     else
@@ -322,7 +340,7 @@ void qspi_flash::waitProgress()
 
     while (isInProgress())
     {
-        Thread::sleep(2);
+        Thread::sleep(1);
     }
 }
 
@@ -337,6 +355,10 @@ uint8_t qspi_flash::read_byte(uint32_t address)
     // 3 read only the first byte of data
     // in this case, since no data is needed, the communication starts whenever
     // the adress register (QUADSPI->DR) is updated.
+
+    // check on correct address range
+    if (address > FlashMemory::MEMORY_SIZE)
+        return 0;
 
     QSPI::abort_reset();
 
@@ -420,6 +442,10 @@ bool qspi_flash::sector_erase(uint32_t address)
     // 5 write_disable command to be sure
     // 6 check the result (flasg E_FAIL)
 
+    // check on address range
+    if ((address < 0) || (address > FlashMemory::MEMORY_SIZE))
+        return false;
+
     waitProgress();
 
     write_enable();
@@ -461,6 +487,10 @@ bool qspi_flash::block32_erase(uint32_t address)
     // 4 wait till flash has completed the operation
     // 5 write_disable command to be sure
     // 6 check the result (flasg E_FAIL)
+
+    // check on correct address range
+    if ((address < 0) || (address > FlashMemory::MEMORY_SIZE))
+        return false;
 
     waitProgress();
 
@@ -504,6 +534,10 @@ bool qspi_flash::block64_erase(uint32_t address)
     // 5 write_disable command, just to be sure
     // 6 check the result (flasg E_FAIL)
 
+    // check on correct address range
+    if ((address < 0) || (address > FlashMemory::MEMORY_SIZE))
+        return false;
+
     waitProgress();
 
     write_enable();
@@ -535,8 +569,10 @@ bool qspi_flash::block64_erase(uint32_t address)
     return check_erase();
 }
 
-bool qspi_flash::byte_program(uint8_t data, uint32_t address)
+bool qspi_flash::byte_program(uint8_t data, uint32_t address, bool verify)
 {
+
+    // double-check data may take some extra time !
 
     // write a byte at the specified address (any address is ok)
     // 1 wait till every memory operation is ended
@@ -545,6 +581,10 @@ bool qspi_flash::byte_program(uint8_t data, uint32_t address)
     // 4 wait till the operation is ended
     // 5 write disable
     // 6 return the result
+
+    // check on address range
+    if ((address < 0) || (address > FlashMemory::MEMORY_SIZE))
+        return false;
 
     waitProgress();
 
@@ -575,6 +615,13 @@ bool qspi_flash::byte_program(uint8_t data, uint32_t address)
 
     write_disable();
 
+    // if verify = true, double check on byte program operation
+    if (verify == true)
+    {
+        if (read_byte(address) != data)
+            return false;
+    }
+
     return check_program();
 }
 
@@ -603,7 +650,10 @@ uint8_t qspi_flash::read_security_reg()
     // wait till data trasfer is complete
     QSPI::waitTransfer();
 
-    uint8_t value = (uint8_t)QUADSPI->DR;
+    // sus, ma funziona, stesso problema nella readID()
+    Thread::sleep(1);
+
+    uint32_t value = (uint8_t)QUADSPI->DR;
 
     QSPI::disable();
 
@@ -663,11 +713,11 @@ void qspi_flash::software_reset()
 bool qspi_flash::check_erase()
 {
 
-    // check for bit E_FAIL in security register of memory:
-    // E_fail = 1 the last erase operation has failed
-    // e_fail = 0 tha last operation has succeded
-    // returns true  - erase operation has succeded
-    // returns false - erase operation has failed
+    // ATTTENTION! - this function check only if the last erase operation has
+    // been completed, so it's not so reliable ! check for bit E_FAIL in
+    // security register of memory: E_fail = 1 the last erase operation has
+    // failed e_fail = 0 tha last operation has succeded returns true  - erase
+    // operation has succeded returns false - erase operation has failed
 
     uint8_t reg = read_security_reg();
     return reg & (1 << 6) ? false : true;
@@ -676,10 +726,225 @@ bool qspi_flash::check_erase()
 bool qspi_flash::check_program()
 {
 
-    // check for bit P_FAIL in security register of memory:
-    // returns true  - erase operation has succeded
-    // returns false - erase operation has failed
+    // ATTTENTION! - this function check only if the last operation has been
+    // completed, so it's not so reliable ! check for bit P_FAIL in security
+    // register of memory: returns true  - erase operation has succeded returns
+    // false - erase operation has failed
 
     uint8_t reg = read_security_reg();
     return reg & (1 << 5) ? false : true;
+}
+
+bool qspi_flash::read_sector(std::vector<uint8_t>& vector, uint32_t sector_num)
+{
+
+    // read an entire sector of the flash into a vector
+    // that will modify the vector and his elements.
+    // that fix size and capacity of the vector to SECTOR_SIZE.
+
+    if (sector_num < 0 || sector_num > SECTORS_NUM)
+    {
+        return false;
+    }
+
+    // reserve enough bytes to retain a sector of the flash, also
+    // make sure that size parameter match with the capacity.
+    if (vector.capacity() < SECTOR_SIZE)
+    {
+        vector.reserve(SECTOR_SIZE);
+        vector.resize(vector.capacity());  // SUS, altrimenti size != capacity
+    }
+
+    if (vector.capacity() >= SECTOR_SIZE)
+    {
+        vector.resize(SECTOR_SIZE);
+        vector.shrink_to_fit();
+    }
+
+    uint32_t addr = SECTOR_SIZE * sector_num;
+
+    uint32_t index = 0;
+    for (index = 0; index < vector.capacity(); index++)
+    {
+        vector[index] = read_byte(addr);
+        addr++;
+    }
+
+    return true;
+}
+
+bool qspi_flash::page_program(std::vector<uint8_t>& vector,
+                              uint32_t start_address, bool verify)
+{
+
+    // fifo interna: 32 bytes
+    // imposta prima il dlr
+    // scrivi byte per byte nel DR
+    // program the last 256 bytes of a vector starting by a specific page
+    // address. WEL bit is set to zero automatically after the program
+    // operation. NO WRITE_DISABLE the address specified must be a starting
+    // address of a page !!!!
+
+    // not a starting address of a page
+    if ((start_address % PAGE_SIZE) != 0)
+        return false;
+
+    // start_address out of address space of the memory
+    if (start_address >= MEMORY_SIZE)
+        return false;
+
+    // empty vector
+    if (vector.size() == 0)
+        return false;
+
+    // vector bigger than a page size
+    if (vector.size() > PAGE_SIZE)
+        return false;
+
+    write_enable();
+
+    QSPI::abort_reset();
+
+    // idirect write mode
+    QUADSPI->CCR |= QUADSPI_CCR_DMODE_0 |   // data on a single line
+                    QUADSPI_CCR_ADSIZE_1 |  // address size 24 bit
+                    QUADSPI_CCR_ADMODE_0 |  // address on a single line
+                    QUADSPI_CCR_IMODE_0;    // instruction on a single line
+
+    QSPI::enable();
+
+    // page program command
+    QUADSPI->CCR |= Commands::PAGE_PROGRAM;
+
+    // number of bytes to be transferred
+    QUADSPI->DLR = vector.size() - 1;
+
+    // starting address
+    QUADSPI->AR = start_address;
+
+    // load data vector into the QUADSPI FIFO
+    uint16_t i = 0;
+    for (i = 0; i < vector.size(); i++)
+    {
+
+        // if FIFO is full - wait till it has at least a byte available.
+        while (((QUADSPI->SR & QUADSPI_SR_FLEVEL) >> 8) >= 32)
+        {
+            ;
+        }
+
+        // add a single byte to be sent into the QSPI FIFO
+        ((uint8_t*)&QUADSPI->DR)[0] = static_cast<uint8_t>(vector[i]);
+    }
+
+    QSPI::waitBusy();
+
+    waitProgress();
+
+    // if verify flag is set, double check on written data
+    if (verify == true)
+    {
+        for (i = 0; i < vector.size(); i++)
+        {
+
+            if (read_byte(start_address + i) != vector[i])
+                return false;
+        }
+    }
+
+    return check_program();
+}
+
+bool qspi_flash::write_vector(std::vector<uint8_t>& vector, uint32_t sector_num,
+                              bool verify_write)
+{
+
+    // this function program into the flash just the real elements of the vector
+    // (size). THE VECTOR PASSED MUST HAVE SAME SIZE AND CAPACITY. return true:
+    // program operation succeded return false: program operation failed
+
+    // wrong secton_num specified
+    if (sector_num < 0 || sector_num >= SECTORS_NUM)
+    {
+        return false;
+    }
+
+    // if the vector doesn't have elements
+    if (vector.size() == 0 || vector.capacity() == 0)
+    {
+        return false;
+    }
+
+    // if the vector is bigger than the flash capacity
+    if (vector.size() > MEMORY_SIZE)
+        return false;
+
+    // if the whole vector is bigger than the rest of sectors starting by
+    // sector_num
+    if (vector.size() > (SECTOR_SIZE * (SECTORS_NUM - sector_num)))
+        return false;
+
+    // constant start address
+    uint32_t const start_address = SECTOR_SIZE * sector_num;
+
+    // compute number of sectors needed to store the vector, then add one more
+    // partial sector for the last part of vector.
+    uint32_t sectors_needed = vector.size() / SECTOR_SIZE;
+    if ((vector.size() % SECTOR_SIZE) != 0)
+        sectors_needed += 1;
+
+    // erase all sectors needed to store the entire vector
+    uint32_t sector_index = 0;
+    for (sector_index = 0; sector_index < sectors_needed; sector_index++)
+    {
+        if (sector_erase(SECTOR_SIZE * (sector_num + sector_index)) == false)
+            return false;
+    }
+
+    // compute the number of pages needed to store the entire vector
+    uint32_t pages_needed = vector.size() / PAGE_SIZE;
+    if ((vector.size() % PAGE_SIZE) != 0)
+        pages_needed += 1;
+
+    // creating a copy vector
+    std::vector<uint8_t> v;
+    v.reserve(PAGE_SIZE);
+    v.resize(0);
+
+    // for every page needed
+    uint32_t page = 0;
+    uint32_t elem = 0;
+    for (page = 0; page < pages_needed; page++)
+    {
+        v.resize(0);
+        uint32_t start_elem = page * PAGE_SIZE;
+        for (elem = start_elem;
+             (elem < start_elem + PAGE_SIZE) && (elem < vector.size()); elem++)
+        {
+            v.push_back(vector[elem]);
+        }
+
+        if (page_program(v, start_address + (page * PAGE_SIZE), false) == false)
+            return false;
+    }
+
+    // if verify_write is true:
+    // check that the bytes written into the flash match with the ones in the
+    // vector.
+    if (verify_write)
+    {
+
+        uint32_t index = 0;
+        uint32_t addr  = start_address;
+        for (index = 0; index < vector.size(); index++)
+        {
+            if (read_byte(addr) != vector[index])
+            {
+                return false;
+            }
+            addr++;
+        }
+    }
+
+    return true;
 }
