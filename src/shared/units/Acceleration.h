41a3920dee8818720f18496b01fd3c4130ac8f98
Davide Basso <davide.basso@skywarder.eu>
2024-05-04 20:59:44 +0200

    [Style] Fix copyright linting issue

--- src/shared/units/Acceleration.h
+++ src/shared/units/Acceleration.h
@@ -1,5 +1,5 @@
 /* Copyright (c) 2023 Skyward Experimental Rocketry
- * Authors: Davide Basso
+ * Author: Davide Basso
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
