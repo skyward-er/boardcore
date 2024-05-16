eaf730943173b2dc1c2ddf11b7ef62ac5267275f
Emilio Corigliano <emilio.corigliano@skywarder.eu>
2024-05-16 13:27:41 +0200

    [HILSensor] Implemented the HILSensor class and the hillificator function
    The HILSensor class, templated on a real sensor of type T, overrides the sampleImpl method to use the passed mapping function to generate the last sample from simulator data. The init and selfTest methods are overridden so that the real methods are called only if the "hardware is enabled". Also, a getRealLastSample method is implemented so that, in case that the "hardware is enabled", we can get also the corresponding real sample.
    
    The hillificator function is an helper function that, once passed the sensor pointer, the flag to enable the hardware and the mapping function, returns the corresponding HILSensor. The returned sensor behaves just like the real sensor, but the sample that it returns comes from the updateData function (which usually maps the simulator data to the data returned by the sensor).

--- src/shared/sensors/Sensor.h
+++ src/shared/sensors/Sensor.h
@@ -1,5 +1,5 @@
 /* Copyright (c) 2016-2020 Skyward Experimental Rocketry
- * Authors: Alain Carlucci, Luca Conterio
+ * Authors: Alain Carlucci, Luca Conterio, Davide Mor, Emilio Corigliano
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -133,6 +133,110 @@ public:
         return lastSample;
     }
 };
+
+/**
+ * @brief Class that wraps a real sensor to perform HIL simulations.
+ *
+ * This class extends a real sensor class T. For the real sensor construction is
+ * used the move constructor of an already created T sensor. The sampleImpl
+ * method is overridden in order to expose the sample returned by the updateData
+ * function passed in the constructor.
+ * In this way, the corresponding HILSensor behaves exactly like a sensor T, but
+ * returns the data given by the simulator.
+ *
+ * The real hardware could be used passing a true enableHw flag; In this case,
+ * when sample method is called, it is first sampled the real sensor and then
+ * the data from the simulator are mapped into the SensorData struct.
+ */
+template <class T>
+class HILSensor : public T
+{
+public:
+    // A requirement is that the class T must be move constructible
+    static_assert(std::is_move_constructible<T>::value,
+                  "T must be move constructible!");
+    // A requirement is that the class T must be a Sensor
+    static_assert(std::is_base_of<Sensor<typename T::DataType>, T>::value,
+                  "T must inherit from Sensor<T::DataType>");
+
+    using UpdateFn = std::function<typename T::DataType(void)>;
+
+    /**
+     * @brief Constructor of the HILSensor which decorates the passed sensor
+     * with the sampling of the simulator data.
+     *
+     * @param sensor The sensor to be wrapped.
+     * @param enableHw Flag to enable the sampling of the real hardware.
+     * @param updateData Function that maps the data from the simulator data to
+     * the sensor current sample.
+     */
+    HILSensor(T&& sensor, bool enableHw, UpdateFn updateData)
+        : T{std::move(sensor)}, enableHw{enableHw}, updateData{updateData}
+    {
+    }
+
+    bool init() override { return (enableHw ? T::init() : true); }
+
+    bool selfTest() override { return (enableHw ? T::selfTest() : true); }
+
+    /**
+     * @brief Overridden sampleImpl method so that the data could be actually
+     * taken from the simulator.
+     *
+     * If the hardware is enabled, it also samples the real sensor.
+     *
+     * @return The data struct with the current simulator sample.
+     */
+    typename T::DataType sampleImpl() override
+    {
+        if (enableHw)
+        {
+            auto realSample = T::sampleImpl();
+            {
+                miosix::Lock<miosix::FastMutex> l(mutexRealData);
+                lastRealSample = realSample;
+            }
+        }
+
+        return updateData();
+    }
+
+    /**
+     * @brief Method analogous to getLastSample but returns the real sample
+     * taken from the hardware sensor.
+     *
+     * @return The data struct with the current sensor sample.
+     */
+    typename T::DataType getRealLastSample()
+    {
+        miosix::Lock<miosix::FastMutex> l(mutexRealData);
+        return lastRealSample;
+    }
+
+protected:
+    typename T::DataType lastRealSample;  // Last sample of the real sensor
+    bool enableHw;                        // Flag to enable hardware sampling
+    UpdateFn updateData;  // Function to get the lastSample from simulatorData
+
+    // Thread safe mutex to synchronize writes and reads on lastRealSample
+    miosix::FastMutex mutexRealData;
+};
+
+/**
+ * @brief Static method that hillificates sensors, turning a real sensor in
+ * a HILSensor.
+ *
+ * Creates a HILSensor of the sensor type T. The passed sensor must be an
+ * already instantiated sensor.
+ */
+template <typename T>
+static T* hillificator(T* sensor, bool enableHw,
+                       typename HILSensor<T>::UpdateFn updateData)
+{
+    sensor = new Boardcore::HILSensor<T>(std::move(*sensor), enableHw,
+                                         std::move(updateData));
+    return sensor;
+}
 
 /**
  * @brief Interface for sensor that implement a FIFO.
