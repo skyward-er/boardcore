622876ef845cd0a34138a6fae50ec5c0490df479
Nicolò Caruso <niccolo.caruso@skywarder.eu>
2024-06-23 11:07:01 +0200

    [BSP] Added stepper pins initialization
    Stepper pins initialization was missing from the BSP

--- src/bsps/stm32f767zi_lyra_gs/interfaces-impl/bsp.cpp
+++ src/bsps/stm32f767zi_lyra_gs/interfaces-impl/bsp.cpp
@@ -326,6 +326,13 @@ void IRQbspInit()
     ethernet::nrst::high();
     ethernet::intr::mode(Mode::INPUT);
 
+    stepper1::enable::mode(Mode::OUTPUT);
+    stepper1::enable::high();
+    stepper1::direction::mode(Mode::OUTPUT);
+    stepper2::enable::mode(Mode::OUTPUT);
+    stepper2::direction::mode(Mode::OUTPUT);
+    stepper2::enable::high();
+
     DefaultConsole::instance().IRQset(intrusive_ref_ptr<Device>(new STM32Serial(
         defaultSerial, defaultSerialSpeed, STM32Serial::NOFLOWCTRL)));
 }
