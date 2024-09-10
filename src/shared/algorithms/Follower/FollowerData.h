5fe9d06671d6b2cc405e773972a4c96382a9cbb4
Federico Lolli <federico.lolli@skywarder.eu>
2024-09-11 00:04:51 +0200

    [Follower] Add follower algorithm from obsw
    [Follower] Add minimize rotationa as suggested in previous obsw pull req.
    
    See [this](https://git.skywarder.eu/avn/swd/obsw/-/merge_requests/34)
    for more context
    
    [logger] Updated logger
    
    [ARP Follower] Yaw gain now 0.1
    
    The yaw gain gain now is no more 1 but fixed to 0.1
    
    [ARP] Added timestamp to AntennaAngles for log
    
    Added timestamp to the AntennaAngles logged. Changed accordingly the constructors.
    
    [Fix] Follower algorithm limitation issue due to proportional on yaw
    
    The 0.1* product to have a proportional on the yaw was creating issues with the angle limiting function.
    
    [ARP] Added nrPropagations for log in AntennaAngle now logged by SMA
    
    Added nrPropagations to AntennaAngles
    Moved the log of it from the Follower to SMA
    
    [ARP] Follower initRocketCoordinates now is NASOrigin
    
    Changed semantic of initialRocketCoordinates to NASOrigin since now it is set always as we have a NASOrigin.
    
    [ARP] WIP fix for online initial coordinates support
    
    Add support for initial coordinates online setting
    
    [ARP] Follower Mutexes for antenna and NAS origin's coordinates
    
    Now getter states to protect such variables with mutexes

---
+++ src/shared/algorithms/Follower/FollowerData.h
@@ -0,0 +1,116 @@
+/* Copyright (c) 2024 Skyward Experimental Rocketry
+ * Author: Emilio Corigliano
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#pragma once
+
+#include <sensors/SensorData.h>
+
+namespace Boardcore
+{
+
+struct NEDCoords
+{
+    float n = 0;
+    float e = 0;
+    float d = 0;
+};
+
+/**
+ * @brief A structure for storing angles relative to the NED frame.
+ */
+struct AntennaAngles
+{
+    uint64_t timestamp = 0;
+
+    float yaw;    //!< Angle between the X axis (N axis) and the target position
+                  //!< on the XY plane (NE plane), positive anti-clockwise [deg]
+    float pitch;  //!< Angle between the XY plane (NE plane) and the target
+    //!< position [deg]
+    uint32_t nrPropagations =
+        0;  //!< Nr of propagations by the propagator (0 if no propagation)
+
+    AntennaAngles() : timestamp{0}, yaw(0), pitch(0), nrPropagations{0} {};
+    AntennaAngles(uint64_t timestamp, float yaw, float pitch)
+        : timestamp{timestamp}, yaw(yaw), pitch(pitch), nrPropagations{0} {};
+    AntennaAngles(uint64_t timestamp, float yaw, float pitch,
+                  uint32_t nrPropagations)
+        : timestamp{timestamp}, yaw(yaw),
+          pitch(pitch), nrPropagations{nrPropagations} {};
+
+    static std::string header()
+    {
+        return "timestamp,yaw,pitch,nrPropagations\n";
+    }
+
+    void print(std::ostream& os) const
+    {
+        os << timestamp << "," << yaw << "," << pitch << "," << nrPropagations
+           << "\n";
+    }
+};
+
+struct FollowerState
+{
+    uint64_t timestamp;
+    float yaw;    // [deg]
+    float pitch;  // [deg]
+    float horizontalSpeed;
+    float verticalSpeed;
+
+    FollowerState()
+        : timestamp(0), yaw(0), pitch(0), horizontalSpeed(0), verticalSpeed(0)
+    {
+    }
+
+    static std::string header()
+    {
+        return "timestamp,yaw,pitch,horizontalSpeed,verticalSpeed\n";
+    }
+
+    void print(std::ostream& os) const
+    {
+        os << timestamp << "," << yaw << "," << pitch << "," << horizontalSpeed
+           << "," << verticalSpeed << "\n";
+    }
+};
+
+/**
+ * @brief A structure for logging the ARP system coordinates set in the
+ * Follower.
+ */
+struct LogAntennasCoordinates : public GPSData
+{
+    explicit LogAntennasCoordinates(const GPSData& data) : GPSData(data) {}
+
+    LogAntennasCoordinates() {}
+};
+
+/**
+ * @brief A structure for logging the Rocket coordinates set in the Follower.
+ */
+struct LogRocketCoordinates : public GPSData
+{
+    explicit LogRocketCoordinates(const GPSData& data) : GPSData(data) {}
+
+    LogRocketCoordinates() {}
+};
+}  // namespace Boardcore
