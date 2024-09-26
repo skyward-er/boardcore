289690759dd8cd1fa2c798214bdda044bbdf5fdd
Fabrizio Monti <fabrizio.monti@skywarder.eu>
2024-09-26 15:37:56 +0200

    [VN100] Adapted vn100-serial driver to VNCommonSerial and implemented the binary output.

--- src/shared/sensors/Vectornav/VN100/VN100SerialData.h
+++ src/shared/sensors/Vectornav/VN100/VN100SerialData.h
@@ -1,5 +1,5 @@
 /* Copyright (c) 2021 Skyward Experimental Rocketry
- * Author: Matteo Pignataro
+ * Author: Matteo Pignataro, Fabrizio Monti
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
