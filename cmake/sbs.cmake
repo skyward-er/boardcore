4bae80b00e99dc78bf0e958754804e1046446fef
Davide Basso <davide.basso@skywarder.eu>
2024-11-04 18:12:20 +0100

    [SBS] Update list boards command

--- cmake/sbs.cmake
+++ cmake/sbs.cmake
@@ -28,21 +28,6 @@ cmake_path(GET CMAKE_CURRENT_LIST_DIR PARENT_PATH SBS_BASE)
 list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
 include(boardcore)
 
-# Command to print all the available boards used by the sbs script
-string(REGEX MATCHALL "bsps\/[^\/]+" BOARDCORE_BOARDS ${BOARDCORE_BOARDS_OPTIONS_FILES})
-string(REGEX MATCHALL "arch\/[^\/]+" MIOSIX_BOARDS ${MIOSIX_BOARDS_OPTIONS_FILES})
-# Replace "/bsps" and "/arch" with empty string, as lookbehind is not supported
-string(REGEX REPLACE "bsps\/" "" BOARDCORE_BOARDS "${BOARDCORE_BOARDS}")
-string(REGEX REPLACE "arch\/" "" MIOSIX_BOARDS "${MIOSIX_BOARDS}")
-
-string(REPLACE ";" "\\n" BOARDS_STR "${MIOSIX_BOARDS};${BOARDCORE_BOARDS}")
-add_custom_target(
-    help-boards
-    COMMAND printf ${BOARDS_STR}
-    COMMENT "All boards available:"
-    VERBATIM
-)
-
 # Function to link the Boardcore library to the target
 function(sbs_target TARGET OPT_BOARD)
     if(NOT OPT_BOARD)
