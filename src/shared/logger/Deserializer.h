96d2d035023e282e38b67aca4bda4e8612bc726b
Pietro Bortolus <pietro.bortolus@skywarder.eu>
2025-05-16 17:43:45 +0200

    [Logger] updated the Deserializer class

--- src/shared/logger/Deserializer.h
+++ src/shared/logger/Deserializer.h
@@ -1,5 +1,5 @@
-/* Copyright (c) 2015-2022 Skyward Experimental Rocketry
- * Authors: Luca Erbetta, Alberto Nidasio
+/* Copyright (c) 2015-2025 Skyward Experimental Rocketry
+ * Authors: Luca Erbetta, Alberto Nidasio, Pietro Bortolus
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -29,8 +29,11 @@
 #include <fstream>
 #include <iostream>
 #include <limits>
+#include <map>
 #include <ostream>
 #include <regex>
+#include <sstream>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
@@ -44,6 +47,8 @@ namespace Boardcore
 {
 
 typedef std::numeric_limits<float> flt;
+typedef float float32_t;
+typedef double float64_t;
 
 // linter off
 
@@ -60,19 +65,6 @@ public:
     Deserializer(std::string fileName);
 
     ~Deserializer();
-
-    /**
-     * @brief Register a type to be deserialized.
-     *
-     * Node: The object type must provide a static header function and a print
-     * function with the following prototypes:
-     * static std::string header()
-     * void print(std::ostream& os) const
-     *
-     * @tparam T The object type to be deserialized.
-     */
-    template <typename T>
-    void registerType();
 
     /**
      * @brief Deserializes the provided file.
@@ -98,14 +90,25 @@ private:
     template <typename T>
     void printType(T& t, std::string path = "", std::string prefix = "");
 
+    /**
+     * @brief Register all types from the mapping file.
+     *
+     * @param mapping The mapping file stream
+     */
+    template <typename F>
+    void registerTypes(F&& f);
+
     bool closed = false;
 
     std::map<std::string, std::ofstream*> fileStreams;
+    std::map<std::string, std::function<void(std::ifstream&)>> types;
+
     tscpp::TypePoolStream tps;
 
     std::string logFilename;
     std::string logFilenameWithoutExtension;
     std::string logFolderPath;
+    std::string mappingFileName;
 };
 
 Deserializer::Deserializer(std::string logFilename) : logFilename(logFilename)
@@ -114,19 +117,13 @@ Deserializer::Deserializer(std::string logFilename) : logFilename(logFilename)
     logFilenameWithoutExtension =
         logFilename.substr(0, logFilename.find_last_of("."));
     logFolderPath = logFilenameWithoutExtension + "/";
+
+    // prepare the mapping file name
+    mappingFileName = logFolderPath + "mapping" +
+                      logFilenameWithoutExtension.erase(0, 3) + ".dat";
 }
 
 Deserializer::~Deserializer() { close(); }
-
-template <typename T>
-void Deserializer::registerType()
-{
-    std::function<void(T & t)> callback =
-        std::bind(&Deserializer::printType<T>, this, std::placeholders::_1,
-                  logFolderPath, logFilenameWithoutExtension);
-
-    tps.registerType<T>(callback);
-}
 
 template <typename T>
 void Deserializer::printType(T& t, std::string path, std::string prefix)
@@ -173,6 +170,155 @@ void Deserializer::printType(T& t, std::string path, std::string prefix)
         t.print(std::ref(*stream));
 }
 
+/**
+ * @brief Register the types from the mapping file
+ *
+ * @param mapping The mapping file stream
+ */
+template <typename F>
+void registerTypes(F&& f)
+{
+    // open the mapping file
+    std::ifstream mapping(mappingFileName);
+
+    // iterate over all of the types' descriptions
+    while (mapping.peek() != -1)
+    {
+        // get the name of the type
+        std::string type_name;
+        // Read null terminated name string
+        while (mapping.peek() != 0)
+            type_name += mapping.get();
+        // Dummy null read
+        mapping.get();
+
+        // read the structure of the type and save it
+
+        // store the number of fields
+        uint8_t field_count;
+        mapping.read((char*)&field_count, sizeof field_count);
+
+        // Dummy null read
+        mapping.get();
+
+        // create an array to store the field types
+        std::vector<char> field_types(field_count);
+
+        for (int i = 0; i < field_count; i++)
+        {
+            // get the name of the field, we don't actually need it for this
+            // part but we need to read it to get the type
+            std::string field_name;
+            // Read null terminated name string
+            while (mapping.peek() != 0)
+                field_name += mapping.get();
+            // Dummy null read
+            mapping.get();
+
+            // get the type of the field
+            std::string field_type;
+            // Read null terminated name string
+            while (mapping.peek() != 0)
+                field_type += mapping.get();
+            // Dummy null read
+            mapping.get();
+
+            // add the field type to field_types
+            field_types[i] = field_type[0];
+        }
+
+        // register a callback for this type that takes field_types as an
+        // argument and prints them to std::cout after having converted the
+        // value read from the input stream into the correct type
+        types.insert({type_name, [f = std::move(f), field_types,
+                                  field_count](std::istream& is)
+                      {
+                          for (int i = 0; i < field_count; i++)
+                          {
+                              std::ostringstream
+                                  oss;  // Create a string stream for each field
+
+                              // Read the value from the input stream and
+                              // store it as the correct type based on the
+                              // strcutre described in field_types
+                              switch (field_types[i])
+                              {
+                                  case 'a':
+                                      int8_t int8;
+                                      is.read((char*)&int8, sizeof int8);
+                                      // The cast is necessary because int8_t
+                                      // could be interpreted as char
+                                      // otherwhise
+                                      oss << static_cast<int>(int8);
+                                      break;
+                                  case 's':
+                                      int16_t int16;
+                                      is.read((char*)&int16, sizeof int16);
+                                      oss << int16;
+                                      break;
+                                  case 'i':
+                                      int32_t int32;
+                                      is.read((char*)&int32, sizeof int32);
+                                      oss << int32;
+                                      break;
+                                  case 'l':
+                                      int64_t int64;
+                                      is.read((char*)&int64, sizeof int64);
+                                      oss << int64;
+                                      break;
+                                  case 'h':
+                                      uint8_t uint8;
+                                      is.read((char*)&uint8, sizeof uint8);
+                                      // The cast is necessary because C++
+                                      // sucks
+                                      oss << static_cast<unsigned int>(uint8);
+                                      break;
+                                  case 't':
+                                      uint16_t uint16;
+                                      is.read((char*)&uint16, sizeof uint16);
+                                      oss << uint16;
+                                      break;
+                                  case 'j':
+                                      uint32_t uint32;
+                                      is.read((char*)&uint32, sizeof uint32);
+                                      oss << uint32;
+                                      break;
+                                  case 'm':
+                                      uint64_t uint64;
+                                      is.read((char*)&uint64, sizeof uint64);
+                                      oss << uint64;
+                                      break;
+                                  case 'f':
+                                      float32_t float32;
+                                      is.read((char*)&float32, sizeof float32);
+                                      oss << float32;
+                                      break;
+                                  case 'd':
+                                      float64_t float64;
+                                      is.read((char*)&float64, sizeof float64);
+                                      oss << float64;
+                                      break;
+                                  default:
+                                      std::cerr
+                                          << "Unknown type: " << field_types[i]
+                                          << std::endl
+                                          << "Aborting deserialization"
+                                          << std::endl;
+                                      throw std::runtime_error(
+                                          "Unknown typeID: " +
+                                          std::string(1, field_types[i]));
+                                      return;
+                              }
+                              // Print the converted string
+                              f(oss);
+                          }
+                      }});
+        std::cout << "Registered type: " << type_name << std::endl;
+    }
+    mapping.close();
+    return;
+}
+
 bool Deserializer::deserialize()
 {
     if (closed)
@@ -199,28 +345,32 @@ bool Deserializer::deserialize()
         return false;
     }
 
-    tscpp::UnknownInputArchive inputArchive(file, tps);
-    while (true)
+    while (file.peek() != -1)
     {
+        std::string type_name;
+
+        // Read null terminated name string
+        while (file.peek() != 0)
+            type_name += file.get();
+        // Dummy null read
+        file.get();
+
         try
         {
-            inputArchive.unserialize();
+            // Invoke correct parser for this type
+            std::cout << "Deserializing type: " << type_name << std::endl;
+            (types.at(type_name))(file);
         }
-        catch (tscpp::TscppException& ex)
+        catch (const std::out_of_range& e)
         {
-            // Reached end of file
-            if (strcmp(ex.what(), "eof") == 0)
-            {
-                return true;
-            }
-            // Unknown type found
-            else if (strcmp(ex.what(), "unknown type") == 0)
-            {
-                std::string unknownTypeName = ex.name();
-                std::cout << "Unknown type found: " << unknownTypeName
-                          << std::endl;
-                return false;
-            }
+            std::cerr << "Type not found: " << type_name << std::endl;
+            return false;
+        }
+        catch (std::runtime_error& e)
+        {
+            std::cerr << "Error deserializing type: " << type_name << " - "
+                      << e.what() << std::endl;
+            return false;
         }
     }
 
