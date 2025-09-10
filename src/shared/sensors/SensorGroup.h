695709a9337be07a568a458869c84dab4d780ac6
Fabrizio Monti <fabrizio.monti@skywarder.eu>
2025-09-10 11:30:10 +0200

    [WIP] cppcheck: solving noExplicitConstructor and noOperatorEq.

--- src/shared/sensors/SensorGroup.h
+++ src/shared/sensors/SensorGroup.h
@@ -55,7 +55,10 @@ public:
      * will assign to SensorSamplers incremental IDs starting from the maximum
      * among the tasks already existing in the TaskScheduler.
      */
+    // cppcheck-suppress  noExplicitConstructor
     SensorGroup(GroupId_t groupId, TaskScheduler* scheduler = nullptr);
+
+    SensorGroup& operator=(const SensorGroup&) = delete;
 
     /**
      * @brief Deallocates the scheduler if needed.
