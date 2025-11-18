a5746a27ce500f5170e345b9e93ce47d3cbe6c19
giovannaloro <giovanni.annaloro@skywarder.eu>
2025-11-18 14:41:21 +0100

    [ada-autocode] ada controller modified

---
+++ src/shared/algorithms/ADA/ADA_Algorithm0_private.h
@@ -0,0 +1,32 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+//
+// File: ADA_Algorithm0_private.h
+//
+// Code generated for Simulink model 'ADA_Algorithm0'.
+//
+// Model version                  : 1.11
+// Simulink Coder version         : 23.2 (R2023b) 01-Aug-2023
+// C/C++ source code generated on : Wed Nov  1 00:43:29 2023
+//
+// Target selection: ert.tlc
+// Embedded hardware selection: STMicroelectronics->ST10/Super10
+// Code generation objectives:
+//    1. Execution efficiency
+//    2. Safety precaution
+// Validation result: Passed (20), Warning (1), Error (0)
+//
+#ifndef RTW_HEADER_ADA_Algorithm0_private_h_
+#define RTW_HEADER_ADA_Algorithm0_private_h_
+#include <stdbool.h>
+#include <stdint.h>
+#include "ADA_Algorithm0_types.h"
+#endif                                 // RTW_HEADER_ADA_Algorithm0_private_h_
+
+//
+// File trailer for generated code.
+//
+// [EOF]
+//
