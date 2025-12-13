d2916b06083a15203f20985493d551cb45374735
Pietro Bortolus <pietro.bortolus@skywarder.eu>
2025-12-13 15:41:49 +0100

    [algorithms] Added an algorithm for an electronic regulator
    This algorithm takes a pressure input downstream from a valve and uses a simple PID to control the
    valve's servo to regulate the downstream pressure.
    It is possible to change the reference point and the PID constants while the algorithm is running,
    doing so resets the internal state of the algorithm.

---
+++ src/shared/algorithms/EReg/EReg.h
@@ -0,0 +1,85 @@
+/* Copyright (c) 2025 Skyward Experimental Rocketry
+ * Authors: Pietro Bortolus
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#pragma once
+
+#include <algorithms/Algorithm.h>
+
+#include <functional>
+#include <limits>
+#include <vector>
+
+#include "ERegPIDConfig.h"
+
+namespace Boardcore
+{
+
+class EReg : public Algorithm
+{
+public:
+    EReg(const ERegPIDConfig& config, float targetPressure,
+         std::function<float()> getPressure,
+         std::function<void(float)> setValvePosition);
+
+    bool init() { return true; }
+
+    /**
+     * @brief Change the reference point for the PID, this also resets the
+     * state.
+     */
+    void setReferencePoint(float newTargetPressure);
+
+    /**
+     * @brief Change the config of the PID, this also resets the state.
+     */
+    void changePIDConfig(const ERegPIDConfig& newConfig);
+
+protected:
+    /**
+     * @brief Update the PID internal state.
+     * */
+    void step() override;
+
+private:
+    /**
+     * @brief Anti-windup mechanism.
+     */
+    float antiWindUp(float u, float uMin, float uMax);
+
+    /**
+     * @brief Resets the PID internal state.
+     */
+    void resetState();
+
+    ERegPIDConfig config;  // PID configs.
+    float targetPressure;  // Reference point.
+
+    std::function<float()> getPressure;
+    std::function<void(float)> setValvePosition;
+
+    float i         = 0;      // Integral contribution.
+    float d         = 0;      // Derivative contribution.
+    float lastError = 0;      // Error at the previous step.
+    bool saturation = false;  ///< have we reached saturation?
+};
+
+}  // namespace Boardcore
