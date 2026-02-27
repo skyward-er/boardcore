cbbdf84c074cf3f09c1451b8293d1b0ca9bf00ca
Riccardo Sironi <riccardo.sironi@skywarder.eu>
2026-02-27 15:08:46 +0100

    [PCA9685] Moved Channels enum into a separate class

--- src/shared/drivers/PCA9685/PCA9685.h
+++ src/shared/drivers/PCA9685/PCA9685.h
@@ -25,30 +25,13 @@
 #include <drivers/i2c/I2C.h>
 #include <sensors/Sensor.h>
 
+#include "PCA9685Channels.h"
+
 namespace Boardcore
 {
 class PCA9685
 {
 public:
-    enum class Channels : uint8_t
-    {
-        CHANNEL_0  = 0,
-        CHANNEL_1  = 1,
-        CHANNEL_2  = 2,
-        CHANNEL_3  = 3,
-        CHANNEL_4  = 4,
-        CHANNEL_5  = 5,
-        CHANNEL_6  = 6,
-        CHANNEL_7  = 7,
-        CHANNEL_8  = 8,
-        CHANNEL_9  = 9,
-        CHANNEL_10 = 10,
-        CHANNEL_11 = 11,
-        CHANNEL_12 = 12,
-        CHANNEL_13 = 13,
-        CHANNEL_14 = 14,
-        CHANNEL_15 = 15
-    };
     enum class OutputType : uint8_t
     {
         OPEN_DRAIN,
@@ -113,7 +96,7 @@ public:
      * @param dutyCycle Duty cycle percentage (0.0 - 100.0)
      * @return true if setting the duty cycle succeeded, false otherwise.
      */
-    bool setDutyCycle(PCA9685::Channels channel, float dutyCycle);
+    bool setDutyCycle(PCA9685Channels::Channel channel, float dutyCycle);
     /**
      * @brief Sets the duty cycle on all channels
      * @param dutyCycle Duty cycle percentage (0.0 - 100.0)
@@ -135,7 +118,7 @@ public:
      * Note: on and off cannot be equal.
      * @return true if setting the bits succeeded, false otherwise.
      */
-    bool setPWM(PCA9685::Channels channel, uint16_t on, uint16_t off);
+    bool setPWM(PCA9685Channels::Channel channel, uint16_t on, uint16_t off);
     /**
      * @brief Sets the 12-bit PWM signal on all channels
      * @param on Tick where the signal should turn ON (0-4095)
---
+++ src/shared/drivers/PCA9685/PCA9685Channels.h
@@ -0,0 +1,26 @@
+#include <stdint.h>
+namespace Boardcore
+{
+namespace PCA9685Channels
+{
+enum class Channel : uint8_t
+{
+    CHANNEL_0  = 0,
+    CHANNEL_1  = 1,
+    CHANNEL_2  = 2,
+    CHANNEL_3  = 3,
+    CHANNEL_4  = 4,
+    CHANNEL_5  = 5,
+    CHANNEL_6  = 6,
+    CHANNEL_7  = 7,
+    CHANNEL_8  = 8,
+    CHANNEL_9  = 9,
+    CHANNEL_10 = 10,
+    CHANNEL_11 = 11,
+    CHANNEL_12 = 12,
+    CHANNEL_13 = 13,
+    CHANNEL_14 = 14,
+    CHANNEL_15 = 15
+};
+}
+}  // namespace Boardcore
