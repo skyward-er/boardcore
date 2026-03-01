c7ad63d970e0827e15d4479698512095cc1384be
Leonardo Montecchi <leonardo.montecchi@skywarder.eu>
2026-03-01 19:21:41 +0100

    [AltitudeMap] Initial class layout for Altitude Map flash memory read-only access

---
+++ src/shared/algorithms/Flare/AltitudeMap/AltitudeMap.h
@@ -0,0 +1,57 @@
+/* Copyright (c) 2026 Skyward Experimental Rocketry
+ * Authors: Leonardo Montecchi
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#pragma once
+
+#include <diagnostic/PrintLogger.h>
+
+#include "AltitudeMapData.h"
+
+namespace Boardcore
+{
+/**
+ * @brief Class to retrieve altitude data from flash memory.
+ *
+ * This class provides methods to retrieve altitude data from flash memory.
+ * It is used by the Flare algorithm determine when to activate.
+ */
+class AltitudeMap
+{
+public:
+    AltitudeMap(const uint8_t* startAddress);
+
+    bool isInsideMap(float x, float y) const;
+
+    float getGroundAltitude(float x, float y) const;
+
+private:
+    const uint8_t* startAddress;
+    const MapHeader* header;
+    const TileDescriptor* tiles;
+
+    const TileDescriptor* getTileDescriptor(float x, float y) const;
+
+    // PrintLogger logger = Logging::getLogger("AltitudeMap");
+
+    miosix::FastMutex altitudeMapMutex;
+};
+}  // namespace Boardcore
