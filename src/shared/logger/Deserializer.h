c2de275ce37c3288db2faf037ada776832ff486c
Raul Radu <raul.radu@skywarder.eu>
2026-04-29 12:00:35 +0200

    [LogDecoder] fix format issue

--- src/shared/logger/Deserializer.h
+++ src/shared/logger/Deserializer.h
@@ -1,5 +1,6 @@
 /* Copyright (c) 2015-2026 Skyward Experimental Rocketry
- * Authors: Luca Erbetta, Alberto Nidasio, Pietro Bortolus, Niccolò Betto, Raul Radu
+ * Authors: Luca Erbetta, Alberto Nidasio, Pietro Bortolus, Niccolò Betto, Raul
+ * Radu
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
