dd1ae6b3cb37f5867ef68bb33958c83db38a723a
Pietro Bortolus <pietro.bortolus@skywarder.eu>
2026-05-30 15:43:39 +0200

    [Algorithms] Added Kriging autocoded algorithm

---
+++ src/shared/algorithms/SDA/Kriging0_private.h
@@ -0,0 +1,33 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+//
+// File: Kriging0_private.h
+//
+// Code generated for Simulink model 'Kriging0'.
+//
+// Model version                  : 11.257
+// Simulink Coder version         : 24.2 (R2024b) 21-Jun-2024
+// C/C++ source code generated on : Sun May 24 16:00:49 2026
+//
+// Target selection: ert.tlc
+// Embedded hardware selection: ARM Compatible->ARM Cortex-M
+// Code generation objectives:
+//    1. Execution efficiency
+//    2. Debugging
+//    3. RAM efficiency
+// Validation result: Not run
+//
+#ifndef Kriging0_private_h_
+#define Kriging0_private_h_
+#include <stdbool.h>
+#include <stdint.h>
+#include "Kriging0_types.h"
+#endif                                 // Kriging0_private_h_
+
+//
+// File trailer for generated code.
+//
+// [EOF]
+//
