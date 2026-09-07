6b48786b65b7a11e74d90530bb3c0ed83ba7fa54
Giovanni Annaloro <giovanni.annaloro@skywarder.eu>
2026-09-07 18:26:47 +0200

    Use submodules for Catch2, eigen, fmt, mavlink and socrate

--- libs/mavlink-skyward-lib/generate.sh
+++
@@ -1,3 +0,0 @@
-cd  mavlink
-python3 -m pymavlink.tools.mavgen --lang=C --wire-protocol=1.0 --output=../mavlink_lib ../message_definitions/orion.xml
-python3 -m pymavlink.tools.mavgen --lang=Python --wire-protocol=1.0 --output=../mavlink_lib ../message_definitions/orion.xml
