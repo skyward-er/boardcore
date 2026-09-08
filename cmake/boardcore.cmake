bbc8d0e27e6aa00e85328a8f77d3b96caecc5688
Giovanni Annaloro <giovanni.annaloro@skywarder.eu>
2026-09-08 16:06:56 +0200

    fixed target lists

--- cmake/boardcore.cmake
+++ cmake/boardcore.cmake
@@ -34,6 +34,7 @@ set(BOARDCORE_SRC
     # Actuators
     ${BOARDCORE_PATH}/src/shared/actuators/HBridge/HBridge.cpp
     ${BOARDCORE_PATH}/src/shared/actuators/Servo/Servo.cpp
+    ${BOARDCORE_PATH}/src/shared/actuators/Servo/ServoWinch.cpp
     ${BOARDCORE_PATH}/src/shared/actuators/stepper/Stepper.cpp
     ${BOARDCORE_PATH}/src/shared/actuators/stepper/StepperPWM.cpp
     ${BOARDCORE_PATH}/src/shared/Valve/Valve.cpp
@@ -58,6 +59,7 @@ set(BOARDCORE_SRC
     ${BOARDCORE_PATH}/libs/autocoded-algorithms/zvk/ZVK.cpp
     ${BOARDCORE_PATH}/src/shared/algorithms/StateInitializer/StateInitializer.cpp
     ${BOARDCORE_PATH}/src/shared/algorithms/Ereg/Ereg.cpp
+    ${BOARDCORE_PATH}/src/shared/algorithms/SchmittTrigger/SchmittTrigger.cpp
     ${SBS_BASE}/src/shared/algorithms/Propagator/Propagator.cpp
     ${SBS_BASE}/src/shared/algorithms/Follower/Follower.cpp
 
@@ -141,9 +143,11 @@ set(BOARDCORE_SRC
     ${BOARDCORE_PATH}/src/shared/sensors/LPS28DFW/LPS28DFW.cpp
     ${BOARDCORE_PATH}/src/shared/sensors/LPS22DF/LPS22DF.cpp
     ${BOARDCORE_PATH}/src/shared/sensors/LSM6DSRX/LSM6DSRX.cpp
+    ${BOARDCORE_PATH}/src/shared/sensors/AS5047D/AS5047DSPI.cpp
     ${BOARDCORE_PATH}/src/shared/sensors/ND015X/ND015D.cpp
     ${BOARDCORE_PATH}/src/shared/sensors/ND015X/ND015A.cpp
-    ${BOARDCORE_PATH}/src/shared/sensors/ND030D/ND030D.cpp
+    ${BOARDCORE_PATH}/src/shared/sensors/ND030X/ND030D.cpp
+    ${BOARDCORE_PATH}/src/shared/sensors/ND030X/ND030A.cpp
 
     # Calibration
     ${BOARDCORE_PATH}/src/shared/sensors/calibration/BiasCalibration/BiasCalibration.cpp
@@ -162,6 +166,7 @@ set(BOARDCORE_SRC
     ${BOARDCORE_PATH}/src/shared/utils/AltitudeMap/AltitudeQuadMap.cpp
     ${BOARDCORE_PATH}/src/shared/utils/ButtonHandler/ButtonHandler.cpp
     ${BOARDCORE_PATH}/src/shared/utils/PinObserver/PinObserver.cpp
+    ${BOARDCORE_PATH}/src/shared/utils/PinObserver/ExternalPinObserver.cpp
     ${BOARDCORE_PATH}/src/shared/utils/SkyQuaternion/SkyQuaternion.cpp
     ${BOARDCORE_PATH}/src/shared/utils/Stats/Stats.cpp
     ${BOARDCORE_PATH}/src/shared/utils/TestUtils/TestHelper.cpp
